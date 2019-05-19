368a1b7f582beb5443402562867b9705fd14790f
Luis Donis <emc2@ldonis.com>
2019-05-19 20:33:31 +0200

    Setting devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+    devise_scope :user do
+        get "user/register", to: "devise/registrations#new"
+        get "user/logout", to: "devise/sessions#destroy"
+        get "user/login", to: "devise/sessions#new"
+    end
     devise_for :users
     
     root to: 'website/pages#landing'
--- db/schema.rb
+++ db/schema.rb
@@ -21,15 +21,22 @@ ActiveRecord::Schema.define(version: 2019_05_18_103347) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "website_landings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
