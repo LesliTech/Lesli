0df8501c8cb4803aeeb889dd4851d414dd07ff9f
Luis Donis <ldonis.emc2@gmail.com>
2019-08-10 21:32:29 -0600

    Clickable sidenav

--- app/vue/layout/sidenav.vue
+++ app/vue/layout/sidenav.vue
@@ -1,10 +1,31 @@
 <script>
 export default {
 
+    data() {
+        return {
+            show:false
+        }
+    },
+
+    mounted() {
+        this.bus.$on('lesli.layout.sidenav.show', this.showMenu)
+    },
+
+    methods: {
+
+        showMenu() {
+
+            this.show = !this.show
+            setTimeout(() => this.show = false, 4000)
+
+        }
+
+    }
+
 }
 </script>
 <template>
-    <nav class="sidenav">
+    <nav :class="['sidenav', { 'show':show }]">
         <ul>
             <li v-for="(a,i) in [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6]" :key="i">
                 <a href="">
