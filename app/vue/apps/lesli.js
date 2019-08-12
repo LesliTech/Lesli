afa71cd102b2444c1067e13f5fed9a5a4b1ba6f3
Luis Donis <ldonis.emc2@gmail.com>
2019-08-12 06:53:13 -0600

    Adding specific and default vue app to rails workflow

--- app/vue/apps/lesli.js
+++ app/vue/apps/lesli.js
@@ -35,43 +35,54 @@ import VueBootstrap from 'bootstrap-vue'
 
 //  · Loading app plugins
 // ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~
-//import urlPlugin from 'TheCrow/vue/plugins/url'
-//import utilPlugin from 'TheCrow/vue/plugins/util'
-//import httpPlugin from 'TheCrow/vue/plugins/http'
-//import mqttPlugin from 'TheCrow/vue/plugins/mqtt'
+import pluginBus from 'LesliCloud/vue/plugins/bus'
+import pluginUrl from 'LesliCloud/vue/plugins/url'
+import pluginUtil from 'LesliCloud/vue/plugins/util'
+import pluginHttp from 'LesliCloud/vue/plugins/http'
 
 
 //  · Loading app functions
 // ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~
-import documentFunction from 'LesliCloud/vue/functions/document'
+import debug from 'LesliCloud/vue/functions/debug'
+import functionDocument from 'LesliCloud/vue/functions/document'
 
 
 //  · Loading app layout
 // ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~
-import componentHeader from 'LesliCloud/vue/layout/header.vue'
-//import layoutfooter from 'LesliCloud/vue/layout/footer-admin.vue'
+import componentLesliChatbox from 'LesliCloud/vue/components/chatbox.vue'
+import componentLayoutHeader from 'LesliCloud/vue/layout/header.vue'
+import componentLayoutSidenav from 'LesliCloud/vue/layout/sidenav.vue'
+
+
+//  · Loading apps, modules and components
+// ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~        ~·~
+
 
 
 // · Initializing frameworks, libraries and tools
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-//Vue.use(utilPlugin)
-//Vue.use(urlPlugin, { config: lesli_config })
-//Vue.use(httpPlugin, { config: lesli_config })
-//Vue.use(mqttPlugin, { config: lesli_config })
+Vue.use(pluginBus)
+Vue.use(pluginUrl)
+Vue.use(pluginUtil)
+Vue.use(pluginHttp)
 Vue.use(VueBootstrap)
-//Vue.use(VueRouter)
 
 
 // · LesliCloud app
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 let cloud = new Vue({
+
     components: {
-        'component-header': componentHeader,
-        //'layout-footer': layoutfooter
+        'component-layout-header': componentLayoutHeader,
+        'component-layout-sidenav': componentLayoutSidenav,
+        'component-lesli-chatbox': componentLesliChatbox
     },
+
     mounted() {
-        console.log("works")
+        console.log("Default lesli app")
     }
+
 })
 
-documentFunction.ready(() => cloud.$mount("#leslicloud_app"))
+// · 
+functionDocument.ready(() => cloud.$mount("#lesli-cloud-app"))
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,11 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
+Rails.application.config.assets.precompile += %w( users/*.css users/**/*.css )
 Rails.application.config.assets.precompile += %w( websites/landing.css )
 Rails.application.config.assets.precompile += %w( cloud_lesli/*.css  )
-Rails.application.config.assets.precompile += %w( users/*.css users/**/*.css )
-Rails.application.config.assets.precompile += %w( lesli.js )
+Rails.application.config.assets.precompile += %w( cloud_team/*.css )
 
+Rails.application.config.assets.precompile += %w( lesli.js )
+Rails.application.config.assets.precompile += %w( cloud_team/*.js cloud_team/**/*.js)
 Rails.application.config.assets.precompile += %w( cloud_lesli/*.js cloud_lesli/**/*.js )
