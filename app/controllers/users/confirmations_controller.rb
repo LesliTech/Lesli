2f13f6096078cb8715e2205f7b2117080cfa723e
Luis Donis <ldonis.emc2@gmail.com>
2019-08-19 20:02:07 -0600

    resend confirmation email with rails 6 and bulma

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
+    layout 'application_devise'
     # GET /resource/confirmation/new
     # def new
     #   super
