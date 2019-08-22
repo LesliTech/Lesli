0666044a95165ef275e3dfcc22c57ffc8f14bbe1
Luis Donis <ldonis.emc2@gmail.com>
2019-08-21 19:34:17 -0600

    Custom failure app for devise actions

---
+++ app/failures/devise_failure_app.rb
@@ -0,0 +1,17 @@
+class DeviseFailureApp < Devise::FailureApp
+
+    def redirect_url
+        login_path(:subdomain => 'secure')
+    end
+
+    # You need to override respond to eliminate recall
+    def respond
+
+        if http_auth?
+            http_auth
+        else
+            redirect
+        end
+
+    end
+end
