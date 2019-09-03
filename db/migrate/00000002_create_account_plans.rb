a4e718165d515f5531ca61bd2e51203c251a4b6a
Luis Donis <ldonis.emc2@gmail.com>
2019-09-02 20:05:22 -0600

    Adding initial data

--- db/migrate/00000002_create_account_plans.rb
+++ db/migrate/00000002_create_account_plans.rb
@@ -1,8 +1,8 @@
 class CreateAccountPlans < ActiveRecord::Migration[5.2]
     def change
         create_table :account_plans do |t|
-
-            t.timestamps
+            t.string :name
         end
+        execute "insert into account_plans (id, name) values (0, 'demo')"
     end
 end
