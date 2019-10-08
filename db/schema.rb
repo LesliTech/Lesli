feb553bfc1dc69c110f13b3b322e59c99e3bc557
Luis Donis <ldonis.emc2@gmail.com>
2019-10-08 12:27:57 -0600

    Trix editor - proof of concept

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_172447) do
+ActiveRecord::Schema.define(version: 8020001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
