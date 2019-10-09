ee96961f2a53ff8043c3f1fac8feb85ed9c459a0
Luis Donis <ldonis.emc2@gmail.com>
2019-10-08 22:43:15 -0600

    Standard name for migrations

--- db/migrate/00000003_create_users.rb
+++ db/migrate/00000003_create_users.rb
@@ -1,5 +1,4 @@
-# frozen_string_literal: true
-class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.2]
     def change
         create_table :users do |t|
 
