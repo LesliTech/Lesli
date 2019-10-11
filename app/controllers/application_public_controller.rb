f0804de59a47ea8eb8b5907f67b68ff9eeb48d20
Luis Donis <ldonis.emc2@gmail.com>
2019-10-11 11:27:37 -0600

    standard names for controllers and base controllers

---
+++ app/controllers/application_public_controller.rb
@@ -0,0 +1,3 @@
+class ApplicationPublicController < ApplicationController
+
+end
--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -3,23 +3,5 @@ class WebsitesController < PublicApplicationController
 
     def landing
     end
-
-    
-
-    def switch_locale2
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p params[:locale]
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        p '*     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *'
-        session[:locale] = params[:locale] # if I18n.available_locales.include?(params[:locale].to_sym)
-        redirect_back fallback_location: root_path
-    end
     
 end
