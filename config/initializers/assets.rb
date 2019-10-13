5f978867f33b7d9a4d435af6bc82ef76fc9bcf00
Luis Donis <ldonis.emc2@gmail.com>
2019-10-12 23:52:09 -0600

    Include asset folder into asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor')
 
 # Precompile additional assets.
 Rails.application.config.assets.precompile += %w( websites.css )
