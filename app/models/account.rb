a2b1b9aea3903d03079f689c4cb336be802ee325
Luis Donis <ldonis.emc2@gmail.com>
2019-10-13 14:29:51 -0600

    Add help to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,13 +3,13 @@ class Account < ApplicationRecord
     belongs_to :account_plan, foreign_key: 'account_plans_id'
     has_many :user, foreign_key: 'users_id'
 
-    has_one :driver, class_name: "CloudDriver::Account", foreign_key: "id"
-    has_one :team, class_name: "CloudTeam::Account", foreign_key: "id"
-    has_one :bell, class_name: "CloudBell::Account", foreign_key: "id"
-    has_one :kb, class_name: "CloudKb::Account", foreign_key: "id"
-    has_one :books, class_name: "CloudBooks::Account", foreign_key: "id"
-    has_one :help, class_name: "CloudHelp::Account", foreign_key: "id"
-    has_one :panel, class_name: "CloudPanel::Account", foreign_key: "id"
-    has_one :lock, class_name: "CloudLock::Account", foreign_key: "id"
+    has_one :kb,    class_name: "CloudKb::Account",     foreign_key: "id"
+    has_one :team,  class_name: "CloudTeam::Account",   foreign_key: "id"
+    has_one :bell,  class_name: "CloudBell::Account",   foreign_key: "id"
+    has_one :help,  class_name: "CloudHelp::Account",   foreign_key: "id"
+    has_one :lock,  class_name: "CloudLock::Account",   foreign_key: "id"
+    has_one :books, class_name: "CloudBooks::Account",  foreign_key: "id"
+    has_one :panel, class_name: "CloudPanel::Account",  foreign_key: "id"
+    has_one :driver,class_name: "CloudDriver::Account", foreign_key: "id"
 
 end
