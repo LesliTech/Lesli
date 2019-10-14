22d6518e65d06cfdb06d82d4b13e41b960746226
Luis Donis <ldonis.emc2@gmail.com>
2019-10-14 11:33:54 -0600

    Create components to work with comments - proof of concept

---
+++ app/vue/components/forms/comment.vue
@@ -0,0 +1,92 @@
+<script>
+/*
+Lesli
+
+Copyright (c) 2019, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@dev      Luis Donis <ldonis@lesli.tech>
+@author   LesliTech <hello@lesli.tech>
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+
+
+// · Import modules, components and apps
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+import VueTrix from "vue-trix"
+
+
+
+// · Component
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+export default {
+    props: {
+        module: {
+            type: String,
+            required: true
+        }
+    },
+    components: {
+        'component-trix-editor': VueTrix
+    },
+    data() {
+        return {
+            show_simple_form: true,
+            comment: {
+                content: ""
+            }
+        }
+    },
+    methods: {
+        
+        postComment(e) {
+
+            if (e) { e.preventDefault() }
+
+            this.http.post("/help/ticket/comments", {
+                ticket_comment: this.comment
+            }).then(result => {
+                if (result.successful) {
+                    this.comment.content = ""
+                }
+                this.bus.$emit("post:components/forms/comment")
+                console.log(result)
+            }).catch(error => {
+                console.log(error)
+            })
+
+        }
+
+    }
+
+}
+</script>
+<template>
+    <section class="section">
+        <div class="card">
+            <div class="card-content">
+                <form @submit="postComment">
+                    <input v-if="show_simple_form" class="input" type="text" v-model="comment.content" placeholder="Add a comment...">
+                    <component-trix-editor v-if="!show_simple_form" v-model="comment.content"></component-trix-editor>
+                </form>
+            </div>
+        </div>
+    </section>
+</template>
