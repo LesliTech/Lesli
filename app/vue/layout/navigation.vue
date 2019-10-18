5e83756ed4dbfef0bd5017d470aee723472cffef
Luis Donis <ldonis.emc2@gmail.com>
2019-10-18 10:14:49 -0600

    Add slots to navigation

--- app/vue/layout/navigation.vue
+++ app/vue/layout/navigation.vue
@@ -21,9 +21,10 @@ export default {
     <nav class="module-navigation navbar is-transparent">
         <div class="navbar-menu">
             <div class="navbar-start">
-                <slot></slot>
+                <slot name="right"></slot>
             </div>
             <div class="navbar-end">
+                <slot name="left"></slot>
                 <router-link class="navbar-item" v-if="id" :to="`/${ id }/show`">Details</router-link>
                 <router-link class="navbar-item" v-if="id" :to="`/${ id }/discussion`">Discussion</router-link>
                 <router-link class="navbar-item" v-if="id" :to="`/${ id }/actions`">Actions</router-link>
