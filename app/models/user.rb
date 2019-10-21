4dcd6781fed47be0f9390326e4e69bcd97fc1afe
Luis Donis <ldonis.emc2@gmail.com>
2019-10-20 18:44:28 -0600

    Implement user notifiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
     
     belongs_to :account , foreign_key: 'accounts_id', optional: true
     has_one :user_detail, foreign_key: 'user_details_id'
+    has_many :notifications
 
 end
