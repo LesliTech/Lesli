96f0099c33374c2fc9da0c291d6ad0bd3d92713e
Luis Donis <ldonis.emc2@gmail.com>
2019-10-29 11:12:01 -0600

    Rspec implementation && first test passed

---
+++ spec/controllers/websites_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe WebsitesController, type: :controller do
+    describe "GET landing" do
+        it "responds successfully with an HTTP 200 status code" do
+            get :landing
+            expect(response.status).to eq(200)
+        end
+    end
+end
