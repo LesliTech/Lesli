de470c5a780ce8224c5319df3c5ed3bb4a291a4d
Luis Donis <ldonis.emc2@gmail.com>
2019-10-29 11:13:37 -0600

    remove minitest

--- test/models/company/location_country_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Company::LocationCountryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/company/role_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Company::RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
