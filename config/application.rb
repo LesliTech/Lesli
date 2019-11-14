791b37ee3ea9eb5cd9946d1083e1d9212bd8323f
volant360 <efrainh12@galileo.edu>
2019-11-14 11:30:54 -0600

    Added engines translations paths to config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module LesliCloud
         # -- all .rb files in that directory are automatically loaded after loading
         # the framework and any gems in your application.
         config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+        config.i18n.load_path += Dir[Rails.root.join('engines', '*', 'config', 'locales', '**', '*.{rb,yml}')]
         config.i18n.default_locale = :en
         config.i18n.available_locales = [:en]
 
