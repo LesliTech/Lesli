0c0693f00fb810677cfd6b0e71d183447ec3752b
Luis Donis <ldonis.emc2@gmail.com>
2019-11-21 09:30:34 -0600

    implement cable (core)

--- app/assets/javascripts/assistants/lesli_dashboard.js
+++ app/assets/javascripts/assistants/lesli_dashboard.js
@@ -30063,11 +30063,13 @@ Building a better future, one line of code at a time.
         Vue.prototype.bus.publish("keyboard-ctrl-save");
       }
     }, false);
-    var cable = Object(action_cable["createConsumer"])('/courier/cable');
-    cable.subscriptions.create("CloudCourier::LesliChannel", {
+    var cable = Object(action_cable["createConsumer"])();
+    cable.subscriptions.create("LesliChannel", {
+      connected: function connected() {},
       received: function received(data) {
         Vue.prototype.bus.publish(data.channel, data);
-      }
+      },
+      disconnected: function disconnected() {}
     });
   }
 });
