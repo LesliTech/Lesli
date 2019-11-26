ee4d2cc0555000038108d006e6cd71d58b0757df
Luis Donis <ldonis.emc2@gmail.com>
2019-11-26 11:35:06 -0600

    add active storage tables

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,27 @@ ActiveRecord::Schema.define(version: 1010211) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "bell_emails", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -289,6 +310,7 @@ ActiveRecord::Schema.define(version: 1010211) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bell_emails", "bells", column: "bells_id"
   add_foreign_key "bell_messages", "bells", column: "bells_id"
   add_foreign_key "bell_notifications", "bells", column: "bells_id"
