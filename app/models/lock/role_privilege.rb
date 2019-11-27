e4318bbf75fceea0aca71c30a5ec1c99bac08ef9
The Lesli Development Team <hello@lesli.tech>
2019-11-26 20:18:31 -0600

    Creation of lock, lock_roles and lock_role_privileges
    When an account is created it is automatically generated a lock, a lock_role and lock_role_privilege to the account. Change relations in the database and models. Add cancancan gem.

--- app/models/lock/role_privilege.rb
+++ app/models/lock/role_privilege.rb
@@ -1,2 +1,3 @@
 class Lock::RolePrivilege < ApplicationRecord
+    belongs_to :lock_role, class_name: "Role", foreign_key: 'lock_roles_id'
 end
--- db/migrate/00000202_create_lock_roles.rb
+++ db/migrate/00000202_create_lock_roles.rb
@@ -1,6 +1,7 @@
 class CreateLockRoles < ActiveRecord::Migration[6.0]
     def change
         create_table :lock_roles do |t|
+            t.string :name
             t.timestamps
         end
         add_reference :lock_roles, :locks, foreign_key: true
