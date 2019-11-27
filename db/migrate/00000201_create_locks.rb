c62a1b7b775f70504357f38804ae1c8373c5cc35
Luis Donis <ldonis.emc2@gmail.com>
2019-11-27 11:24:40 -0600

    active active_storage

--- db/migrate/00000201_create_locks.rb
+++ db/migrate/00000201_create_locks.rb
@@ -1,7 +1,6 @@
 class CreateLocks < ActiveRecord::Migration[6.0]
     def change
         create_table :locks do |t|
-            t.timestamps
         end
         add_foreign_key :locks, :accounts, column: :id
     end
--- db/migrate/20191126170845_create_user_role_overrides.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserRoleOverrides < ActiveRecord::Migration[6.0]
-  def change
-    create_table :user_role_overrides, :id => false do |t|
-        t.timestamps
-    end
-    add_reference :user_role_overrides, :users, foreign_key: true
-    add_reference :user_role_overrides, :lock_role_overrides, foreign_key: true
-  end
-end
