3365e9488be6b04a3d0596ef7338ac5cd457c1c6
Luis Donis <ldonis.emc2@gmail.com>
2019-11-28 09:41:36 -0600

    add lib as courier - proof of concept

---
+++ app/services/courier/team/employees.rb
@@ -0,0 +1,12 @@
+module Courier
+    class Team::Employees
+
+        def getEmployees
+            return [{
+                name: 'ldonis'
+            },{
+                name: 'wishin'
+            }]
+        end
+    end
+end
