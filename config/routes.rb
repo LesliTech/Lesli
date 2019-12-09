5e3c3210a4223a17263ed393ca7fb03917ea76b6
Luis Donis <ldonis.emc2@gmail.com>
2019-12-08 21:05:03 -0600

    implement CloudBabel module - translation and localization builder

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
         resource :account
 
         mount ActionCable.server  => "/cable"
+        mount CloudBabel::Engine  => "/babel"  if defined?(CloudBabel)
         mount CloudPanel::Engine  => "/panel"  if defined?(CloudPanel)
         mount CloudLesli::Engine  => "/lesli"  if defined?(CloudLesli)
         mount CloudTeam::Engine   => "/team"   if defined?(CloudTeam)
