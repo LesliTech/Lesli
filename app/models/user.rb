765c15c59f1809193d45faa919109a62c9b1246f
Luis Donis <ldonis.emc2@gmail.com>
2019-12-13 12:34:08 -0600

    add modules account creation for current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
             :trackable, 
             :validatable,
             :confirmable
+
+    after_save :check_accounts    
     
     belongs_to  :account , foreign_key: 'accounts_id', optional: true
     belongs_to  :role, class_name: 'CloudLock::Role', foreign_key:'cloud_lock_roles_id', optional: true
@@ -30,12 +32,31 @@ class User < ApplicationRecord
         return email
     end
 
+    
+
     def revoke_access
         update_attributes(active: false)
     end
 
     def ability
         @ability ||= Ability.new(self)
+    end
+
+    def check_accounts
+        if defined? CloudTeam
+            if self.account.team.blank?
+                self.account.team = CloudTeam::Account.new
+                self.account.team.account = self.account
+                self.account.team.save!
+            end
+        end
+        if defined? CloudBell
+            if self.account.bell.blank?
+                self.account.bell = CloudBell::Account.new
+                self.account.bell.account = self.account
+                self.account.bell.save!
+            end
+        end
     end
 
 end
