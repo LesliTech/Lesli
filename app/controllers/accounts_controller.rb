ff04bebfff2a993f4923c91c19ba0ad5389abc59
Luis Donis <ldonis.emc2@gmail.com>
2020-01-02 21:30:56 -0600

    improve yard

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,29 @@
-# Converts the object into textual markup given a specific format.
-#
-# @param format [Symbol] the format type, `:text` or `:html`
-# @return [String] the object converted into the expected format.
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@description    Converts the object into textual markup given a specific format.
+@author         LesliTech <hello@lesli.tech>
+@license        Propietary - all rights reserved.
+@version        0.1.0-alpha
+
+=end
+
 class AccountsController < ApplicationLesliController
     before_action :set_account, only: [:edit, :update, :destroy]
     layout 'application_public'
