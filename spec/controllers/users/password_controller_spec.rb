9a98e61a02be4cbdf7acc1881640296649e07f37
Luis Donis <ldonis.emc2@gmail.com>
2020-01-13 19:36:24 -0600

    add test for users/password

---
+++ spec/controllers/users/password_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.configure do |config|
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+end
+
+RSpec.describe Users::PasswordsController, type: :controller do
+
+    before :each do
+        request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+
+    describe "post:/users/password" do
+
+        it "Not valid email" do
+            post :create, params: {
+                user: {
+                    email: "random@lesli.cloud"
+                }
+            }
+            expect(response).to have_http_status(:success) 
+            expect(response.content_type).to eq("application/json; charset=utf-8")
+            expect(JSON.parse(response.body)["successful"]).to eql(false)
+            expect(JSON.parse(response.body)).to eql({
+                "successful"=> false,
+                "error"=> {
+                    "details"=> nil, 
+                    "message"=> "Email not found"
+                }
+            })
+        end
+
+    end
+end
