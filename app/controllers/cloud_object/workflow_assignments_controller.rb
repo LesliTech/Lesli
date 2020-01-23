aa157d8a9916492ce6d188eafb2a119dc7951ad8
The Lesli Development Team <hello@lesli.tech>
2020-01-23 13:37:30 -0600

    add trello/936 Workflow Assignments implemented in core

---
+++ app/controllers/cloud_object/workflow_assignments_controller.rb
@@ -0,0 +1,143 @@
+module CloudObject
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@author   Carlos Hermosilla
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+@description Base controller for the *workflow* *assignment* core entity. This controller
+    will manage associations between a *workflow* and its *cloud_object*.
+=end
+    class WorkflowAssignmentsController < ApplicationLesliController
+
+        before_action :set_workflow_assignment, only: :update
+
+=begin
+@return [HTML|JSON] HTML view for listing all workflow assignments or a Json that contains a list 
+    of all workflow assignments associated to this *account*
+@description Retrieves and returns all workflow assignments associated to a *CloudHelp::Account*. 
+    The account is obtained directly from *current_user*. The HTTP request has to specify
+    wheter the HTML or the JSON text should be rendered
+@example
+    # Executing this controller's action from javascript's frontend
+    this.http.get('127.0.0.1/help/ticket_workflow_assignments');
+=end
+        def index
+            respond_to do |format|
+                format.html {}
+                format.json do
+                    dynamic_info = self.class.dynamic_info
+                    model = dynamic_info[:model]
+                    
+                    responseWithSuccessful(model.detailed_info(current_user.account, params[:name], params[:resource_id])) 
+                end
+            end
+        end
+
+        def workflow_assignment_options
+            dynamic_info = self.class.dynamic_info
+            model = dynamic_info[:model]
+            responseWithSuccessful(model.associations)
+        end
+
+        def update
+            return responseWithNotFound unless @workflow_assignment
+            if @workflow_assignment.update(workflow_assignment_params)
+                responseWithSuccessful(@workflow_assignment)
+            else
+                responseWithError(@workflow_assignment.errors.full_messages.to_sentence)
+            end
+        end
+
+        private
+
+=begin
+@return [void]
+@description Sets the variable @workflow_assignment. The variable contains the *workflow* *assignment* 
+    to be handled by the controller action that called this method
+@example
+    #suppose params[:id] = 1 and controller is CloudHelp::TicketWorkflowAssignmentsController
+    puts @workflow_assignment # will display nil
+    set_state
+    puts @workflow_assignment # will display an instance of CloudHelp:TicketWorkflowAssignment
+=end
+        def set_workflow_assignment
+            dynamic_info = self.class.dynamic_info
+
+            module_name = dynamic_info[:module_name]
+            model = dynamic_info[:model]
+
+            @workflow_assignment = model.find_by(
+                id: params[:id],
+                "cloud_#{module_name}_accounts_id".to_sym => current_user.account.id
+            )
+        end
+
+=begin
+@return [Parameters] Allowed parameters for the workflow_assignment
+@description Sanitizes the parameters received from an HTTP call to only allow the specified ones.
+    Allowed params are :cloud_[cloud_module_name]_[cloud_object_name]_workflows_id
+@example
+    # supose params contains {
+    #    "workflow_assignment": {
+    #        "id": 5,
+    #        "cloud_help_ticket_workflows_id": 5,
+    #        "cloud_help_ticket_types_id": 9
+    #    }
+    #}
+    filtered_params = workflow_assignment_params
+    puts filtered_params
+    # will only print {
+    #    "cloud_help_ticket_workflows_id": 5
+    #}
+=end
+        def workflow_assignment_params
+            dynamic_info = self.class.dynamic_info
+
+            module_name = dynamic_info[:module_name]
+            cloud_object_name = dynamic_info[:cloud_object_name]
+            object_name = dynamic_info[:object_name]
+
+            params.fetch(object_name.to_sym, {}).permit("cloud_#{module_name}_#{cloud_object_name}_workflows_id".to_sym)
+        end
+
+=begin
+@return [Hash] Hash that contains information about the class
+@description Returns dynamic information based on the current implementation of this abstract class
+@example
+    # Imagine the current class is an instance of CloudHelp::TicketWorkflowAssignmentsController < CloudObject::WorkflowAssignmentsController
+    info = CloudHelp::TicketWorkflowAssignmentsController.dynamic_info
+    puts info[:module_name] # will print 'help'
+    puts info[:object_name] # will print 'ticket_workflow_assignment'
+    info[:cloud_object_name] # will print 'ticket'
+    info[:model].new # will print an instance of CloudHelp::TicketWorkflowAssignment
+=end
+        def self.dynamic_info
+            module_info = self.name.split("::")
+            cloud_object_name = module_info[1].sub("WorkflowAssignmentsController", "")
+            {
+                module_name: module_info[0].sub("Cloud", "").downcase,
+                cloud_object_name: cloud_object_name.downcase,
+                object_name: "#{cloud_object_name.downcase}_workflow_assignment",
+                model: "#{module_info[0]}::#{cloud_object_name}WorkflowAssignment".constantize
+            }
+        end
+    end
+end
+    
--- app/models/cloud_object/workflow_assignment.rb
+++ app/models/cloud_object/workflow_assignment.rb
@@ -27,7 +27,119 @@ Building a better future, one line of code at a time.
     class WorkflowAssignment < ApplicationRecord
         self.abstract_class = true
 
-        assingment_keys = []
+=begin
+@param account [Account] Account to which the records are associated to
+@param association_name [String] The name of the association that requested the assignments
+@param association_id [Integer] The id of the associated record specified by *association_name*
+@return [Array] Information about all the assignments, including workflow name, and associations names
+@descriptions Retrieves all information about the assignments and returns it. If the assignments are associated
+    to two or more tables, also retrieves important information for those association that are not the one
+    provided in the *association_name* param 
+@example
+    puts CloudHelp::TicketWorkflowAssignments.detailed_info(current_user.account, 'ticket_type')
+    # Will print something similar to 
+    #[
+    #    {
+    #        "id":7,                                     "created_at":"2020-01-22T18:04:14.247Z",
+    #        "updated_at":"2020-01-22T18:04:14.247Z",    "cloud_help_ticket_types_id":3,
+    #        "cloud_help_ticket_categories_id":4,        "cloud_help_ticket_workflows_id":2,
+    #        "cloud_help_accounts_id":1,                 "ticket_type_name":"Issue",
+    #        "workflow_name":"Dummy Workflow"
+    #    },{
+    #        "id":8,                                     "created_at":"2020-01-22T18:04:14.250Z",
+    #        "updated_at":"2020-01-22T18:04:14.250Z",    "cloud_help_ticket_types_id":3,
+    #        "cloud_help_ticket_categories_id":5,        "cloud_help_ticket_workflows_id":2,
+    #        "cloud_help_accounts_id":1,                 "ticket_type_name":"Issue",
+    #        "workflow_name":"Dummy Workflow"
+    #    },{
+    #        "id":9,                                     "created_at":"2020-01-22T18:04:14.253Z",
+    #        "updated_at":"2020-01-22T18:04:14.253Z",    "cloud_help_ticket_types_id":3,
+    #        "cloud_help_ticket_categories_id":6,        "cloud_help_ticket_workflows_id":2,
+    #        "cloud_help_accounts_id":1,                 "ticket_type_name":"Issue",
+    #        "workflow_name":"Dummy Workflow"
+    #    }
+    #]
+=end
+        def self.detailed_info(account, association_name, association_id)
+            module_name = dynamic_info[:module_name]
+
+            workflow_assignments = self.where(
+                "cloud_#{module_name}_accounts_id".to_sym => account.id,
+                "cloud_#{module_name}_#{association_name.pluralize}_id".to_sym => association_id
+            ).order(
+                id: :asc
+            )
+            responses = []
+
+            workflow_assignments.each do |assignment|
+
+                response = assignment.attributes.merge({
+                    workflow_name: assignment.workflow.name
+                })
+                self.associations.each do |association|
+                    next if association[:name] == association_name
+
+                    associated_record = association[:class].constantize.find(assignment[association[:key]])
+                    responses.push(
+                        response.merge({
+                            "#{association[:name]}_#{association[:identifier]}" => associated_record[association[:identifier]]
+                        })
+                    )
+                end
+            end
+
+            responses
+        end
+
+=begin
+@return [Array] Information about the associations of a workflow to other tables
+@description Returns a list of hashes, each Hash contains information about one association of a workflow.
+    All hashes must contain a *name* (String), representing the name to which the belongs_to association is made,
+    a *class* (String), that represents the full class to which this association belongs to, a *key* (Symbol)
+    that represents the name of the foreign key field in the WorkflowAssociation model, and an *identifier* field,
+    which is a field in the associated table that identifies that record from all others (since the id is not very
+    intuitive)
+@example
+    # Imagine the current class is an instance of CloudHelp::TicketWorkflowAssignment < CloudObject::WorkflowAssignment
+    puts self.associations.to_json
+    # Will display
+    #    [
+    #        {
+    #            "name": "ticket_category",
+    #            "class": "CloudHelp::TicketCategory",
+    #            "key": : "cloud_help_ticket_categories_id",
+    #            "identifier": "name"
+    #        },{
+    #            "name": "ticket_type",
+    #            "class": "CloudHelp::TicketType",
+    #            "key': "cloud_help_ticket_types_id",
+    #            "identifier": "name"
+    #        }
+    #    ]
+=end
+        def self.associations
+            []
+        end
+
+        private
+
+=begin
+@return [Hash] Hash that contains information about the class
+@description Returns dynamic information based on the current implementation of this abstract class
+@example
+    # Imagine the current class is an instance of CloudHelp::TicketWorkflowAssignmentsController < CloudObject::WorkflowAssignmentsController
+    info = dynamic_info
+    puts info[:module_name] # will print 'help'
+    puts info[:object_name] # will print 'ticket_workflow_assignment'
+=end
+        def self.dynamic_info
+            module_info = self.name.split("::")
+            cloud_object_name = module_info[1].sub("WorkflowAssignment", "")
+            {
+                module_name: module_info[0].sub("Cloud", "").downcase,
+                object_name: "#{cloud_object_name.downcase}_workflow_assignment"
+            }
+        end
     end
 end
     
