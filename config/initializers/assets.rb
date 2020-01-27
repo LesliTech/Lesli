32de1a9157cdef58053f16e7f4e48ce5815bdd69
Luis Donis <ldonis.emc2@gmail.com>
2020-01-26 19:12:32 -0600

    add assets for CloudTeam

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,12 +10,17 @@ Rails.application.config.assets.paths << Rails.root.join('vendor')
 Rails.application.config.assets.precompile += %w( websites.css )
 Rails.application.config.assets.precompile += %w( users/*.css )
 Rails.application.config.assets.precompile += %w( dashboards.css )
+Rails.application.config.assets.precompile += %w( cloud_team/*.css )
 Rails.application.config.assets.precompile += %w( cloud_house/*.css )
+Rails.application.config.assets.precompile += %w( cloud_babel/*.css )
+
 
 Rails.application.config.assets.precompile += %w( websites/*.js )
 Rails.application.config.assets.precompile += %w( users/*.js )
 Rails.application.config.assets.precompile += %w( dashboards/app.js )
+Rails.application.config.assets.precompile += %w( cloud_team/*.js )
 Rails.application.config.assets.precompile += %w( cloud_house/*.js )
+Rails.application.config.assets.precompile += %w( cloud_babel/*.js )
 
 
 Rails.application.config.assets.precompile += %w( i18n.js )
@@ -26,12 +31,12 @@ Rails.application.config.assets.precompile += %w( accounts.css )
 Rails.application.config.assets.precompile += %w( dashboards.css )
 Rails.application.config.assets.precompile += %w( users/*.css users/**/*.css )
 Rails.application.config.assets.precompile += %w( cloud_driver/*.css )
-Rails.application.config.assets.precompile += %w( cloud_babel/*.css )
+
 Rails.application.config.assets.precompile += %w( cloud_lesli/*.css )
 Rails.application.config.assets.precompile += %w( cloud_books/*.css )
 
 Rails.application.config.assets.precompile += %w( cloud_panel/*.css )
-Rails.application.config.assets.precompile += %w( cloud_team/*.css )
+
 Rails.application.config.assets.precompile += %w( cloud_help/*.css )
 Rails.application.config.assets.precompile += %w( cloud_lock/*.css )
 Rails.application.config.assets.precompile += %w( cloud_bell/*.css )
@@ -46,12 +51,12 @@ Rails.application.config.assets.precompile += %w( websites_landing.js )
 Rails.application.config.assets.precompile += %w( accounts_new.js )
 Rails.application.config.assets.precompile += %w( users/*.js )
 Rails.application.config.assets.precompile += %w( cloud_driver/*.js )
-Rails.application.config.assets.precompile += %w( cloud_babel/*.js )
+
 Rails.application.config.assets.precompile += %w( cloud_lesli/*.js )
 Rails.application.config.assets.precompile += %w( cloud_books/*.js )
 
 Rails.application.config.assets.precompile += %w( cloud_panel/*.js )
-Rails.application.config.assets.precompile += %w( cloud_team/*.js )
+
 Rails.application.config.assets.precompile += %w( cloud_bell/*.js )
 Rails.application.config.assets.precompile += %w( cloud_help/*.js )
 Rails.application.config.assets.precompile += %w( cloud_lock/*.js )
