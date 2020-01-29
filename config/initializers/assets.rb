e10ecc2b65d800b6d4f89a4b6848aa69f54ea755
Luis Donis <ldonis.emc2@gmail.com>
2020-01-29 11:58:47 -0600

    enable CloudLesli

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( users/*.css )
 Rails.application.config.assets.precompile += %w( cloud_lock/*.css )
 Rails.application.config.assets.precompile += %w( cloud_team/*.css )
 Rails.application.config.assets.precompile += %w( cloud_help/*.css )
+Rails.application.config.assets.precompile += %w( cloud_lesli/*.css )
 Rails.application.config.assets.precompile += %w( cloud_house/*.css )
 Rails.application.config.assets.precompile += %w( cloud_babel/*.css )
 Rails.application.config.assets.precompile += %w( cloud_driver/*.css )
@@ -28,6 +29,7 @@ Rails.application.config.assets.precompile += %w( users/*.js )
 Rails.application.config.assets.precompile += %w( cloud_lock/*.js )
 Rails.application.config.assets.precompile += %w( cloud_team/*.js )
 Rails.application.config.assets.precompile += %w( cloud_help/*.js )
+Rails.application.config.assets.precompile += %w( cloud_lesli/*.js )
 Rails.application.config.assets.precompile += %w( cloud_house/*.js )
 Rails.application.config.assets.precompile += %w( cloud_babel/*.js )
 Rails.application.config.assets.precompile += %w( cloud_driver/*.js )
@@ -41,7 +43,7 @@ Rails.application.config.assets.precompile += %w( dashboards.css )
 Rails.application.config.assets.precompile += %w( users/*.css users/**/*.css )
 
 
-Rails.application.config.assets.precompile += %w( cloud_lesli/*.css )
+
 Rails.application.config.assets.precompile += %w( cloud_books/*.css )
 
 Rails.application.config.assets.precompile += %w( cloud_panel/*.css )
@@ -61,7 +63,7 @@ Rails.application.config.assets.precompile += %w( accounts_new.js )
 Rails.application.config.assets.precompile += %w( users/*.js )
 
 
-Rails.application.config.assets.precompile += %w( cloud_lesli/*.js )
+
 Rails.application.config.assets.precompile += %w( cloud_books/*.js )
 
 Rails.application.config.assets.precompile += %w( cloud_panel/*.js )
