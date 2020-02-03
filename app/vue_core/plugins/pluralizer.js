fd060e5b749b65f12458ad17b254cc9bb2c8deae
volant360 <efrainh12@galileo.edu>
2020-02-03 08:45:46 -0600

    add trello/967 Pluralizer plugin

---
+++ app/vue_core/plugins/pluralizer.js
@@ -0,0 +1,47 @@
+/*
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+
+// · Plugin initializing 
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+export default {
+
+    install (Vue) {
+    
+        Vue.prototype.pluralizer = {
+
+            // · Pluralizes a string, regarding of it's regularity
+            pluralize(string) {
+                if(string.endsWith('y')){
+                    return string.slice(0, -1).concat('ies')
+                }else{
+                    return string.concat('s')
+                }
+            }
+        }
+
+    }
+    
+}
