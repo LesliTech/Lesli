fcc71880bfcc947e045a651a8bbfe72f98f73c90
The Lesli Development Team <hello@lesli.tech>
2020-02-05 11:15:40 -0600

    add trello/969 updated templates and generated erb and scss files

--- lib/generators/vue_app/vue_app_generator.rb
+++ lib/generators/vue_app/vue_app_generator.rb
@@ -2,39 +2,66 @@ class VueAppGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
     argument :model, type: :string
 
-    def create_main_app
-        engine_data = parse_engine_data
-        app_data = parse_app_path
+    def parse_app_name
+        @engine_data = parse_engine_data
+        @app_data = parse_app_path
+    end
 
-        destination_main_app_path = "#{engine_data[:base_path]}/app/vue/#{app_data[:path]}/app.js"
-
-        # Copying main app
-        copy_file("app_js.template", destination_main_app_path)
-        gsub_file(destination_main_app_path, "%engine%", engine_data[:name])
-        gsub_file(destination_main_app_path, "%app_route%", app_data[:route])
-
-        # Copying list|new|show|edit apps
-        ["list", "new", "edit", "show"].each do |app|
-            destination_app_path = "#{engine_data[:base_path]}/app/vue/#{app_data[:path]}/apps/#{app}.vue"
-
-            copy_file("apps/#{app}_vue.template", destination_app_path)
-            gsub_file(destination_app_path, "%app_route%", app_data[:route])
-            gsub_file(destination_app_path, "%data_resource%", app_data[:data_resource])
-            gsub_file(destination_app_path, "%data_resources%", app_data[:data_resource].pluralize)
-            gsub_file(destination_app_path, "%function_resource%", app_data[:function_resource])
-            gsub_file(destination_app_path, "%function_resources%", app_data[:function_resource].pluralize)
+    def create_erb_files
+        destination_path = "#{@engine_data[:base_path]}/app/views/cloud_#{@app_data[:route][1..-1]}"
+        if @engine_data[:name] == "Core"
+            destination_path = "#{@engine_data[:base_path]}/app/views#{@app_data[:route]}"
         end
 
-        # Copying form component
-        ["form"].each do |component|
-            destination_component_path = "#{engine_data[:base_path]}/app/vue/#{app_data[:path]}/components/#{component}.vue"
+        ["index", "new", "edit", "show"].each do |view|
+            copy_file("views/view_html_erb.template", "#{destination_path}/#{view}.html.erb")
+        end
+    end
 
-            copy_file("components/#{component}_vue.template", destination_component_path)
-            gsub_file(destination_component_path, "%app_route%", app_data[:route])
-            gsub_file(destination_component_path, "%data_resource%", app_data[:data_resource])
-            gsub_file(destination_component_path, "%data_resources%", app_data[:data_resource].pluralize)
-            gsub_file(destination_component_path, "%function_resource%", app_data[:function_resource])
-            gsub_file(destination_component_path, "%function_resources%", app_data[:function_resource].pluralize)
+    def create_scss_files
+        destination_path = "#{@engine_data[:base_path]}/app/assets/stylesheets/cloud_#{@app_data[:route][1..-1]}.scss"
+        if @engine_data[:name] == "Core"
+            destination_path = "#{@engine_data[:base_path]}/app/assets/stylesheets#{@app_data[:route]}.scss"
+        end
+
+        copy_file("assets/stylesheet_css.template", destination_path)
+    end
+    
+    def create_main_app
+        destination_path = "#{@engine_data[:base_path]}/app/vue/#{@app_data[:path]}/app.js"
+
+        copy_file("app_js.template", destination_path)
+        gsub_file(destination_path, "%engine%", @engine_data[:name])
+        gsub_file(destination_path, "%app_route%", @app_data[:route])
+    end
+
+    def create_apps
+        ["list", "new", "edit", "show"].each do |app|
+            destination_path = "#{@engine_data[:base_path]}/app/vue/#{@app_data[:path]}/apps/#{app}.vue"
+
+            copy_file("apps/#{app}_vue.template", destination_path)
+            gsub_file(destination_path, "%app_route%", @app_data[:route])
+            gsub_file(destination_path, "%underscore_resource%", @app_data[:underscore_resource])
+            gsub_file(destination_path, "%underscore_resources%", @app_data[:underscore_resource].pluralize)
+            gsub_file(destination_path, "%camel_case_resource%", @app_data[:camel_case_resource])
+            gsub_file(destination_path, "%camel_case_resources%", @app_data[:camel_case_resource].pluralize)
+            gsub_file(destination_path, "%snake_case_resource%", @app_data[:snake_case_resource])
+            gsub_file(destination_path, "%humanized_resource%", @app_data[:humanized_resource])
+        end
+    end
+
+    def create_components
+        ["form"].each do |component|
+            destination_path = "#{@engine_data[:base_path]}/app/vue/#{@app_data[:path]}/components/#{component}.vue"
+
+            copy_file("components/#{component}_vue.template", destination_path)
+            gsub_file(destination_path, "%app_route%", @app_data[:route])
+            gsub_file(destination_path, "%underscore_resource%", @app_data[:underscore_resource])
+            gsub_file(destination_path, "%underscore_resources%", @app_data[:underscore_resource].pluralize)
+            gsub_file(destination_path, "%camel_case_resource%", @app_data[:camel_case_resource])
+            gsub_file(destination_path, "%camel_case_resources%", @app_data[:camel_case_resource].pluralize)
+            gsub_file(destination_path, "%snake_case_resource%", @app_data[:snake_case_resource])
+            gsub_file(destination_path, "%humanized_resource%", @app_data[:humanized_resource])
         end
     end
 
@@ -55,39 +82,43 @@ class VueAppGenerator < Rails::Generators::Base
 
     def parse_app_path
         route = ""
-        data_resource = []
-        function_resource = []
+        underscore_resource = []
+        camel_case_resource = []
         
         name_data = model.split("::")
-
         engine = name_data.shift
-        if engine != "Core"
-            route = engine.gsub("Cloud","").downcase
-        end
-
-
 
         name_data = name_data.each_with_index.map do |string, index|
-            function_resource.push(string)
-            string = string.downcase
-            data_resource.push(string)
+            if (index + 1) == name_data.length
+                camel_case_resource.push(string)
+            end
+            string = string.underscore
             
             if (index + 1) == name_data.length
+                underscore_resource.push(string)
                 string = string.pluralize
             end
 
             route += "/#{string}"
             string
         end
+
+        if engine != "Core"
+            route = "/#{engine.gsub("Cloud","").downcase}#{route}" 
+        end
         
-        function_resource = function_resource.join("")
-        data_resource = data_resource.join("_")
+        camel_case_resource = camel_case_resource.join("")
+        underscore_resource = underscore_resource.join("_")
+        snake_case_resource = underscore_resource.gsub("_", "-")
+        humanized_resource = underscore_resource.humanize
 
         {
             path: name_data.join("_"),
             route: route,
-            data_resource: data_resource,
-            function_resource: function_resource
+            underscore_resource: underscore_resource,
+            camel_case_resource: camel_case_resource,
+            snake_case_resource: snake_case_resource,
+            humanized_resource: humanized_resource
         }
         
     end
