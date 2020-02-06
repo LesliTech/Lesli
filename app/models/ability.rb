96fbad77a146277cdeafef0eaabb74c6d5f79ae6
Luis Donis <ldonis.emc2@gmail.com>
2020-02-06 10:22:27 -0600

    temporary disable abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,50 +17,47 @@ LesliCloud - Your Smart Business Assistant
 Powered by https://www.lesli.tech
 Building a better future, one line of code at a time.
 
-@author   Luis Davila
 @license  Propietary - all rights reserved.
 @version  0.1.0-alpha
-@description Model in charge of roles and privileges
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
 
 =end
 
 class Ability
     include CanCan::Ability
 
-=begin
-initialize(user)
-@description Return everything that each role can do with its privileges
+    def initialize2(user)
 
-=end
-    def initialize(user)
+        user.role.role_privileges.each do |privilege|
+            module_name = privilege.privilege_object_name.gsub('/', '::',)
+            module_name = module_name.gsub(/\b('?[a-z])/) { $1.capitalize }
+            module_name = module_name.split('::').map{ |s| s.singularize }.join '::'
+            module_name = module_name.remove("Core::").camelize
 
-            user.role.role_privileges.each do |privilege|
-                module_name = privilege.privilege_object_name.gsub('/', '::',)
-                module_name = module_name.gsub(/\b('?[a-z])/) { $1.capitalize }
-                module_name = module_name.split('::').map{ |s| s.singularize }.join '::'
-                module_name = module_name.remove("Core::").camelize
-
-                begin
-                    Module.const_get(module_name) #Verify that the models exist
-                    module_name = module_name.constantize 
-                rescue NameError
-                    nil
-                end
-                # Everything that each privilege can do
-                can [:index], module_name  if privilege.privilege_index == true 
-                can [:show], module_name  if privilege.privilege_show == true 
-                can [:create], module_name  if privilege.privilege_create == true 
-                can [:new], module_name  if privilege.privilege_new == true 
-                can [:edit], module_name  if privilege.privilege_edit == true 
-                can [:update], module_name  if privilege.privilege_update == true
-                can [:destroy], module_name  if privilege.privilege_destroy == true
-                can [:default], module_name  if privilege.privilege_default == true
-                can [:empty], module_name  if privilege.privilege_empty == true
-                # All the options in each engine
-                can [:social_account_options, :details_options, :ticket_options, :workflow_options, 
-                    :workflow_options, :assignment_options], module_name  if privilege.privilege_options == true
-
+            begin
+                Module.const_get(module_name) #Verify that the models exist
+                module_name = module_name.constantize 
+            rescue NameError
+                nil
             end
+
+            # Everything that each privilege can do
+            can [:index], module_name  if privilege.privilege_index == true 
+            can [:show], module_name  if privilege.privilege_show == true 
+            can [:create], module_name  if privilege.privilege_create == true 
+            can [:new], module_name  if privilege.privilege_new == true 
+            can [:edit], module_name  if privilege.privilege_edit == true 
+            can [:update], module_name  if privilege.privilege_update == true
+            can [:destroy], module_name  if privilege.privilege_destroy == true
+            can [:default], module_name  if privilege.privilege_default == true
+            can [:empty], module_name  if privilege.privilege_empty == true
+            # All the options in each engine
+            can [:social_account_options, :details_options, :ticket_options, :workflow_options, 
+                :workflow_options, :assignment_options], module_name  if privilege.privilege_options == true
+
+        end
     end
 
 end
