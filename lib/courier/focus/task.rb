788ae782b5023b81a48cf7ccb2f4bd67add42c3b
Luis Donis <ldonis.emc2@gmail.com>
2020-02-18 13:39:19 -0600

    add focus courier

---
+++ lib/courier/focus/task.rb
@@ -0,0 +1,44 @@
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+module Courier
+    module Focus
+        class Task
+
+            def self.for(current_user)
+            end
+
+            def self.for_with_deadline(current_user)
+                return unless defined? CloudFocus
+                current_user.account.focus.tasks.joins(:detail)
+                .select(:id, :title, :description, :deadline)
+                .where("cloud_focus_task_details.deadline is not null")
+            end
+            
+        end
+    end
+end
