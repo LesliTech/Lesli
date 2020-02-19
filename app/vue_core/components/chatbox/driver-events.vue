8456784903e802b3143a51e79833c6af6e2ccf67
Luis Donis <ldonis.emc2@gmail.com>
2020-02-19 13:42:50 -0600

    add chatbot component driver-events

---
+++ app/vue_core/components/chatbox/driver-events.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    props: {
+        data: {}
+    },
+    data() {
+        return {
+            events: []
+        }
+    },
+    mounted() {
+        this.http.get("driver/calendars/default.json").then(result => {
+            this.events =  result.data.focus_tasks
+        })
+    }
+}
+</script>
+<template>
+   <ul>
+       <li v-for="event in events" :key="event.id">
+           {{ event.title }} due {{ event.end }}
+       </li>
+   </ul>
+</template>
