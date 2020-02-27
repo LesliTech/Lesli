f933b5db33e740ec7ed783576f3744ead38c8a48
Luis Donis <ldonis.emc2@gmail.com>
2020-02-26 18:19:05 -0600

    add routes per company

---
+++ config/routes/routes_haus.rb
@@ -0,0 +1,45 @@
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+module RoutesHaus
+    def self.extended(router)
+        router.instance_exec do
+            mount CloudHaus::Engine  => "/crm"
+            mount CloudHouse::Engine  => "/house"
+            mount CloudFocus::Engine  => "/tasks" if defined?(CloudFocus)
+            mount CloudDriver::Engine => "/calendar" if defined?(CloudDriver)
+            
+
+            authenticated :user do
+                root to: redirect('/crm'),   as: :root_cloud_haus_authenticated
+            end
+
+            root to: redirect('/login'), as: :root_login_unauthenticated
+
+        end
+    end
+end
