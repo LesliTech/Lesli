34dbf2253d8eb9406b5568d913ad483de2ad30fb
Luis Donis <ldonis.emc2@gmail.com>
2020-02-27 14:12:49 -0600

    implement CloudFocus

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -33,6 +33,11 @@ if defined?(CloudDriver)
     Rails.application.config.assets.precompile += %w( cloud_driver/*.css )
 end
 
+if defined?(CloudFocus)
+    Rails.application.config.assets.precompile += %w( cloud_focus/*.js )
+    Rails.application.config.assets.precompile += %w( cloud_focus/*.css )
+end
+
 =begin
 
 Rails.application.config.assets.precompile += %w( cloud_kb/*.css )
@@ -42,7 +47,6 @@ Rails.application.config.assets.precompile += %w( cloud_team/*.css )
 Rails.application.config.assets.precompile += %w( cloud_help/*.css )
 Rails.application.config.assets.precompile += %w( cloud_house/*.css )
 Rails.application.config.assets.precompile += %w( cloud_lesli/*.css )
-Rails.application.config.assets.precompile += %w( cloud_focus/*.css )
 Rails.application.config.assets.precompile += %w( cloud_babel/*.css )
 
 Rails.application.config.assets.precompile += %w( cloud_kb/*.js )
@@ -52,7 +56,6 @@ Rails.application.config.assets.precompile += %w( cloud_team/*.js )
 Rails.application.config.assets.precompile += %w( cloud_help/*.js )
 Rails.application.config.assets.precompile += %w( cloud_house/*.js )
 Rails.application.config.assets.precompile += %w( cloud_lesli/*.js )
-Rails.application.config.assets.precompile += %w( cloud_focus/*.js )
 Rails.application.config.assets.precompile += %w( cloud_babel/*.js )
 
 =end
