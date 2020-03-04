0209d065b7de5b6b57d68594d0b7aceabbe259d1
Luis Donis <ldonis.emc2@gmail.com>
2020-03-04 13:11:55 -0600

    enable settings management

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,58 +1,101 @@
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+LesliCloud - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
 class SettingsController < ApplicationController
-  before_action :set_setting, only: [:show, :edit, :update, :destroy]
+    before_action :set_setting, only: [:show, :edit, :update, :destroy]
 
-  # GET /settings
-  def index
-    @settings = Setting.all
-  end
-
-  # GET /settings/1
-  def show
-  end
-
-  # GET /settings/new
-  def new
-    @setting = Setting.new
-  end
-
-  # GET /settings/1/edit
-  def edit
-  end
-
-  # POST /settings
-  def create
-    @setting = Setting.new(setting_params)
-
-    if @setting.save
-      redirect_to @setting, notice: 'Setting was successfully created.'
-    else
-      render :new
+    # GET /settings
+    def index
+        
     end
-  end
 
-  # PATCH/PUT /settings/1
-  def update
-    if @setting.update(setting_params)
-      redirect_to @setting, notice: 'Setting was successfully updated.'
-    else
-      render :edit
+    # GET /settings/1
+    def show
+        respond_to do |format|
+            format.json {
+                responseWithSuccessful(@setting)
+            }
+        end
     end
-  end
 
-  # DELETE /settings/1
-  def destroy
-    @setting.destroy
-    redirect_to settings_url, notice: 'Setting was successfully destroyed.'
-  end
+    # GET /settings/new
+    def new
+    end
 
-  private
+    # GET /settings/1/edit
+    def edit
+    end
+
+    # POST /settings
+    def create
+
+        # check if setting exists
+        setting = Setting.find_by(name: setting_params[:name])
+
+        # update settings if exists
+        if setting
+            if setting.update(setting_params)
+                responseWithSuccessful(setting)
+            else
+                responseWithError("Error on create settings", setting.errors)
+            end
+        end
+        
+        # create settings if does not exist
+        if setting.blank?
+            setting = current_user.account.settings.new(setting_params)
+            if setting.save
+                responseWithSuccessful(setting)
+            else
+                responseWithError("Error on create settings", setting.errors) 
+            end
+        end
+
+        
+    end
+
+    # PATCH/PUT /settings/1
+    def update
+
+    end
+
+    # DELETE /settings/1
+    def destroy
+
+    end
+
+    private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_setting
-      @setting = Setting.find(params[:id])
+        @setting = Setting.find_by(name: params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def setting_params
-      params.fetch(:setting, {})
+        params.require(:setting).permit(:name, :value)
     end
 end
