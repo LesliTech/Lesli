e66717cfbc8b245ccf6e77af2e06b2264db3cece
Luis Donis <ldonis.emc2@gmail.com>
2020-03-23 14:04:59 -0600

    move from LesliCloud to Lesli

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module LesliCloud
-
+module Lesli
     class Application < Rails::Application
-
+        
         # Initialize configuration defaults for originally generated Rails version.
         config.load_defaults 6.0
 
@@ -31,5 +30,4 @@ module LesliCloud
         config.autoload_paths << Rails.root.join('lib')
 
     end
-
 end
