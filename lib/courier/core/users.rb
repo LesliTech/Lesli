7f388028802b6fa78c130662e68d790310d7d60f
Luis Donis <ldonis.emc2@gmail.com>
2020-03-23 22:24:19 -0600

    add datetime api - proof of concept

--- lib/courier/core/users.rb
+++ lib/courier/core/users.rb
@@ -1,6 +1,35 @@
+=begin
+
+Lesli
+
+Copyright (c) 2020, Lesli Technologies, S. A.
+
+All the information provided by this website is protected by laws of Guatemala related 
+to industrial property, intellectual property, copyright and relative international laws. 
+Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
+rights of the code, texts, trade mark, design, pictures and any other information.
+Without the written permission of Lesli Technologies, S. A., any replication, modification,
+transmission, publication is strictly forbidden.
+For more information read the license file including with this software.
+
+Lesli - Your Smart Business Assistant
+
+Powered by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@license  Propietary - all rights reserved.
+@version  0.1.0-alpha
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
 module Courier
+
     module Core
+
         class Users
+
             def self.list()
 
                 employees = []
@@ -28,6 +57,9 @@ module Courier
                     return ::User.select(:id, :email, :created_at).find(id)
                 end
             end
+
         end
+
     end
+    
 end
