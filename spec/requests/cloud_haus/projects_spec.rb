0fb8ffeee126946ee1f9f18d223f9299da750aac
FR98 <frosal@lomax.com.gt>
2020-03-28 18:07:56 -0600

    Add Projects tests

--- spec/requests/cloud_haus/projects_spec.rb
+++ spec/requests/cloud_haus/projects_spec.rb
@@ -17,12 +17,12 @@ RSpec.describe "CloudHaus::Projects", type: :request do
         end
     end
 
-    def create_project(account=nil)
+    def create_project(account = nil)
         account = @account unless account
         workflow = CloudHouse::Workflow.create!(
+            account: account,
             name:  Faker::Verb.base,
-            default: true,
-            account: account
+            default: true
         )
         workflow_status = CloudHouse::Workflow::Status.create!(
             name: Faker::Verb.base,
@@ -33,8 +33,8 @@ RSpec.describe "CloudHaus::Projects", type: :request do
             status: workflow_status
         )
         company_category = CloudHouse::Catalog::CompanyCategory.create!(
-            name: Faker::Verb.base,
-            account: account
+            account: account,
+            name: Faker::Verb.base
         )
         company = CloudHouse::Company.create!(
             account: account,
@@ -49,10 +49,42 @@ RSpec.describe "CloudHaus::Projects", type: :request do
             account: account,
             status: workflow_status,
             property: property,
-            main_employee: @user,
-            detail_attributes: {}
+            main_employee: @user
         )
         return project
+    end
+
+    def create_project_props
+        workflow = CloudHouse::Workflow.create!(
+            account: @account,
+            name:  Faker::Verb.base,
+            default: true
+        )
+        @workflow_status = CloudHouse::Workflow::Status.create!(
+            name: Faker::Verb.base,
+            workflow: workflow
+        )
+        @property = CloudHouse::Property.create!(
+            account: @account,
+            status: @workflow_status
+        )
+        company_category = CloudHouse::Catalog::CompanyCategory.create!(
+            account: @account,
+            name: Faker::Verb.base
+        )
+        company = CloudHouse::Company.create!(
+            account: @account,
+            status: @workflow_status,
+            company_category: company_category
+        )
+        @main_employee = CloudHouse::Employee.create!(
+            account: @account,
+            company: company
+        )
+        @catalog_project_type = CloudHouse::Catalog::ProjectType.create!(
+            account: @account,
+            name: Faker::Verb.base
+        )
     end
 
     def json_headers
@@ -84,6 +116,7 @@ RSpec.describe "CloudHaus::Projects", type: :request do
             expect(response.status).to be 200
             expect(response.headers["Content-Type"]).to eq "text/html; charset=utf-8"
         end
+
     end
 
 
@@ -100,7 +133,243 @@ RSpec.describe "CloudHaus::Projects", type: :request do
             expect(response.headers["Content-Type"]).to eq "text/html; charset=utf-8"
         end
 
+        # it "test valid request" do
+        #     login_admin
+        #     create_account
+
+        #     project = create_project
+        #     puts project.detail
+
+        #     get "/crm/projects/#{project.id}.json"
+        #     response_json = JSON.parse(response.body)
+
+        #     expect(response.status).to be 200
+        #     expect(response.headers["Content-Type"]).to eq "application/json; charset=utf-8"
+        #     expect(response_json["successful"]).to be true
+        # end
+
+        it "test invalid request (non-existent projects)" do
+            login_admin
+            create_account
+
+            get "/crm/projects/#{Faker::Number.number(digits: 6)}.json"
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 404
+        end
+
+        it "test invalid request (requesting projects from another account)" do
+          login_admin
+          create_account
+
+          other_account = Account.create!(id: Account.order(id: :asc).last.id + 1)
+          project = create_project(other_account.house)
+
+          get "/crm/projects/#{project.id}.json"
+          response_json = JSON.parse(response.body)
+
+          expect(response.status).to be 404
+      end
+
     end
 
+
+    describe "NEW integration test" do
+        
+        it "test http html response" do
+            login_admin
+
+            get "/crm/projects/new.html"
+
+            expect(response.status).to be 200
+            expect(response.headers["Content-Type"]).to eq "text/html; charset=utf-8"
+        end
+
+    end
+
+
+    describe "EDIT integration test " do
+
+        it "test http html response" do
+            login_admin
+
+            get "/crm/projects/edit.html"
+
+            expect(response.status).to be 200
+            expect(response.headers["Content-Type"]).to eq "text/html; charset=utf-8"
+        end
+
+    end
+
+
+    describe "CREATE integration test" do
+
+        # it "test valid request" do
+        #     login_admin
+        #     create_account
+        #     create_project_props
+
+        #     request_json = {
+        #         project: {
+        #             main_employee: @main_employee.id,
+        #             cloud_house_accounts_id: @account.id,
+        #             cloud_house_catalog_project_types_id: @catalog_project_type.id,
+        #             cloud_house_properties_id: @property.id,
+        #             cloud_house_workflow_statuses_id: @workflow_status.id
+        #         }
+        #     }.to_json
+        #     headers = json_headers
+
+        #     post "/crm/projects", params: request_json, headers: headers
+        #     response_json = JSON.parse(response.body)
+            
+        #     expect(response.status).to be 200
+        #     expect(response.headers["Content-Type"]).to eq "application/json; charset=utf-8"
+        #     expect(response_json["successful"]).to be true
+        # end
+
+        it "test invalid request (no props)" do
+            login_admin
+            create_account
+
+            request_json = {
+                project: {
+                    main_employee: nil,
+                    cloud_house_accounts_id: @account.id,
+                    cloud_house_catalog_project_types_id: nil,
+                    cloud_house_properties_id: nil,
+                    cloud_house_workflow_statuses_id: nil
+                }
+            }.to_json
+            headers = json_headers
+
+            post "/crm/projects", params: request_json, headers: headers
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 200
+            expect(response.headers["Content-Type"]).to eq "application/json; charset=utf-8"
+            expect(response_json["successful"]).to be false
+        end
+        
+    end
+
+
+    describe "UPDATE integration test" do
+
+        it "test valid request" do
+            login_admin
+            create_account
+            create_project_props
+            project = create_project
+
+            request_json = {
+                project: {
+                    cloud_house_workflow_statuses_id: @workflow_status.id
+                }
+            }.to_json
+            headers = json_headers
+
+            put "/crm/projects/#{project.id}", params: request_json, headers: headers
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 200
+            expect(response.headers["Content-Type"]).to eq "application/json; charset=utf-8"
+            expect(response_json["successful"]).to be true
+        end
+
+        it "test invalid request (workflow status changed to nil)" do
+            login_admin
+            create_account
+            # create_project_props
+            project = create_project
+
+            request_json = {
+                project: {
+                    cloud_house_workflow_statuses_id: nil
+                }
+            }.to_json
+            headers = json_headers
+
+            put "/crm/projects/#{project.id}", params: request_json, headers: headers
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 200
+            expect(response.headers["Content-Type"]).to eq "application/json; charset=utf-8"
+            expect(response_json["successful"]).to be false
+        end
+
+        it "test invalid request (non-existent type)" do
+            login_admin
+            create_account
+
+            request_json = {
+                name: Faker::Verb.base
+            }.to_json
+            headers = json_headers
+
+            put "/crm/projects/#{Faker::Number.number(digits: 10)}", params: request_json, headers: headers
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 404
+        end
+
+        it "test invalid request (updating project from another account)" do
+            login_admin
+            create_account
+            create_project_props
+            other_account = Account.create!(id: Account.order(id: :asc).last.id + 1)
+            project = create_project(other_account.house)
+
+            request_json = {
+                project: {
+                    cloud_house_workflow_statuses_id: @workflow_status.id
+                }
+            }.to_json
+            headers = json_headers
+
+            put "/crm/projects/#{project.id}", params: request_json, headers: headers
+
+            expect(response.status).to be 404
+        end
+
+    end
+
+    describe "DESTROY integration test" do
+
+        # it "test valid request" do
+        #     login_admin
+        #     create_account
+        #     project = create_project
+
+        #     delete "/crm/projects/#{project.id}"
+        #     response_json = JSON.parse(response.body)
+
+        #     expect(response.status).to be 200
+        #     expect(response_json["successful"]).to be true
+        # end
+
+        it "test invalid request (non-existent project)" do
+            login_admin
+            create_account
+
+            delete "/crm/projects/#{Faker::Number.number(digits: 10)}"
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 404
+        end
+
+        it "test invalid request (deleting project from another account)" do
+            login_admin
+            create_account
+            other_account = Account.create!(id: Account.order(id: :asc).last.id + 1)
+            project = create_project(other_account.house)
+
+            delete "/house/projects/#{project.id}"
+            response_json = JSON.parse(response.body)
+
+            expect(response.status).to be 404
+        end
+
+    end
 
 end
