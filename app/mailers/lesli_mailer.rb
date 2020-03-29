447c9d47e8eaa51a704884b26825e82d10f26bd7
Luis Donis <ldonis.emc2@gmail.com>
2020-03-28 21:39:21 -0600

    add global mailer

---
+++ app/mailers/lesli_mailer.rb
@@ -0,0 +1,22 @@
+class LesliMailer < ApplicationMailer
+    def email
+
+        mail(
+            to: "ldonis@lomax.com.gt",
+            subject: "Lesli Cloud Notification: @subject",
+            template_path: 'cloud_haus/mailers'
+        )
+
+        #mail(
+        #    to: 'marketing@deutsche-leibrenten.de',
+        #    cc: current_kop.email,
+        #    bcc: 'ldonis@lomax.com.gt',
+        #    from: "Deutsche Leibrenten CRM <no-reply@deutsche-leibrente.de>", 
+        #    reply_to: "#{current_kop.email}",
+        #    subject: 'Bestellformular',
+        #    template_path: 'mailer/kops_mails',
+        #    template_name: 'request_order'
+        #)
+
+    end
+end
