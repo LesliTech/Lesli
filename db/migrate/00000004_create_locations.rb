a87457849f06e65f34f7ac2f105a5ac86f5e4f00
volant360 <efrainh12@galileo.edu>
2020-03-30 15:00:53 -0600

    add trello/1091 some extra fields for locations

--- db/migrate/00000004_create_locations.rb
+++ db/migrate/00000004_create_locations.rb
@@ -2,11 +2,18 @@ class CreateLocations < ActiveRecord::Migration[6.0]
     def change
         create_table :locations do |t|
 
+            # General information acout the location
+            t.string        :name
+            t.string        :short_name
+            t.string        :postal_code
+            t.string        :latitude
+            t.string        :longitude
+            t.string        :code
+
             # Imagine USA, we have country > state > city. In guatemala, we have country > departamento > municipio
             # levels for Guatemala, Alta Verapaz, Cobán will be country, state, city
             # native_levels for Guatemala, Alta Verapaz, Cobán will be country, departamento, municipio
             # native_levels are only reference, for relating entries we will use the 'level' field
-            t.string        :name
             t.string        :level          # For example, country, state, city, etc.
             t.string        :native_level   # The original name of the level, based on language and location
             t.string        :parent_id
