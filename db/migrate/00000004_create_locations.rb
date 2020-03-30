4560c3d450e9a7032952302b1d02b8ac6aa7ec7a
volant360 <efrainh12@galileo.edu>
2020-03-30 14:15:02 -0600

    add native_level field

--- db/migrate/00000004_create_locations.rb
+++ db/migrate/00000004_create_locations.rb
@@ -1,8 +1,14 @@
 class CreateLocations < ActiveRecord::Migration[6.0]
     def change
         create_table :locations do |t|
+
+            # Imagine USA, we have country > state > city. In guatemala, we have country > departamento > municipio
+            # level for Guatemala, Alta Verapaz, Cobán will be country, state, city
+            # native_levels for Guatemala, Alta Verapaz, Cobán will be country, departamento, municipio
+            # native_levels are only reference, for relating entries we will use the 'level' field
             t.string        :name
-            t.string        :level      # For example, country, state, city, municipio, etc.
+            t.string        :level          # For example, country, state, city, etc.
+            t.string        :native_level   # The original name of the level, based on language and location
             t.string        :parent_id
 
             t.timestamps
