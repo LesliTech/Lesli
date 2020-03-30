09391e37f4b0a971d07240d414fefeba4a30e994
volant360 <efrainh12@galileo.edu>
2020-03-30 15:35:31 -0600

    add trello/1090 accounts relation to locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,8 @@
 class Location < ApplicationRecord
 
-    has_many :sub_locations,        class_name: "Location", foreign_key: "parent_id"
+    has_many    :sub_locations,     class_name: "Location", foreign_key: "parent_id"
     belongs_to :parent_location,    class_name: "Location", foreign_key: "parent_id", optional: true
+    belongs_to :account,            class_name: "Account",  foreign_key: "accounts_id"
 
     enum level: {
         continent: "continent",
