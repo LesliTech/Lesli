91cdc8cfc57dc318851e8b087cc219d0b5f51061
volant360 <efrainh12@galileo.edu>
2020-03-30 15:28:29 -0600

    add trello/1090 basic levels

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,13 @@
 class Location < ApplicationRecord
+
+    has_many :sub_locations,        class_name: "Location", foreign_key: "parent_id"
+    belongs_to :parent_location,    class_name: "Location", foreign_key: "parent_id", optional: true
+
+    enum level: {
+        continent: "continent",
+        country: "country",
+        state: "state",
+        city: "city",
+        zone: "zone"
+    }
 end
