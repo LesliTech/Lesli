f1779df89809ee1833d668c86f01898179dd5b5a
Luis Donis <ldonis.emc2@gmail.com>
2020-04-03 11:35:07 -0600

    add application mailer

--- app/mailers/lesli_mailer.rb
+++ app/mailers/lesli_mailer.rb
@@ -1,24 +1,9 @@
 class LesliMailer < ApplicationMailer
 
-    def email
+    def welcome(to, variation:"standard")
 
-        mail(
-            to: ["ldonis@lomax.com.gt", "ldonis.emc2@gmail.com"],
-            subject: "Lesli Cloud Notification: @subject",
-            template_path: 'cloud_haus/mailers'
-        )
-
-        #mail(
-        #    to: 'marketing@deutsche-leibrenten.de',
-        #    cc: current_kop.email,
-        #    bcc: 'ldonis@lomax.com.gt',
-        #    from: "Deutsche Leibrenten CRM <no-reply@deutsche-leibrente.de>", 
-        #    reply_to: "#{current_kop.email}",
-        #    subject: 'Bestellformular',
-        #    template_path: 'mailer/kops_mails',
-        #    template_name: 'request_order'
-        #)
-
+        send()
+        
     end
 
 end
---
+++ spec/mailers/previews/bell_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/bell
+class BellPreview < ActionMailer::Preview
+
+end
