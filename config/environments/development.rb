688dbc90399c673146200f2eef3993b5ac6cb9ab
Luis Donis <ldonis.emc2@gmail.com>
2020-04-03 10:03:08 -0600

    fix mailer smtp sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,13 +28,17 @@ Rails.application.configure do
     end
 
     # Don't care if the mailer can't send.
-    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = true
 
     config.action_mailer.perform_caching = true
 
     config.action_mailer.default_url_options = { host: '0.0.0.0:3000' }
 
     config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.default_options = {
+        from: "hello@lesli.tech"
+    }
 
     config.action_mailer.smtp_settings = {
         port: 25,
