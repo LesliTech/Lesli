693b06bae7e2195a113f003f57b0a1ba8904f44b
Luis Donis <ldonis.emc2@gmail.com>
2020-04-14 16:49:02 -0600

    add settings manager by instance

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,11 @@ Rails.application.configure do
 
     config.action_mailer.perform_caching = true
 
-    config.action_mailer.default_url_options = { host: Rails.configuration.defaults["default_url_options_host"] }
+    config.action_mailer.default_url_options = { 
+        host: ""
+    }
 
-    config.action_mailer.asset_host = Rails.configuration.defaults["asset_host"]
+    config.action_mailer.asset_host = ""
 
     #config.action_mailer.delivery_method = :smtp
 
@@ -43,7 +45,7 @@ Rails.application.configure do
     config.action_mailer.perform_deliveries = true
 
     config.action_mailer.default_options = {
-        from: Rails.configuration.defaults["default_options_from"]
+        from: "dev@lesli.cloud"
     }
 
     config.action_mailer.smtp_settings = {
