d36d052b79ab8da2e649eb91105d63d80a02f18c
Luis Donis <ldonis.emc2@gmail.com>
2020-04-27 11:34:35 -0600

    add custom validation - proof of concept

--- app/models/application_lesli_record.rb
+++ app/models/application_lesli_record.rb
@@ -1,3 +1,25 @@
 class ApplicationLesliRecord < ApplicationRecord
     self.abstract_class = true
+
+    before_validation :custom_validation_rules
+
+    def custom_validation_rules
+
+        validations = CloudFocus::CustomValidation.first
+        .validation_rule
+        .validation_fields.where(
+            :model_to_validate => self.class.name,
+            :column_to_validate => CloudFocus::Task::Detail.column_names
+        ).to_a
+
+        validations.each do |validation|
+            if validation.rule.rule_required
+                if self[validation.column_to_validate].blank?
+                    errors.add(validation.column_to_validate, validation.rule.message_error)
+                end
+            end
+        end
+
+    end
+
 end
