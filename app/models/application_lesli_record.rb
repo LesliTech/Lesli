1ee7f46319f78b1e0915a778f7a78bbad11425ec
volant360 <efrainh12@galileo.edu>
2020-04-27 12:30:43 -0600

    added dynamic model access to custom validations

--- app/models/application_lesli_record.rb
+++ app/models/application_lesli_record.rb
@@ -4,12 +4,14 @@ class ApplicationLesliRecord < ApplicationRecord
     before_validation :custom_validation_rules
 
     def custom_validation_rules
+        dynamic_validation_info = self.class.dynamic_validation_info
+        custom_validation_model = dynamic_validation_info[:custom_validation_model]
 
-        validations = CloudFocus::CustomValidation.first
+        validations = custom_validation_model.first
         .validation_rule
         .validation_fields.where(
             :model_to_validate => self.class.name,
-            :column_to_validate => CloudFocus::Task::Detail.column_names
+            :column_to_validate => self.class.column_names
         ).to_a
 
         validations.each do |validation|
@@ -21,5 +23,17 @@ class ApplicationLesliRecord < ApplicationRecord
         end
 
     end
+
+        # @return [Hash] Hash that contains information about the class
+        # @description Returns dynamic information based on the current implementation of this abstract class
+        # @example
+        #     dynamic_validation_info = CloudHelp::Ticket::Detail.dynamic_validation_info
+        #     puts dynamic_validation_info[:custom_validation_model].new # will print a new instance of CloudHelp::CustomValidation
+        def self.dynamic_validation_info
+            module_info = self.name.split("::")
+            {
+                custom_validation_model: "#{module_info[0]}::CustomValidation".constantize
+            }
+        end
 
 end
