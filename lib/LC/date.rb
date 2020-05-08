76a9abe980ada48f37b8955373262a61f19f5396
The Lesli Development Team <hello@lesli.tech>
2020-05-08 17:23:11 -0600

    add trello/1162 settings now load from db

--- lib/LC/date.rb
+++ lib/LC/date.rb
@@ -20,6 +20,7 @@ Building a better future, one line of code at a time.
 @contact  <hello@lesli.tech>
 @website  <https://lesli.tech>
 @license  Propietary - all rights reserved.
+@todo       Separate settings from account
 
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 // · 
@@ -58,107 +59,84 @@ module LC
                 value: "%H:%M"
             }
         ]
+
+        @settings = nil
         
-        # @return [void]
-        # @param account [Account] The account whose settings need to be reset
-        # @description Resets the settings stored in @default_settings associated to the received account
-        # @example
-        #   Account.find(1).settings.find_by(name: "time_zone").update(value: "Guatemala")
-        #   # -- Time zone at this point has not changed at all in LC::Date
-        #   LC::Date.reset_db_settings(Account.find(1))
-        #   # -- Time zone at this point has been refresed and updated to "Guatemala"
-        def self.reset_db_settings(account)
-            account_settings = {}
-            db_settings = account.settings
+        def self.reset_db_settings
+            settings = {}
  
             @default_settings.each do |default_setting|
                 setting_value = default_setting[:value]
-                db_setting = db_settings.find_by(name: default_setting[:name])
+                db_setting = Setting.where(name: default_setting[:name]).order(id: :asc).first
                 if db_setting
                     setting_value = db_setting.value
                 end
  
-                account_settings[default_setting[:name].to_sym] = setting_value
+                settings[default_setting[:name].to_sym] = setting_value
             end
  
-            @accounts_settings[account.id] = account_settings
+            @settings = settings
         end
  
-        # @return [Hash] The settings for this account. If one or more settings are not available in the database, default values are used
-        # @param account [Account] The account whose settings need to be reset
-        # @description Returns the settings associated to this account that are stored in memory. If there are not, it retrieves them from
-        #   the databae keeps them in memory.
-        # @example
-        #   settings = LC::Date.get_settings(Account.find(1))
-        #   puts settings.to_json # will display something like
-        #   # {
-        #   #      time_zome: "Europe/Berlin",
-        #   #     date_format: "%Y.%m.%d",
-        #   #     date_format_full: "%a, %B %d, %Y",
-        #   #     date_format_time: "%Y.%m.%d %H:%M",
-        #   #     time_format: "%H:%M"
-        #   # }
-        def self.get_settings(account)
-            return @accounts_settings[account.id] if @accounts_settings[account.id]
+        def self.verify_settings
+            return if @accounts_settings
  
-            self.reset_db_settings(account)
- 
-            return @accounts_settings[account.id]
+            self.reset_db_settings
         end
  
-        def self.to_string(account, datetime_object)
-            settings = self.get_settings(account)
+        def self.to_string(datetime_object)
+            self.verify_settings
             
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
-            datetime_object.in_time_zone(zone).strftime(settings[:date_format])
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
+            datetime_object.in_time_zone(zone).strftime(@settings[:date_format])
         end
  
-        def self.to_string_datetime(account, datetime_object)
-            settings = self.get_settings(account)
- 
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
-            datetime_object.in_time_zone(zone).strftime(settings[:date_format_time])
+        def self.to_string_datetime(datetime_object)
+            self.verify_settings
+            
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
+            datetime_object.in_time_zone(zone).strftime(@settings[:date_format_time])
         end
  
-        def self.to_string_datetime_words(account, datetime_object)
-            settings = self.get_settings(account)
- 
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
-            datetime_object.in_time_zone(zone).strftime(settings[:date_format_full])
+        def self.to_string_datetime_words(datetime_object)
+            self.verify_settings
+            
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
+            datetime_object.in_time_zone(zone).strftime(@settings[:date_format_full])
         end
  
-        def self.to_string_time(account, datetime_object)
-            settings = self.get_settings(account)
- 
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
-            datetime_object.in_time_zone(zone).strftime(settings[:time_format])
+        def self.to_string_time(datetime_object)
+            self.verify_settings
+            
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
+            datetime_object.in_time_zone(zone).strftime(@settings[:time_format])
         end
  
-        def self.today_at_midnight(account)
-            settings = self.get_settings(account)
- 
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
+        def self.today_at_midnight
+            self.verify_settings
+            
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
             return Time.current.in_time_zone(zone).beginning_of_day
         end
  
-        def self.tomorrow_at_midnight(account)
-            settings = self.get_settings(account)
- 
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
+        def self.tomorrow_at_midnight
+            self.verify_settings
+            
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
             return Time.current.in_time_zone(zone).beginning_of_day + 1.day
         end
  
-        def self.now(account)
-            settings = self.get_settings(account)
+        def self.now
+            self.verify_settings
             
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
             return Time.current.in_time_zone(zone)
         end
  
-        def self.distance_to_words(account)
-            settings = self.get_settings(account)
- 
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
+        def self.distance_to_words(time_from, time_to)
+            self.verify_settings
+            
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
  
             time_from = time_from.in_time_zone(zone)
             time_to = time_to.in_time_zone(zone)
@@ -191,21 +169,21 @@ module LC
  
         end
  
-        def self.get_year_difference(account, from_time, to_time)
-            settings = self.get_settings(account)
- 
+        def self.get_year_difference(time_from, time_to)
+            self.verify_settings
+            
             # We set the same timezone for both Time variables
-            zone = ActiveSupport::TimeZone.new(settings[:time_zone])
-            from_time = from_time.in_time_zone(zone)
-            to_time = to_time.in_time_zone(zone)
+            zone = ActiveSupport::TimeZone.new(@settings[:time_zone])
+            time_from = time_from.in_time_zone(zone)
+            time_to = time_to.in_time_zone(zone)
             
             # We extract years, months, and days
-            from_year = from_time.year
-            to_year = to_time.year
-            from_month = from_time.month
-            to_month = to_time.month
-            from_day = from_time.day
-            to_day = to_time.day
+            from_year = time_from.year
+            to_year = time_to.year
+            from_month = time_from.month
+            to_month = time_to.month
+            from_day = time_from.day
+            to_day = time_to.day
  
             year_difference = to_year - from_year
             if from_month > to_month
