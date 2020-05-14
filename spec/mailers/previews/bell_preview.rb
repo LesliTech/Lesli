ca7846aed2604cf1bf55286b4053b01e5d2fa0d5
Luis Donis <ldonis.emc2@gmail.com>
2020-05-14 12:17:18 -0600

    move core tests to lesli folder

--- spec/mailers/previews/bell_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/bell
-class BellPreview < ActionMailer::Preview
-
-end
--- spec/mailers/previews/focus_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/focus
-class FocusPreview < ActionMailer::Preview
-
-end
