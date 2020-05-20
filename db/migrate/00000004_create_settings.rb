dc601ffe5102868d010108fa4232afc23400a3ca
The Lesli Development Team <hello@lesli.tech>
2020-05-20 12:59:13 -0600

    Clean

--- db/migrate/00000004_create_settings.rb
+++ db/migrate/00000004_create_settings.rb
@@ -1,9 +1,8 @@
 class CreateSettings < ActiveRecord::Migration[6.0]
     def change
-        create_table :settings do |t|            
+        create_table :settings do |t|
             t.string :name
             t.string :value
-
             t.timestamps
         end
         add_reference :settings, :accounts, foreign_key: true
