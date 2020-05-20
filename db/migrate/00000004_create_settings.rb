5ffaa206cd24e0c240e3122ff17fad7cdb6e9301
The Lesli Development Team <hello@lesli.tech>
2020-05-20 12:56:46 -0600

    Add deleted_at

--- db/migrate/00000004_create_settings.rb
+++ db/migrate/00000004_create_settings.rb
@@ -1,8 +1,9 @@
 class CreateSettings < ActiveRecord::Migration[6.0]
     def change
-        create_table :settings do |t|
+        create_table :settings do |t|            
             t.string :name
             t.string :value
+
             t.timestamps
         end
         add_reference :settings, :accounts, foreign_key: true
