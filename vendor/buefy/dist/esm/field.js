b8077be1ff34970bf3f5e6f5c9db6c2554a932dd
Luis Donis <ldonis.emc2@gmail.com>
2020-05-29 10:38:04 -0600

    Update npm dependencies (vendors)

---
+++ vendor/buefy/dist/esm/field.js
@@ -0,0 +1,14 @@
+import './chunk-1fafdf15.js';
+import './chunk-6985c8ce.js';
+import { r as registerComponent, u as use } from './chunk-cca88db8.js';
+import { F as Field } from './chunk-eb60b35a.js';
+export { F as BField } from './chunk-eb60b35a.js';
+
+var Plugin = {
+  install: function install(Vue) {
+    registerComponent(Vue, Field);
+  }
+};
+use(Plugin);
+
+export default Plugin;
