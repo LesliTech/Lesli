6be34813b03917514f78f476d0cc0663edce8f9f
Luis Donis <ldonis.emc2@gmail.com>
2020-06-16 10:41:36 -0600

    enable account composition

--- app/vue/accounts/apps/new.vue
+++ app/vue/accounts/apps/new.vue
@@ -2,16 +2,19 @@
 import accountForm from '../components/form.vue'
 
 export default {
+    mounted() {
+        console.log("works")
+    },
    components:{
       'account-form': accountForm
    }
 }
 </script>
 <template>
-   <section>
-      <a class="logo" href="/">
-         <img alt="LesliCloud logo" src="/assets/brand/leslicloud-logo.svg"/>
-      </a>
-      <account-form/>
-   </section>
+    <section class="application-component">
+        <a class="logo" href="/">
+            <img alt="LesliCloud logo" src="/assets/lesli_cloud/brand/lesli-logo.svg" width="150px" />
+        </a>
+        <account-form></account-form>
+    </section>
 </template>
