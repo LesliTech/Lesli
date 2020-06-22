269ea213c73b57ba8f25bce4b070ac78e5c5dbf6
Luis Donis <ldonis.emc2@gmail.com>
2020-06-22 00:50:47 -0600

    add apps navigation

--- lib/vue/layout/application-header.vue
+++ lib/vue/layout/application-header.vue
@@ -112,6 +112,10 @@ export default {
             introJs().start()
         },
 
+        showAppsPanel() {
+            this.bus.publish("show:/core/layout/apps#panel")
+        },
+
         showNotificationPanel() {
             this.bus.publish("show:/core/layout/notification#panel")
         },
@@ -165,6 +169,12 @@ export default {
                 -->
 
                 <slot name="languages"></slot>
+
+                <a class="navbar-item" @click="showAppsPanel()">
+                    <span class="icon">
+                        <svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><title>ionicons-v5-h</title><path d='M104,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,160Z'/><path d='M256,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,160Z'/><path d='M408,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,160Z'/><path d='M104,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,312Z'/><path d='M256,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,312Z'/><path d='M408,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,312Z'/><path d='M104,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,464Z'/><path d='M256,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,464Z'/><path d='M408,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,464Z'/></svg>
+                    </span>
+                </a>
 
                 <a class="navbar-item notification-indicator" data-intro="Notification icon" @click="showNotificationPanel()">
                     <i v-if="notification.count > 0" class="fas fa-bell has-text-link"></i>
