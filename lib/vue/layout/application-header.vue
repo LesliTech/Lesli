b7a50b30c61b92e7a311836ffa2f5a181b9dbc0d
Luis Donis <ldonis.emc2@gmail.com>
2020-06-24 21:34:16 -0600

    add right quickview

--- lib/vue/layout/application-header.vue
+++ lib/vue/layout/application-header.vue
@@ -113,8 +113,8 @@ export default {
             introJs().start()
         },
 
-        showAppsPanel() {
-            this.bus.publish("show:/core/layout/apps#panel")
+        showAppsPanel(side) {
+            this.bus.publish("show:/core/layout/apps#panel", side)
         },
 
         showNotificationPanel() {
@@ -141,6 +141,11 @@ export default {
     <header class="application-header">
         <b-navbar transparent>
             <template slot="brand">
+                <span class="icon" @click="showAppsPanel('right')">
+                    <span class="icon">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><title>ionicons-v5-h</title><path d='M104,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,160Z'/><path d='M256,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,160Z'/><path d='M408,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,160Z'/><path d='M104,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,312Z'/><path d='M256,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,312Z'/><path d='M408,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,312Z'/><path d='M104,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,464Z'/><path d='M256,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,464Z'/><path d='M408,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,464Z'/></svg>
+                    </span>
+                </span>
                 <slot name="brand"></slot>
             </template>
             <template slot="start">
@@ -158,17 +163,17 @@ export default {
             <template slot="end">
 
                 <slot name="languages"></slot>
-                 
-                <a class="navbar-item apps" @click="showAppsPanel()">
-                    <span class="icon">
-                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><title>ionicons-v5-h</title><path d='M104,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,160Z'/><path d='M256,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,160Z'/><path d='M408,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,160Z'/><path d='M104,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,312Z'/><path d='M256,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,312Z'/><path d='M408,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,312Z'/><path d='M104,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,464Z'/><path d='M256,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,464Z'/><path d='M408,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,464Z'/></svg>
-                    </span>
-                </a>
 
                 <a class="navbar-item notification-indicator" data-intro="Notification icon" @click="showNotificationPanel()">
                     <i v-if="notification.count > 0" class="fas fa-bell has-text-link"></i>
                     <i v-if="notification.count == 0" class="far fa-bell"></i>
                     <span>{{ notification.count }}</span>
+                </a>
+
+                <a class="navbar-item apps" @click="showAppsPanel('left')">
+                    <span class="icon">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><title>ionicons-v5-h</title><path d='M104,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,160Z'/><path d='M256,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,160Z'/><path d='M408,160a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,160Z'/><path d='M104,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,312Z'/><path d='M256,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,312Z'/><path d='M408,312a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,312Z'/><path d='M104,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,104,464Z'/><path d='M256,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,256,464Z'/><path d='M408,464a56,56,0,1,1,56-56A56.06,56.06,0,0,1,408,464Z'/></svg>
+                    </span>
                 </a>
 
                 <div class="navbar-item has-dropdown is-hoverable" data-intro="Account options">
