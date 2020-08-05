4dda5cd02dac1424d59bd7c3157a3bcadc466a1a
The Lesli Development Team <hello@lesli.tech>
2020-08-05 09:32:49 -0600

    Jmeter stress tests

--- tests/stress/requests/lesli/users_test.rb
+++ tests/stress/requests/lesli/users_test.rb
@@ -3,56 +3,28 @@ require 'json'
 
 test do
     threads count: 1, loops: 1 do
-        visit name: 'Lesli', url: 'http://127.0.0.1:3000'
-        visit name: 'Login Page', url: 'http://127.0.0.1:3000/login' do
+        user_defined_variables [{name: 'email', value: 'dev@lesli.cloud'}, {name: 'password', value: 'lesli2020'}]
+
+        visit name: 'Lesli', url: 'https://intern.deutsche-leibrenten.de'
+        # auth url: '/login', email: 'dev@lesli.cloud', password: 'lesli2020', domain: 'https://intern.deutsche-leibrenten.de'
+        
+        visit name: 'Login page', url: 'https://intern.deutsche-leibrenten.de/login' do
             extract name: 'csrf-token', xpath: "//meta[@name='csrf-token']/@content", tolerant: true
             extract name: 'csrf-param', xpath: "//meta[@name='csrf-param']/@content", tolerant: true
+            # extract name: 'authenticity_token', regex: 'name="authenticity_token" value="(.+?)"'
         end
-
-        # auth url: '/login', email: 'dev@lesli.cloud', password: 'lesli2020', domain: 'http://127.0.0.1:3000'
-        # visit name: 'Dashboard', url: 'http://127.0.0.1:3000/crm'
-
-        # user = { email: 'dev@lesli.cloud', password:'lesli2020' }
-        # payload = {
-        #     user: {
-        #         email: 'dev@lesli.cloud',
-        #         password:'lesli2020',
-        #     },
-        #     'csrf-token' => '${csrf-token}'
-        # }
-
-        header [
-            # { name: 'Content-Type', value: 'application/json' },
-            { name: 'X-CSRF-Token', value: '${csrf-token}' }
-        ]
-        # http_header_manager name: 'X-CSRF-Token', value: '${csrf-token}'
-        submit name: 'Login', url: 'http://127.0.0.1:3000/login', fill_in: {
+        
+        http_header_manager name: 'X-CSRF-Token', value: '${csrf-token}'
+        
+        submit name: 'Login', url: 'https://intern.deutsche-leibrenten.de/login',
+        fill_in: {
             '${csrf-param}' => '${csrf-token}',
-            'user[email]' => 'dev@lesli.cloud',
-            'user[password]' => 'lesli2020',
-            # :user => {
-            #     :email => 'dev@lesli.cloud',
-            #     :password => 'lesli2020'
-            # }
-            # authorization: {
-            #     user: {
-            #         email: 'dev@lesli.cloud',
-            #         password:'lesli2020',
-            #     },
-            # }
-            # "user" => {
-            #     "email"=>"dev@lesli.cloud",
-            #     "password"=>"lesli2020"
-            # }
+            'user[email]' => '${email}',
+            'user[password]' => '${password}',
+            # 'authenticity_token' => '${authenticity_token}',
         }
-        # .to_json do
-        #     with_xhr
-        # end
-        # visit name: 'Dashboard', url: 'http://127.0.0.1:3000/crm'
 
-        # header [{ name: 'Content-Type', value: 'application/json' }]
-        # post name: 'Login', url: "http://127.0.0.1:3000/login", raw_body: payload.to_json
-        # visit name: 'Dashboard', url: 'http://127.0.0.1:3000/crm'
+        # visit name: 'Dashboard', url: 'https://intern.deutsche-leibrenten.de/crm'
     end
 end.run(
     file: "tests/stress/test_plan/lesli/users_test.jmx",
