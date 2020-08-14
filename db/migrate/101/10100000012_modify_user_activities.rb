e287e593b8d64ee9b175f544afe73121f96768e7
Luis Donis <ldonis.emc2@gmail.com>
2020-08-14 11:26:53 -0600

    add request_action to user activities

--- db/migrate/101/10100000012_modify_user_activities.rb
+++ db/migrate/101/10100000012_modify_user_activities.rb
@@ -1,7 +1,7 @@
 class ModifyUserActivities < ActiveRecord::Migration[6.0]
     def change
         add_column :user_activities, :request_controller, :string
-        #add_column :user_activities, :request_action, :string
+        add_column :user_activities, :request_action, :string
         remove_column :user_activities, :log_scope
     end
 end
