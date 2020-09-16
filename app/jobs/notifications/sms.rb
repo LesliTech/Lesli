4c5a1b028e2655c06b3426afcd08bf940cff1e6a
FR98 <frosal@lomax.com.gt>
2020-09-16 11:39:15 -0600

    Move notifications to jobs and add activity log

---
+++ app/jobs/notifications/sms.rb
@@ -0,0 +1,23 @@
+module Notifications
+
+    class Sms < ApplicationJob
+        queue_as :default
+
+        def self.send(user, message, request_method, request_controller)
+            sms = Aws::SNS::Client.new(
+                region: Rails.application.credentials.services[:aws][:region],
+                access_key_id: Rails.application.credentials.services[:aws][:access_key_id],
+                secret_access_key: Rails.application.credentials.services[:aws][:secret_access_key],
+            ).publish(
+                phone_number: user.detail.telephone,
+                message: message
+            )
+
+            user.log_activity( request_method, request_controller, nil, nil, 'Token sent successfully')
+
+            sms
+        end
+
+    end
+
+end
