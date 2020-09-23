27c323b6480d0e648b176ac88dda79c84c9cc1ee
Carlos Hermosilla <efrainh12@galileo.edu>
2020-09-23 11:14:33 -0600

    fixed sms job

--- app/jobs/notifications/sms.rb
+++ app/jobs/notifications/sms.rb
@@ -3,21 +3,16 @@ module Notifications
     class Sms < ApplicationJob
         queue_as :default
 
-        def self.send(user, message, request_method, request_controller)
-            sms = LC::Providers::Aws::Sns.new()
-
-            sms.publish({
-                phone_number: user.detail.telephone,
-                message: message
-            })
-
+        def self.send(user, message, request_method, request_controller) 
             begin
-                sms.publish(
+                sms = LC::Providers::Aws::Sns.new()
+                sms.publish({
                     phone_number: user.detail.telephone,
                     message: message
-                )
+                })
             rescue => error
                 puts "Failed SMS send"
+                puts error
             end
 
             user.log_activity(request_method, request_controller, nil, nil, 'Token sent successfully')
