65959bbe0deee89e8b572abe186f146e41ad9dec
volant360 <efrainh12@galileo.edu>
2020-09-29 09:20:42 -0600

    removed test

--- app/jobs/files/aws_upload_job.rb
+++ app/jobs/files/aws_upload_job.rb
@@ -7,8 +7,6 @@ class Files::AwsUploadJob < ApplicationJob
                 file.update!(attachment: file.attachment_local)
                 file.update!(attachment_local: nil)
 
-                puts x.name
-
                 # Sanity check in case the file got deleted when uploading it to AWS
                 # We reload the ActiveRecord because the one received as parameter is not up to date
                 file.update!(attachment: nil) unless file.class.find_by(id: file.id)
