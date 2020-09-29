3651d1e78443fcccacbd1d5b5fb1edb3bc9ee745
volant360 <efrainh12@galileo.edu>
2020-09-29 09:18:26 -0600

    add trello/1899 activity log when a file is uploaded

--- app/jobs/files/aws_upload_job.rb
+++ app/jobs/files/aws_upload_job.rb
@@ -2,13 +2,23 @@ class Files::AwsUploadJob < ApplicationJob
     queue_as :default
 
     def perform(file)
-        if file.attachment_local.file && !file.attachment.file
-            file.update!(attachment: file.attachment_local)
-            file.update!(attachment_local: nil)
+        begin
+            if file.attachment_local.file && !file.attachment.file
+                file.update!(attachment: file.attachment_local)
+                file.update!(attachment_local: nil)
 
-            # Sanity check in case the file got deleted when uploading it to AWS
-            # We reload the ActiveRecord because the one received as parameter is not up to date
-            file.update!(attachment: nil) unless file.class.find_by(id: file.id)
+                puts x.name
+
+                # Sanity check in case the file got deleted when uploading it to AWS
+                # We reload the ActiveRecord because the one received as parameter is not up to date
+                file.update!(attachment: nil) unless file.class.find_by(id: file.id)
+            end
+        rescue => error
+            file.cloud_object.account.account.activities.create({
+                system_module: file.class.name.split("::")[0],
+                system_process: "aws_uploader",
+                payload: error.to_json
+            })
         end
     end
 end
