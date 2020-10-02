6850518ed20caeeb9ca704a95b44ca100bef33d8
Luis Donis <ldonis.emc2@gmail.com>
2020-10-01 19:34:00 -0600

    Add support for child rails components for core entities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -133,7 +133,7 @@ module ApplicationHelper
     def is_lesli_engine?(engine=nil)
         current_engine = lesli_engine
         return current_engine == engine if not engine.blank?
-        return false if ["profiles", "users", "abouts", "settings"].include?(current_engine)
+        return false if ["profiles", "users", "abouts", "settings", "accounts"].include?(current_engine)
         return true
     end 
 
