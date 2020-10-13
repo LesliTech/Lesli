ec17c141800997988e896697841293161f22ae97
volant360 <efrainh12@galileo.edu>
2020-10-13 09:54:34 -0600

    add trello/1858 moved vue components to core

---
+++ lib/vue/cloud_objects/dashboards/apps/edit.vue
@@ -0,0 +1,55 @@
+<script>
+import componentForm from "../components/form.vue"
+
+
+export default {
+
+    props: {
+        cloudEngine: {
+            type: String,
+            required: true
+        },
+
+        engineNamespace: {
+            type: String,
+            required: true
+        },
+        renderComponents: {
+            required: true
+        }
+    },
+    
+    components: {
+        'component-form': componentForm
+    },
+
+    data(){
+        return {
+            translations: {
+                main: I18n.t(`${this.cloudEngine.toLowerCase()}.dashboards`)
+            }
+        }
+    }
+}
+</script>
+<template>
+    <section class="application-component">
+        <nav class="navbar component-header">
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <div class="navbar-item">
+                        <h4 class="is-size-3">
+                            {{translations.main.view_title_main}}
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <component-form
+            :render-components="renderComponents"
+            :cloud-engine="cloudEngine"
+            :engineNamespace="engineNamespace"
+        >
+        </component-form>
+    </section>
+</template>
---
+++ lib/vue/cloud_objects/dashboards/apps/new.vue
@@ -0,0 +1,50 @@
+<script>
+import componentForm from '../components/form.vue'
+
+
+export default {
+    props: {
+        cloudEngine: {
+            type: String,
+            required: true
+        },
+
+        engineNamespace: {
+            type: String,
+            required: true
+        }
+    },
+    
+    components: {
+        'component-form': componentForm
+    },
+
+    data(){
+        return {
+            translations: {
+                main: I18n.t(`${this.cloudEngine.toLowerCase()}.dashboards`)
+            }
+        }
+    }
+}
+</script>
+<template>
+    <section class="application-component">
+        <nav class="navbar component-header">
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <div class="navbar-item">
+                        <h4 class="is-size-3">
+                            {{translations.main.view_title_main}}
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <component-form
+            :cloud-engine="cloudEngine"
+            :engineNamespace="engineNamespace"
+        >
+        </component-form>
+    </section>
+</template>
