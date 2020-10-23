bd6076e8166eb05fd820baf3576ba5e46fe827be
Luis Donis <ldonis.emc2@gmail.com>
2020-10-23 08:07:25 -0600

    re-enable user profile view

--- app/vue/users/components/form-information.vue
+++ app/vue/users/components/form-information.vue
@@ -1,25 +1,16 @@
 <script>
-/**
-Lesli
+/*
+Copyright (c) 2020, all rights reserved.
 
-Copyright (c) 2020, Lesli Technologies, S. A.
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
 
-All the information provided by this website is protected by laws of Guatemala related 
-to industrial property, intellectual property, copyright and relative international laws. 
-Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
-rights of the code, texts, trade mark, design, pictures and any other information.
-Without the written permission of Lesli Technologies, S. A., any replication, modification,
+Without the written permission of the owner, any replication, modification,
 transmission, publication is strictly forbidden.
+
 For more information read the license file including with this software.
-
-Lesli - Your Smart Business Assistant
-
-Powered by https://www.lesli.tech
-Building a better future, one line of code at a time.
-
-@contact  <hello@lesli.tech>
-@website  <https://lesli.tech>
-@license  Propietary - all rights reserved.
 
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 // · 
@@ -31,8 +22,8 @@ export default {
     data() {
         return {
             translations: {
-                users: I18n.t('deutscheleibrenten.users'),
-                shared: I18n.t('deutscheleibrenten.shared')
+                users: I18n.t('core.users'),
+                shared: I18n.t('core.shared')
             },
             options: {},
             user: {
@@ -42,7 +33,7 @@ export default {
     },
     methods: {
         putUser() {
-            this.http.put(`/lock/users/${this.user.id}.json`, {
+            this.http.put(`/administration/users/${this.user.id}.json`, {
                 user: this.user
             }).then(result => {
                 if (result.successful) {
@@ -67,12 +58,14 @@ export default {
 </script>
 
 <template>
-    <form @submit.prevent="putUser()">
+    <div class="card">
+        <div class="card-content">
+<form @submit.prevent="putUser()">
         <fieldset>
             <div class="column is-half">
                 <div class="field">
                     <label class="label">
-                        {{ translations.shared.text_salutation }}
+                        {{ translations.shared.view_text_salutation }}
                     </label>
                     <b-radio
                         v-for="option in options.salutations"
@@ -84,33 +77,35 @@ export default {
                     </b-radio>
                 </div>
                 <div class="field">
-                    <label class="label"> {{ translations.shared.text_first_name}} </label> 
+                    <label class="label"> {{ translations.shared.view_text_first_name}} </label> 
                     <div class="control">
                         <input v-model="user.detail_attributes.first_name" required="required" type="text" class="input">
                     </div>
                 </div>
                 <div class="field">
-                    <label class="label"> {{ translations.shared.text_last_name }} </label> 
+                    <label class="label"> {{ translations.shared.view_text_last_name }} </label> 
                     <div class="control">
                         <input v-model="user.detail_attributes.last_name" required="required" class="input">
                     </div>
                 </div>
                 <div class="field">
-                    <label class="label"> {{ translations.shared.text_title }} </label> 
+                    <label class="label"> {{ translations.users.view_text_title }} </label> 
                     <div class="control">
                         <input v-model="user.detail_attributes.title" class="input">
                     </div>
                 </div>
                 <div class="field">
-                    <label class="label"> {{ translations.shared.text_telephone }} </label> 
+                    <label class="label"> {{ translations.shared.view_text_telephone }} </label> 
                     <div class="control">
                         <input v-model="user.detail_attributes.telephone" class="input">
                     </div>
                 </div>
                 <p class="control">
-                    <button class="button is-primary">{{ translations.shared.btn_save }}</button>
+                    <button class="button is-primary">{{ translations.shared.view_btn_save }}</button>
                 </p>
             </div>
         </fieldset>
     </form>
+        </div>
+    </div>
 </template>
