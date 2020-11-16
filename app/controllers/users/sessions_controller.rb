0250d5bca926b21c4287f0ff003abf6609c1a7cd
Luis Donis <ldonis.emc2@gmail.com>
2020-11-16 12:05:53 -0600

    add password expiration date

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -29,40 +29,75 @@ class Users::SessionsController < Devise::SessionsController
     #     If it is not successful, it returs an error message
     # @description Creates a new session for the user and allows them access to the platform
     # @example
-    #     # Executing this controller's action from javascript's frontend
+    #     # Executing this controller"s action from javascript"s frontend
     #     let data = {
     #         user: {
-    #             email: 'john.doe@email.com',
-    #             password: 'my_password_1234567890'
+    #             email: "john.doe@email.com",
+    #             password: "my_password_1234567890"
     #         }
     #     };
-    #     this.http.post('127.0.0.1/login', data);
+    #     this.http.post("127.0.0.1/login", data);
     def create
+
 
         # search for a existing user 
         resource = User.find_for_database_authentication(email: sign_in_params[:email], active: true)
 
-        return respond_with_error(I18n.t('deutscheleibrenten.users/sessions.invalid_credentials')) unless resource
 
+        # respond with a no valid credentials generic error if not valid user found
+        return respond_with_error(I18n.t("core.users/sessions.invalid_credentials")) unless resource
+
+
+        # save a login atempt log for the requested user
         log = resource.logs.create({
-            session_uuid: session[:session_uuid],
+            session_uuid: nil,
             description: "login_atempt"
         })
 
+
+        # check password validation
         unless resource.valid_password?(sign_in_params[:password])
+
+            # save a invalid credentials log for the requested user
             log.update_attribute(:description, "login_atempt_invalid_credentials")
-            return respond_with_error(I18n.t('deutscheleibrenten.users/sessions.invalid_credentials'))
+
+            # respond with a no valid credentials generic error if not valid user found
+            return respond_with_error(I18n.t("core.users/sessions.invalid_credentials"))
+
         end
         
+
+        # check if user is already confirmed
         unless resource.confirmed?
             return respond_with_error(I18n.t("devise.errors.custom.confirmation_required"))
         end
 
-        return respond_with_error(I18n.t('deutscheleibrenten.users/sessions.role_access_denied')) unless resource.role.detail.active?
 
+        # check if the role that the user belongs to, is able to access the system
+        if not resource.role.detail.active?
+            return respond_with_error(I18n.t("deutscheleibrenten.users/sessions.role_access_denied")) 
+        end
+
+
+        # check password expiration date
+        if not resource.password_expiration_at.blank?
+            if resource.password_expiration_at < Time.current
+                log.update_attribute(:description, "login_atempt_expired_password")
+                token = resource.generate_password_token
+                return respond_with_error(I18n.t("core.users/sessions.messages_danger_password_expired"), {
+                    reset_password_token: token
+                })
+            end
+        end
+
+
+        # do a user login
         sign_in :user, resource
 
+
+        # register a successful sign-in log for the current user
         log.update_attribute(:description, "login_atempt_successful")
+
 
         # register a new unique session
         @current_session = resource.sessions.create({
@@ -73,9 +108,12 @@ class Users::SessionsController < Devise::SessionsController
             :last_used_at   => LC::Date.now
         })
 
+
         session[:user_session_id] = @current_session[:id]
 
+
         respond_with_successful()
+
 
     end
 
