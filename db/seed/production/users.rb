a6ee5602b2582dd07bea7303889a51b66e59c744
The Lesli Development Team <hello@lesli.tech>
2020-11-24 15:01:40 -0600

    Fix production seeder

--- db/seed/production/users.rb
+++ db/seed/production/users.rb
@@ -35,12 +35,13 @@ generated_password = Devise.friendly_token.first(32)
 # create development user
 account_logins.each do |account_login|
     User.find_or_create_by(email: account_login["username"]) do |user|
-        user.role = Role.first
         user.password = generated_password
         user.password_confirmation = generated_password
         user.account = Account.first
         user.confirm if not user.confirmed?
         user.save!
+
+        user.user_roles.create({ role: Role.first })
         user.detail.first_name = account_login["fullname"] if not user.detail.blank?
 
         user.account.user = user
