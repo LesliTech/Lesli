656727a8b7e0a5128b5455b1b6a9ce119662dd32
Luis Donis <ldonis.emc2@gmail.com>
2020-11-26 10:08:30 -0600

    fix production/development seeders

---
+++ db/seed/tools.rb
@@ -0,0 +1,54 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+
+def create_development_user dev_user
+
+    # get password
+    password = Rails.application.config.lesli_settings["configuration"]["security"]["password"]["development"]
+    password = password + Time.now.year.to_s + "$"
+
+    # user information
+    email = dev_user[4]
+    last_name = dev_user[3]
+    role_name = dev_user[0]
+    salutation = dev_user[1]
+    first_name = dev_user[2]
+
+    # create development users if email is not registered yet
+    ::User.find_or_create_by(email: email) do |user|
+        user.password = password
+        user.password_confirmation = password
+        user.account = Account.first
+        user.confirm if not user.confirmed?
+        
+        user.detail.salutation = salutation
+        user.detail.first_name = first_name
+        user.detail.last_name = last_name
+
+        user.user_roles.create({ role: Role.find_by("name" => role_name) })
+        
+        user.save!
+
+        user.account.user = user
+        user.account.save!
+    end
+
+end
+
--- docs/development-environment-setup.md
+++ docs/development-environment-setup.md
@@ -50,7 +50,7 @@ EDITOR="nano" rails credentials:edit
 ````
 
 
-__Note:__
+__Notes:__
 
 It seems that the latest version of XCode tools (12 Beta 3) installs a version of Clang (the C compiler used by default on MacOS) that throws an error on implicit functions used on the native extension code of Puma.  
   
@@ -59,6 +59,14 @@ Try running:
 bundle config build.puma --with-cflags="-Wno-error=implicit-function-declaration"
 bundle install
 ````
+
+
+__Install pg gem__
+Sometimes pg gem trigger an error when the entire posgres server is not installed on the same server, to resolve this error run:  
+
+```shell
+sudo apt-get -y install libpq-dev
+```
 
 
 __Credentail file example:__
