de148c20e05ebf91b22b8674b9c992b90e8cf035
Carlos Hermosilla <efrainh12@galileo.edu>
2020-12-16 08:16:03 -0600

    fix trello/2092 list button in new view

--- lib/vue/cloud_objects/workflows/apps/new.vue
+++ lib/vue/cloud_objects/workflows/apps/new.vue
@@ -45,7 +45,8 @@ export default {
     data(){
         return {
             translations: {
-                workflows: I18n.t('core.workflows')
+                workflows: I18n.t('core.workflows'),
+                core: I18n.t('core.shared')
             }
         }
     }
@@ -58,8 +59,18 @@ export default {
                 <div class="navbar-start">
                     <div class="navbar-item">
                         <h4 class="is-size-3">
-                            {{translations.workflows.view_title_main}}
+                            {{translations.workflows.view_title_new}}
                         </h4>
+                    </div>
+                </div>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link to="/" class="button">
+                                <b-icon icon="list" size="is-small"></b-icon>
+                                <span>{{translations.core.view_btn_list}}</span>
+                            </router-link>
+                        </div>
                     </div>
                 </div>
             </div>
