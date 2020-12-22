93e7117b9a65efaab665bb9a4ace284b0c746f5e
Luis Donis <ldonis.emc2@gmail.com>
2020-12-22 08:44:09 -0600

    add mails to own engine

--- lib/mailer_previews/user_mailer_preview.rb
+++ lib/mailer_previews/user_mailer_preview.rb
@@ -1,5 +1,5 @@
 class UserMailerPreview < ActionMailer::Preview
     def welcome_email
-        UserMailer.with(user: User.first).welcome_email
+        UserMailer.welcome_email(User.first)
     end
 end
