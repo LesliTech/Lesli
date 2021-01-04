ba779ae4fe92950e1b7c2c8928ccc39ce1cc5710
The Lesli Development Team <hello@lesli.tech>
2021-01-04 14:21:07 +0100

    add trello/2102 method to retrieve  database date in the correct timezone

--- lib/LC/date.rb
+++ lib/LC/date.rb
@@ -51,6 +51,14 @@ module LC
             "
         end
 
+        def self.db_to_timezone column, alias_name = nil
+            self.verify_settings
+
+            query_string = "#{column} at time zone 'utc' at time zone '#{@settings["time_zone"]}'" 
+            query_string = "(#{query_string}) as #{alias_name}" if alias_name
+            query_string
+        end
+
         def self.db_to_char column, alias_name = nil, include_alias = true
             self.verify_settings
             
