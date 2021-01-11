f4d07e32e805e911f6f550e56a25d5ce95f4f7a3
The Lesli Development Team <hello@lesli.tech>
2021-01-11 09:56:47 -0600

    feat: trello/2111 renamed attachment and attachment_local fields

--- app/jobs/files/aws_upload_job.rb
+++ app/jobs/files/aws_upload_job.rb
@@ -3,15 +3,15 @@ class Files::AwsUploadJob < ApplicationJob
 
     def perform(file)
         begin
-            if file.attachment_local.file && !file.attachment.file
-                file.update!(attachment: file.attachment_local)
+            if file.attachment.file && !file.attachment_s3.file
+                file.update!(attachment_s3: file.attachment)
 
-                if file.attachment
-                    file.update!(attachment_local: nil)
+                if file.attachment_s3
+                    file.update!(attachment: nil)
 
                     # Sanity check in case the file got deleted when uploading it to AWS
                     # We reload the ActiveRecord because the one received as parameter is not up to date
-                    file.update!(attachment: nil) unless file.class.find_by(id: file.id)
+                    file.update!(attachment_s3: nil) unless file.class.find_by(id: file.id)
                 end
             end
         rescue => error
