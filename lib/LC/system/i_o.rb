be7654a6f194faf22eee2c5a7bfbf696f4558b8c
The Lesli Development Team <hello@lesli.tech>
2021-01-28 11:13:19 -0600

    docs: Added documentation for LC::System::IO

--- lib/LC/system/i_o.rb
+++ lib/LC/system/i_o.rb
@@ -23,6 +23,14 @@ module LC
 
         class IO
 
+            # @return a Zip file that contains a list of files that are allocated in local storage, either in lesli/public or lesli/storage
+            # @param zip_file [String] The relative path of the zip file to be created. The file will be allocated in lesli/public/tmp until it's released
+            # @param files [Array] Array of strings that contain the relative path of the files to be added to the zip file
+            # @description Creates a temporal zip file that contains all requested files and stores it in memory to be either used, stored
+            #     or sent using an HTPP response
+            # @example
+            #     zip_file = LC::System::IO.zip("my_zip_file.zip", "image1.png", "documents/document1.pdf")
+            #     send_data zip_file.read, filename: "all_documents_#{Date.today.strftime('%d_%B_%Y')}.zip", type: 'application/zip'
             def self.zip zip_file, files
 
                 base_path = Rails.root.join("public", "tmp")
