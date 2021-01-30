7d35f1e9cede4a2ca5b1c76dab5ef85bc296883b
Luis Donis <ldonis.emc2@gmail.com>
2021-01-29 20:09:06 -0600

    test: password reset 100% test coverage

---
+++ spec/requests/users/password_spec.rb
@@ -0,0 +1,43 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+
+require 'rails_helper'
+require 'spec_helper'
+require 'byebug'
+
+
+RSpec.describe 'POST:/password', type: :request do
+    #include_context 'user authentication'
+
+    before(:all) do
+        @user = User.find_by(:email => "test@lesli.cloud")
+        post("/password", params: {
+            user: {
+                email: @user.email
+            }
+        })
+    end
+
+    #include_examples 'successful standard json response'
+
+    it 'is expected to create a password reset token' do
+        LC::Debug.msg(@response_body)
+    end
+end
