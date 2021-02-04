b7667baca4d6f7a15fd94c3b36e6fcf08345107e
The Lesli Development Team <hello@lesli.tech>
2021-02-04 17:52:35 +0100

    feat: trello/2212 register activities for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -470,6 +470,96 @@ class User < ApplicationLesliRecord
         
     end
 
+    #######################################################################################
+    ##############################  Activities Log Methods   ##############################
+    #######################################################################################
+
+    # @return [void]
+    # @param current_user [::User] The user that created the user
+    # @param [User] The user that was created
+    # @description Creates an activity for this user indicating who created it.
+    # Example
+    #   params = {...}
+    #   user = User.create(params)
+    #   User.log_activity_create(User.find(1), user)
+    def self.log_activity_create(current_user, user)
+        user.activities.create(
+            owner: current_user,
+            category: "action_create"
+        )
+    end
+
+    
+    # @return [void]
+    # @param current_user [::User] The user that created the user
+    # @param user [User] The User that was created
+    # @param old_attributes[Hash] The data of the record before update
+    # @param new_attributes[Hash] The data of the record after update
+    # @description Creates an activity for this user if someone changed any of this values
+    # Example
+    #   user = User.find(1)
+    #   old_attributes  = user.attributes.merge({detail_attributes: user.detail.attributes})
+    #   user.update(main_employee: User.find(33))
+    #   new_attributes = user.attributes.merge({detail_attributes: user.detail.attributes})
+    #   User.log_activity_update(User.find(1), user, old_attributes, new_attributes)
+    def self.log_activity_update(current_user, user, old_attributes, new_attributes)
+        old_attributes.except!("id", "users_id", "created_at", "updated_at", "deleted_at")
+        old_attributes.each do |key, value|
+            if value != new_attributes[key]
+                value_from = value
+                value_to = new_attributes[key]
+                value_from = Courier::Core::Date.to_string_datetime(value_from) if value_from.is_a?(Time) || value_from.is_a?(Date)
+                value_to = Courier::Core::Date.to_string_datetime(value_to) if value_to.is_a?(Time) || value_to.is_a?(Date)
+
+                user.activities.create(
+                    owner: current_user,
+                    category: "action_update",
+                    field_name: key,
+                    value_from: value_from,
+                    value_to: value_to
+                )
+            end
+        end
+    end
+
+    # @return [void]
+    # @param current_user [::User] The user that created the user
+    # @param user [User] The User that was created
+    # @param user [Role] The Role assigned to the user
+    # @description Creates an activity for this user if someone adds a new role
+    # Example
+    #   user = User.find(1)
+    #   role = User.find(1)
+    #   user.log_activity_create_user_role(current_user, user, role)
+    def self.log_activity_create_user_role(current_user, user, role)
+        role_name = role.name 
+
+        user.activities.create(
+            owner: current_user,
+            category: "action_create_user_role",
+            value_to: role_name
+        )
+    end
+
+    # @return [void]
+    # @param current_user [::User] The user that created the user
+    # @param user [User] The User that was created
+    # @param user [Role] The Role assigned to the user
+    # @description Creates an activity for this user if someone removes a role 
+    # Example
+    #   user = User.find(1)
+    #   role = User.find(1)
+    #   user.log_activity_destroy_user_role(current_user, user, role)
+    def self.log_activity_destroy_user_role(current_user, user, role)
+        role_name = role.name 
+
+        user.activities.create(
+            owner: current_user,
+            category: "action_destroy_user_role",
+            value_to: role_name
+        )
+    end
+
     # DEPRECATED 
 
     # @return [void]
