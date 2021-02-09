cf103437fa2cc033fbbad3709ad3e47f62736d06
The Lesli Development Team <hello@lesli.tech>
2021-02-09 13:04:44 -0600

    feat: add courier for cloud one firebase chatroom

---
+++ lib/courier/one/firebase/chatroom.rb
@@ -0,0 +1,33 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+module Courier
+    module One
+        module Firebase
+            class Chatroom
+
+                def self.create(current_user, members, chat_name="")
+                    return unless defined? CloudOne
+                    CloudOne::Firebase::Chatroom.create(current_user, members, chat_name)
+                end
+
+            end
+        end
+    end
+end
