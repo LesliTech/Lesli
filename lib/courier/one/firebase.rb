7d193c68d899017f4013b917c46aac0a0e516bc3
FranciscoRosal <frosal@lomax.com.gt>
2021-02-09 08:30:34 -0600

    feat: add registration to firebase

---
+++ lib/courier/one/firebase.rb
@@ -0,0 +1,31 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+module Courier
+    module One
+        class Firebase
+
+            def self.registration(registration_params)
+                return unless defined? CloudOne
+                CloudOne::Firebase.registration(registration_params)
+            end
+
+        end
+    end
+end
