88194f85e3553bd4cf1a5008652f32dd750f025e
The Lesli Development Team <hello@lesli.tech>
2021-02-16 10:32:00 -0600

    feat: add CloudTalk

--- app/models/account.rb
+++ app/models/account.rb
@@ -39,6 +39,7 @@ class Account < ApplicationRecord
     has_one :bell,       class_name: "CloudBell::Account",       foreign_key: "id"
     has_one :help,       class_name: "CloudHelp::Account",       foreign_key: "id"
     has_one :text,       class_name: "CloudText::Account",       foreign_key: "id"
+    has_one :talk,       class_name: "CloudTalk::Account",       foreign_key: "id"
     has_one :audit,      class_name: "CloudAudit::Account",      foreign_key: "id"
     has_one :lesli,      class_name: "CloudLesli::Account",      foreign_key: "id"
     has_one :books,      class_name: "CloudBooks::Account",      foreign_key: "id"
@@ -178,6 +179,14 @@ class Account < ApplicationRecord
                 self.text = CloudText::Account.new
                 self.text.account = self
                 self.text.save!
+            end
+        end
+
+        if defined? CloudTalk
+            if self.talk.blank?
+                self.talk = CloudTalk::Account.new
+                self.talk.account = self
+                self.talk.save!
             end
         end
 
