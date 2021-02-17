3b15c57bcb5deb94371e2d82acc32214ef6dd721
Luis Donis <ldonis.emc2@gmail.com>
2021-02-17 13:44:25 -0600

    add simple email editor -- proof of concept

---
+++ lib/vue/components/editors/email/blocks/foundation-grid.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: "Grid"
+}
+</script>
+<template>
+    <table class="row">
+        <tbody draggable="true">
+            <tr draggable="true">
+                <th class="small-12 large-4 columns first" draggable="true">
+                    <table draggable="true">
+                        <tbody draggable="true">
+                            <tr draggable="true">
+                                <th contenteditable="true" draggable="true">One</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </th>
+                    <th class="small-12 large-4 columns">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th contenteditable="true">Two</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </th>
+                    <th class="small-12 large-4 columns last">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th contenteditable="true">Three</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </th>
+            </tr>
+        </tbody>
+    </table>
+</template>
