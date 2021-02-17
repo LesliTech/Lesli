e41168249b4e1e2eae94670a1b2c1a8eb516cbcd
Luis Donis <ldonis.emc2@gmail.com>
2021-02-17 13:44:25 -0600

    add simple email editor -- proof of concept

---
+++ lib/vue/components/editors/email/blocks/foundation-button.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "Button"
+}
+</script>
+<template>
+    <table class="button">
+        <tbody>
+            <tr>
+                <td>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><a href="#" contenteditable="true">Button exportable</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
