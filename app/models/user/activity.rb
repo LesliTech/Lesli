42d51b110d6796f77e63351547d77e2f70f6f400
The Lesli Development Team <hello@lesli.tech>
2021-02-19 12:35:55 -0600

    fix: Restored deleted files

--- app/models/user/activity.rb
+++ app/models/user/activity.rb
@@ -27,6 +27,7 @@ class User::Activity < ApplicationRecord
         action_update:                          "action_update",
         action_destroy:                         "action_destroy",
         action_create_user_role:                "action_create_user_role",
-        action_destroy_user_role:               "action_destroy_user_role"
+        action_destroy_user_role:               "action_destroy_user_role",
+        action_become:                          "action_become"
     }
 end
