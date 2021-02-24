cbfbebfdca19b241ba0bd5d5e5508a36f0c1a0bf
Luis Donis <ldonis.emc2@gmail.com>
2021-02-24 11:37:39 -0600

    feat: two-way data biding

--- lib/vue/components/editors/email/blocks/foundation-menu.vue
+++ lib/vue/components/editors/email/blocks/foundation-menu.vue
@@ -1,6 +1,37 @@
 <script>
+/*
+
+Copyright (c) 2021, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+*/
+
 export default {
-    name: "Menu"
+    title: "Menu",
+    code: "foundation-menu",
+    props: {
+        value: {
+            type: String,
+            default: "I'm in a callout!"
+        }
+    },
+    methods:{
+        emitValue(e) {
+            this.$emit('input', e.target.innerText)
+        }
+    }
 }
 </script>
 <template>
@@ -11,9 +42,9 @@ export default {
                     <table>
                         <tbody>
                             <tr>
-                                <th class="menu-item"><a href="https://zurb.com">Item</a></th>
-                                <th class="menu-item"><a href="https://zurb.com">Item</a></th>
-                                <th class="menu-item"><a href="https://zurb.com">Item</a></th>
+                                <th class="menu-item"><a href="#">Item</a></th>
+                                <th class="menu-item"><a href="#">Item</a></th>
+                                <th class="menu-item"><a href="#">Item</a></th>
                             </tr>
                         </tbody>
                     </table>
