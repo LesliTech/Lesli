2f84b64de836ce00122dee020580624648a3714d
Luis Donis <ldonis.emc2@gmail.com>
2021-02-26 12:27:17 -0600

    feat: add text editor for emails

---
+++ lib/vue/components/editors/email/blocks/quill-text.vue
@@ -0,0 +1,75 @@
+<script>
+/*
+
+Copyright (c) 2021, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+*/
+
+import componentEditorRichText from "LesliVue/components/editors/richtext.vue"
+
+export default {
+    title: "Text",
+    code: "quill-text",
+    components: {
+        "component-editor-richtext": componentEditorRichText
+    },
+    props: {
+        value: {
+            type: Object,
+            default: function() {
+                return {}
+            }
+        }
+    },
+    data() {
+        return { 
+            text: {}
+        }
+    },
+    mounted() {
+        this.text = this.value
+    },
+    methods:{
+        emitValue(e) {
+            this.$emit('input', e.target.innerText)
+        }
+    },
+    watch: {
+        value(val) {
+            if (val !== this.text) {
+                this.text = val
+            }
+        },
+        text(val) {
+            this.$emit('input', val)
+        }
+    }
+}
+</script>
+<template>
+    <table class="button">
+        <table>
+            <tbody>
+                <tr>
+                    <th>
+                        <component-editor-richtext @blur="emitValue" v-model="text">
+                        </component-editor-richtext>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </table>
+</template>
