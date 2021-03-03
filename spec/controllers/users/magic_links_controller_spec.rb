8c8770a742fc1350e76469f4dbf3f0b4dac3f7e5
Luis Sagastume <lfsagastumevides@gmail.com>
2021-03-03 11:12:43 -0600

    feat: init magic links

---
+++ spec/controllers/users/magic_links_controller_spec.rb
@@ -0,0 +1,21 @@
+describe Users::MagicLinksController do
+    describe 'GET #create' do
+        context 'when user completes all the conditions to login' do
+            it "Valid Code" do
+
+            end
+
+            it "Wrong code" do
+
+            end
+
+            it "Code out of time" do
+
+            end
+
+            it "Code that has already been used" do
+
+            end
+        end
+    end
+end
