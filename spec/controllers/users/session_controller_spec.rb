811697a6973b0fbcd7e77015033ef4d3fddf15ab
Luis Sagastume <lfsagastumevides@gmail.com>
2021-03-04 10:30:16 -0600

    feat: add test for login

--- spec/controllers/users/session_controller_spec.rb
+++ spec/controllers/users/session_controller_spec.rb
@@ -1,10 +1,10 @@
-=begin 
+=begin
 
 Copyright (c) 2020, all rights reserved.
 
-All the information provided by this platform is protected by international laws related  to 
-industrial property, intellectual property, copyright and relative international laws. 
-All intellectual or industrial property rights of the code, texts, trade mark, design, 
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
 pictures and any other information belongs to the owner of this platform.
 
 Without the written permission of the owner, any replication, modification,
@@ -13,7 +13,7 @@ transmission, publication is strictly forbidden.
 For more information read the license file including with this software.
 
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-// · 
+// ·
 
 =end
 
@@ -21,65 +21,23 @@ require 'rails_helper'
 require 'spec_helper'
 require 'faker'
 
-RSpec.configure do |config|
-    config.include Devise::Test::ControllerHelpers, :type => :controller
-end
-
-RSpec.describe Users::SessionsController, type: :controller do
-
-    before :each do
-        request.env["devise.mapping"] = Devise.mappings[:user]
-    end
-
-    describe "POST:/users/session" do
-
-        it "Sign in with valid credentials" do 
-            # Creating a dummy user
-            user = Account.first.users.create!({
-                email: Faker::Internet.email,
-                password: "tardis2021$",
-                password_confirmation: "tardis2021$"
-            })
-            user.user_roles.create({ role: Account.first.roles.find_by(name: "limited") })
-
-            # confirm my new user so I'm able to login
-            user.confirm
-
-            post :create, params: {
+RSpec.describe 'POST:/administration/asdfroles.json', type: :request do
+    before(:all) do
+        user = Account.first.users.create!({
+                                                   email: Faker::Internet.email,
+                                                   password: "tardis2021$",
+                                                   password_confirmation: "tardis2021$"
+                                           })
+        user.user_roles.create({ role: Account.first.roles.find_by(name: "limited") })
+        user.confirm
+        post "/login", params: {
                 "user": {
-                    "email": user.email,
-                    "password": "tardis2021$"
+                        "email": user.email,
+                        "password": "tardis2021$"
                 }
-            }
-
-            expect(response).to have_http_status(:success) 
-            expect(response.content_type).to eq("application/json; charset=utf-8")
-            expect(JSON.parse(response.body)["successful"]).to eql(true)
-            expect(JSON.parse(response.body)).to eql({
-                "successful"=> true,
-                "data" => nil
-            })
-        end
-
-        it "Sign in with wrong username" do 
-            post :create, params: {
-                user: {
-                    user: "wrong@user.email",
-                    password: "lesli2020"
-                }
-            }
-            expect(response).to have_http_status(:success) 
-            expect(response.content_type).to eq("application/json; charset=utf-8")
-            expect(JSON.parse(response.body)["successful"]).to eql(false)
-            expect(JSON.parse(response.body)).to eql({
-                "successful"=> false,
-                "error"=> {
-                    "details"=> [], 
-                    "message"=> I18n.t("core.users/sessions.invalid_credentials")
-                }
-            })
-        end
-
+        }
     end
-
+    it "Sign in with valid credentials" do
+        expect(response).to have_http_status(:success)
+    end
 end
