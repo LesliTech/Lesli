9243701136b2ceda2903603045beb64e1ca86e71
The Lesli Development Team <hello@lesli.tech>
2021-03-05 10:23:43 -0600

    feat: session validation service

--- spec/controllers/users/session_controller_spec.rb
+++ spec/controllers/users/session_controller_spec.rb
@@ -21,23 +21,144 @@ require 'rails_helper'
 require 'spec_helper'
 require 'faker'
 
-RSpec.describe 'POST:/administration/asdfroles.json', type: :request do
-    before(:all) do
-        user = Account.first.users.create!({
-                                                   email: Faker::Internet.email,
-                                                   password: "tardis2021$",
-                                                   password_confirmation: "tardis2021$"
-                                           })
-        user.user_roles.create({ role: Account.first.roles.find_by(name: "limited") })
-        user.confirm
-        post "/login", params: {
-                "user": {
-                        "email": user.email,
-                        "password": "tardis2021$"
-                }
-        }
+
+RSpec.configure do |config|
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+end
+
+
+RSpec.describe Users::SessionsController, type: :request do
+    describe 'Sign in with valid credentials' do
+        before(:all) do
+            user = Account.first.users.create!({
+                                                       email: Faker::Internet.email,
+                                                       password: "tardis2021$",
+                                                       password_confirmation: "tardis2021$"
+                                               })
+            user_roles = user.user_roles.create({ role: Account.first.roles.find_by(name: "limited") })
+            user_roles.roles.update(active: true)
+            user.confirm
+            post "/login", params: {
+                    "user": {
+                            "email": user.email,
+                            "password": "tardis2021$"
+                    }
+            }
+        end
+        it "is expected to respond with successful" do
+            expect(JSON.parse(response.body)).to eql({
+                                                             "successful"=> true,
+                                                             "data" => nil
+                                                     })
+        end
+        include_examples 'successful standard json response'
     end
-    it "Sign in with valid credentials" do
-        expect(response).to have_http_status(:success)
+
+    describe 'Sign in with wrong username' do
+        before(:all) do
+            post :"/login", params: {
+                    user: {
+                            user: "wrong@user.email",
+                            password: "lesli2020"
+                    }
+            }
+        end
+        it "is expected to respond with successful" do
+            expect(JSON.parse(response.body)).to eql({
+                                                             "successful"=> false,
+                                                             "error"=> {
+                                                                     "details"=> [],
+                                                                     "message"=> I18n.t("core.users/sessions.invalid_credentials")
+                                                             }
+                                                     })
+        end
+        include_examples 'error standard json response'
+    end
+
+    describe 'Sign in with an unconfirmed user' do
+        before(:all) do
+            user = Account.first.users.create!({
+                                                       email: Faker::Internet.email,
+                                                       password: "tardis2021$",
+                                                       password_confirmation: "tardis2021$"
+                                               })
+            user.user_roles.create({ role: Account.first.roles.find_by(name: "limited") })
+            post "/login", params: {
+                    "user": {
+                            "email": user.email,
+                            "password": "tardis2021$"
+                    }
+            }
+        end
+
+        it "is expected to respond with an unconfirmed message" do
+            expect(JSON.parse(response.body)).to eql({
+                                                             "successful"=> false,
+                                                             "error"=> {
+                                                                     "details"=> [],
+                                                                     "message"=> I18n.t("devise.errors.custom.confirmation_required")
+                                                             }
+                                                     })
+        end
+        include_examples 'error standard json response'
+    end
+
+    describe 'Sign in with a user without roles' do
+        before(:all) do
+            user = Account.first.users.create!({
+                                                       email: Faker::Internet.email,
+                                                       password: "tardis2021$",
+                                                       password_confirmation: "tardis2021$"
+                                               })
+            user.confirm
+            post "/login", params: {
+                    "user": {
+                            "email": user.email,
+                            "password": "tardis2021$"
+                    }
+            }
+        end
+
+        it "is expected to respond with a message of has not roles" do
+            expect(JSON.parse(response.body)).to eql({
+                                                             "successful"=> false,
+                                                             "error"=> {
+                                                                     "details"=> [],
+                                                                     "message"=> I18n.t("core.users/sessions.the_user_has_no_assigned_role")
+                                                             }
+                                                     })
+        end
+        include_examples 'error standard json response'
+    end
+
+    describe 'Sign in with a user without active roles' do
+        before(:all) do
+            user = Account.first.users.create!({
+                                                       email: Faker::Internet.email,
+                                                       password: "tardis2021$",
+                                                       password_confirmation: "tardis2021$"
+                                               })
+            user_roles = user.user_roles.create({ role: Account.first.roles.find_by(name: "limited")})
+            user_roles.roles.update(active: false)
+
+            user.confirm
+            post "/login", params: {
+                    "user": {
+                            "email": user.email,
+                            "password": "tardis2021$"
+                    }
+            }
+        end
+
+        it "is expected to respond with an access denied message for inactive roles" do
+            expect(JSON.parse(response.body)).to eql({
+                                                             "successful"=> false,
+                                                             "error"=> {
+                                                                     "details"=> [],
+                                                                     "message"=> I18n.t("deutscheleibrenten.users/sessions.role_access_denied")
+                                                             }
+                                                     })
+        end
+        include_examples 'error standard json response'
     end
 end
