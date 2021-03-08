5a51af06ef566765e60dbd7babb518749e37bee0
The Lesli Development Team <hello@lesli.tech>
2021-03-08 09:05:50 -0600

    feat: trello/2237 Updated notifications component

--- lib/vue/cloud_objects/discussions/form.vue
+++ lib/vue/cloud_objects/discussions/form.vue
@@ -48,9 +48,12 @@ export default {
         return {
             module_name: null,
             object_name: null,
-            show_simple_form: true,
+            submitting_form: false,
             discussion: {
-                content: ""
+                content: ''
+            },
+            translations: {
+                core: I18n.t('core.shared')
             }
         }
     },
@@ -67,13 +70,15 @@ export default {
             this.module_name = parsed_data.cloud_module_name
         },
         
-        postDiscussion(e) {
-
-            if (e) { e.preventDefault() }
+        postDiscussion(event) {
+            if(event){
+                event.preventDefault()
+            }
 
             let form_data = {}
             form_data[`${this.object_name.singular}_discussion`] = this.discussion
-            let url = `/${this.module_name.slash}/${this.object_name.plural}/${this.cloudId}/discussions`
+            let url = `/${this.module_name.slash}/${this.object_name.plural}/${this.cloudId}/discussions.json`
+            this.submitting_form = true
 
             this.http.post(url, form_data).then(result => {
                 if (result.successful) {
@@ -82,6 +87,8 @@ export default {
                 this.bus.$emit('post:components/forms/discussion')
             }).catch(error => {
                 console.log(error)
+            }).finally(()=>{
+                this.submitting_form = false
             })
 
         }
@@ -91,16 +98,29 @@ export default {
 }
 </script>
 <template>
-    <div class="card">
-        <div class="card-header">
-            <h4 class="card-header-title">
-                Add new comment
-            </h4>
-        </div>
-        <div class="card-content">
-            <form @submit="postDiscussion">
-                <input v-if="show_simple_form" class="input" type="text" v-model="discussion.content" placeholder="Add a comment...">
-            </form>
-        </div>
-    </div>
+    <form @submit="postDiscussion">
+        <fieldset :disabled="submitting_form">
+            <b-field grouped>
+                <b-input 
+                    required :placeholder="translations.core.view_placeholder_discussions_add_comment" 
+                    ref="input-comment" 
+                    v-model="discussion.content" 
+                    expanded
+                >
+                </b-input>
+                <p class="control">
+                    <b-button type="is-primary" native-type="submit">
+                        <span v-if="submitting_form">
+                            <b-icon icon="circle-notch" custom-class="fa-spin" size="is-small" />
+                            &nbsp;
+                            {{translations.core.view_btn_saving}}
+                        </span>
+                        <span v-else>
+                            {{translations.core.view_btn_save}}
+                        </span>
+                    </b-button>
+                </p>
+            </b-field>
+        </fieldset>
+    </form>
 </template>
