fabdbb3e3cd8e75a3c101c522f50eb37f80c312a
FranciscoRosal <frosal@lomax.com.gt>
2021-03-10 13:26:49 -0600

    feat: add send message workflow action

---
+++ app/jobs/workflow_actions/send_talk_chatroom_message_job.rb
@@ -0,0 +1,46 @@
+class WorkflowActions::SendTalkChatroomMessageJob < ApplicationJob
+    queue_as :default
+
+    def perform(current_user, cloud_object, action)
+
+        sender_user = nil
+
+        case action.concerning_users["type"]
+        when "creator"
+            sender_user = cloud_object.user_creator
+        when "main"
+            sender_user = cloud_object.user_main
+        when "custom"
+            sender_user = current_user.account.users.find(action.concerning_users["list"][0]["id"]) if action.concerning_users["list"]
+        when "current_user"
+            sender_user = current_user
+        end
+
+        begin
+            replacement_values = {
+                "%global_identifier%" => cloud_object.global_identifier,
+                "%current_user%" => (current_user.full_name || "")
+            }
+
+            action.parse_input_data(replacement_values)
+            input_data = action.input_data
+
+            class_data = cloud_object.class.name.split("::")
+
+            Courier::One::Firebase::Chatroom.send_message(
+                sender_user,
+                cloud_object.chatroom_external_id,
+                input_data["message_text"]
+            )
+
+        rescue StandardError => e
+            if action.configuration["log_errors"]
+                cloud_object.activities.create(
+                    user_creator: current_user,
+                    category: "action_workflow_action_failed",
+                    description: e.message
+                )
+            end
+        end
+    end
+end
