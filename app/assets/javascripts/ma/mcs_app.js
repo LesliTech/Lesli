c1696c5dac50c1e9903c8c900f7d783ba1ef89f1
Luis Donis <ldonis.emc2@gmail.com>
2021-03-16 09:13:09 -0600

    feat: design date2 api

---
+++ app/assets/javascripts/ma/mcs_app.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!*******************************!*\
+  !*** ./app/vue/ma/mcs/app.js ***!
+  \*******************************/
+eval("/*\nCopyright (c) 2021, all rights reserved.\nAll the information provided by this platform is protected by international laws related  to\nindustrial property, intellectual property, copyright and relative international laws.\nAll intellectual or industrial property rights of the code, texts, trade mark, design,\npictures and any other information belongs to the owner of this platform.\nWithout the written permission of the owner, any replication, modification,\ntransmission, publication is strictly forbidden.\nFor more information read the license file including with this software.\n// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~\n// ·\n*/\n\n//# sourceURL=webpack://lesli/./app/vue/ma/mcs/app.js?");
+/******/ })()
+;
