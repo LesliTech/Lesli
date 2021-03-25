8131a90e0fe1bf4aa400872d6542cc62599c946b
The Lesli Development Team <hello@lesli.tech>
2021-03-25 10:47:47 -0600

    fix: trello/1667 fixed email confirmation in core

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -20,16 +20,19 @@ For more information read the license file including with this software.
 class DeviseMailer < Devise::Mailer
     def confirmation_instructions(record, token, opts = {})
 
+        # Depending on wheter there is a new user or they are changing their email, one or another field will be used
+        recipient_email = record.unconfirmed_email || record.email
+
         @data = @data.merge({
             url: "/confirmation?confirmation_token=#{token}",
             user: {
                 full_name: record.full_name,
-                unconfirmed_email: record.unconfirmed_email
+                unconfirmed_email: recipient_email
             }
         })
 
         mail(
-            to: email_address_with_name(record.unconfirmed_email, record.full_name), 
+            to: email_address_with_name(recipient_email, record.full_name), 
             subject: I18n.t("core.users/confirmations.mailer_confirmation_instructions_subject")
         )
     end
