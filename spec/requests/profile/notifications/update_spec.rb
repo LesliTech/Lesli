bdd2c0c874cecaf63dc685f39b7e3606886ac236
Luis Donis <ldonis.emc2@gmail.com>
2021-04-26 20:48:18 -0600

    feat: enable user (own) notification system

---
+++ spec/requests/profile/notifications/update_spec.rb
@@ -0,0 +1,66 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+require 'rails_helper'
+require 'spec_helper'
+require 'byebug'
+
+
+RSpec.describe 'PUT:/administration/profile/notifications/:id.json', type: :request do
+    include_context 'user authentication'
+    
+    before(:all) do
+
+        # register a notification to the user, so we have at least one active notification
+        @notification = Courier::Bell::Notification.new(@user, "notification from rspec")
+
+        # mark notification as read
+        put "/administration/profile/notifications/#{ @notification[:id] }.json"
+
+    end
+
+    include_examples 'successful standard json response'
+
+    it 'is expected to respond with notification id marked as read' do
+        expect(@response_body["data"]).to eql(@notification[:id])
+    end
+end
+
+
+RSpec.describe 'PUT:/administration/profile/notifications/all.json', type: :request do
+    include_context 'user authentication'
+    
+    before(:all) do
+
+        # register a notification to the user, so we have at least one active notification
+        (1..12).each do |i|
+            Courier::Bell::Notification.new(@user, "notification from rspec #{ i }")
+        end
+
+        # mark notification as read
+        put "/administration/profile/notifications/all.json"
+
+    end
+
+    include_examples 'successful standard json response'
+
+    it 'is expected to respond with total notifications marked as read' do
+        expect(@response_body["data"]).to eql(12)
+    end
+end
