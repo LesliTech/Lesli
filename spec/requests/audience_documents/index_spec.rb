333cb30c4cfbabac4507d39d049c5af3ce0dad06
diego-alay <dalay@lomax.com.gt>
2021-05-17 09:26:49 -0600

    feat: add tests

---
+++ spec/requests/audience_documents/index_spec.rb
@@ -0,0 +1,37 @@
+=begin
+
+Copyright (c) 2021, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+
+require 'rails_helper'
+require 'spec_helper'
+require 'byebug'
+
+
+RSpec.describe 'GET:/administration/template/audience_documents.json', type: :request do
+    include_context 'user authentication'
+
+    before(:all) do
+        get '/administration/template/audience_documents.json'
+    end
+
+    include_examples 'successful standard json response'
+
+    it 'is expected to respond with an index of audience documents' do
+        expect(@response_body["data"]["pagination"]["count_total"]).to eql(@user.account.template.audience_documents.count)
+    end
+ends
---
+++ spec/requests/audience_documents/update_spec.rb
@@ -0,0 +1,45 @@
+=begin
+
+Copyright (c) 2021, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+
+require 'rails_helper'
+require 'spec_helper'
+require 'byebug'
+
+
+RSpec.describe 'PUT:/administration/template/audience_documents.json', type: :request do
+    include_context 'user authentication'
+
+    before(:all) do
+        @audience_document = @user.account.template.audience_documents.first
+        @audience_document_name = Faker::Book.title
+        put("/administration/template/audience_documents/#{@audience_document.id}.json", params: {
+            audience_document: {
+                name: @audience_document_name
+            }
+        })
+    end
+
+    include_examples 'successful standard json response'
+
+    it 'is expected to update a audience document' do
+        expect(@response_body["data"]["id"]).to eql(@audience_document.id)
+        expect(@response_body["data"]["name"]).to eql(@audience_document.name)
+        expect(@response_body["data"]["model_type"]).to eql(@audience_document.model_type)
+    end
+end
