9d628878ef59f0353eb00e9c900f68ac1b9b4f8e
Johann Quassowski <qmtutor@gmail.com>
2021-05-21 12:40:37 +0200

    feat: inherit from LesliController and WIP to try things out 2361

--- app/controllers/profile/subscriptions_controller.rb
+++ app/controllers/profile/subscriptions_controller.rb
@@ -1,4 +1,8 @@
-class Profile::SubscriptionsController < ApplicationController
+class Profile::SubscriptionsController < ApplicationLesliController
+  skip_before_action :authorize_request
+  skip_before_action :authorize_privileges
+  # TODO fix permissions and remove the skips
+
   def index
     subscriptions = DeutscheLeibrenten::Project::Subscriber.where(users_id: current_user.id)
 
@@ -13,6 +17,7 @@ class Profile::SubscriptionsController < ApplicationController
   def destroy
   end
 
+  # TODO should we not rather have update?
   def edit
   end
 end
