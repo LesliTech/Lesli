5d096ff8f504d6b3b552bb7206d9de1121a50978
volant360 <efrainh12@galileo.edu>
2021-05-21 19:55:53 +0200

    feat: trello/2361 Implemented generic subscriptions index mode in core

--- config/routes/routes_app.rb
+++ config/routes/routes_app.rb
@@ -43,7 +43,11 @@ module RoutesApp
                     resource :profile, only: [:show] do
                         scope module: :profile do
                             resources :notifications, only: [:index, :update]
-                            resources :subscriptions, only: [:index, :destroy, :update]
+                            resources :subscriptions, only: [:index, :destroy, :update] do
+                                collection do
+                                    get :options
+                                end
+                            end
                         end
 
                     end
