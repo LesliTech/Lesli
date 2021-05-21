87373e51317b3ca9d1e4f7bc6e76fcfb45e6360c
The Lesli Development Team <hello@lesli.tech>
2021-05-21 19:55:53 +0200

    feat: trello/2361 Implemented generic subscriptions index mode in core

--- app/controllers/profile/subscriptions_controller.rb
+++ app/controllers/profile/subscriptions_controller.rb
@@ -1,23 +1,40 @@
+=begin
+
+Copyright (c) 2021, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
 class Profile::SubscriptionsController < ApplicationLesliController
-  skip_before_action :authorize_request
-  skip_before_action :authorize_privileges
-  # TODO fix permissions and remove the skips
 
-  def index
-    subscriptions = DeutscheLeibrenten::Project::Subscriber.where(users_id: current_user.id)
-
-    respond_to do |format|
-      format.html {}
-      format.json do
-        respond_with_successful(subscriptions)
-      end
+    def index
+        respond_to do |format|
+            format.html {}
+            format.json do
+                subscriptions = Profile::Subscription.index(current_user, @query)
+                respond_with_successful(subscriptions)
+            end
+        end
     end
-  end
 
-  def destroy
-  end
+    def options
+        respond_with_successful(Profile::Subscription.options(current_user, @query))
+    end
 
-  # TODO should we not rather have update?
-  def edit
-  end
+    def update
+    end
+
+    def destroy
+    end
 end
