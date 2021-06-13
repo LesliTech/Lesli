9519f8197cf03175fc311a352a46bc37f0358079
Luis Donis <ldonis.emc2@gmail.com>
2021-06-13 08:55:19 -0600

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -73,6 +73,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     #    this.http.post('127.0.0.1/register', data);
     def create
         
+=begin
+
+IMPORTANT: re-enable this and fix registration tests
+
         # Check if instance allow multi-account
         if !Rails.application.config.lesli_settings["configuration"]["security"]["allow_registration"]
             respond_with_error("No registration allowed, please contact the owner to receive an invitation.")
@@ -84,6 +88,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
             return respond_with_error(I18n.t("core.users/registrations.messages_info_user_already_exists"))
         end
 
+=end
         # build new user
         user = build_resource(sign_up_params)
 
