3948aaf2e2346fc1e10e4a7bc2b271d04afce98e
FranciscoRosal <frosal@lomax.com.gt>
2021-07-26 13:26:19 -0600

    feat: use union on has privileges query

--- app/models/user.rb
+++ app/models/user.rb
@@ -144,34 +144,31 @@ class User < ApplicationLesliRecord
     #
     #     current_user.has_privileges?(controllers, actions)
     def has_privileges?(controllers, actions)         
-        granted_list_by_role = self.role_privilege_actions
-        .select("bool_or(role_descriptor_privilege_actions.status) as value")
-        .joins(system_action: [:system_controller])
-        .where("system_controllers.name in (?)", controllers)
-        .where("system_controller_actions.name in (?)", actions)
-        .group("system_controller_actions.name")
-        .map(&:value)
-        &.uniq
+        begin
+            granted_list_by_role = self.role_privilege_actions
+            .select("bool_or(role_descriptor_privilege_actions.status) as value")
+            .joins(system_action: [:system_controller])
+            .where("system_controllers.name in (?)", controllers)
+            .where("system_controller_actions.name in (?)", actions)
+            .group("system_controller_actions.name")
 
-        granted_list_by_user = self.user_privilege_actions
-        .select("bool_or(status) as value")
-        .joins(system_action: [:system_controller])
-        .where("system_controllers.name in (?)", controllers)
-        .where("system_controller_actions.name in (?)", actions)
-        .group("system_controller_actions.name")
-        .map(&:value)
-        &.uniq
+            granted_list_by_user = self.user_privilege_actions
+            .select("bool_or(status) as value")
+            .joins(system_action: [:system_controller])
+            .where("system_controllers.name in (?)", controllers)
+            .where("system_controller_actions.name in (?)", actions)
+            .group("system_controller_actions.name")
 
-        granted_by_role = !(granted_list_by_role.include? false)
-        granted_by_user = !(granted_list_by_user.include? false)
+            granted_list = granted_list_by_role.union_all(granted_list_by_user).map(&:value)&.uniq
 
-        granted_by_role = false if granted_list_by_role.empty?
-        granted_by_user = false if granted_list_by_user.empty?
+            granted = !(granted_list.include? false)
+            granted = false if granted_list.empty?
 
-        return granted_by_role || granted_by_user
-    # rescue => exception
-    #     Honeybadger.notify(exception)
-    #     return false
+            return granted
+        rescue => exception
+            Honeybadger.notify(exception)
+            return false
+        end
     end
 
     # @return [Hash]
