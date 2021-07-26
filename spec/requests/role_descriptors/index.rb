62aec78bb54e51bbadf55695b02d77969205e310
FranciscoRosal <frosal@lomax.com.gt>
2021-07-26 07:23:49 -0600

    test: adding tests

---
+++ spec/requests/role_descriptors/index.rb
@@ -0,0 +1,39 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+
+require 'rails_helper'
+require 'spec_helper'
+require 'byebug'
+
+
+RSpec.describe 'GET:/administration/role_descriptors.json', type: :request do
+    include_context 'user authentication'
+
+    before(:all) do
+        get('/administration/role_descriptors.json')
+    end
+
+    include_examples 'successful standard json response'
+
+    it 'is expected to respond with all the role descriptors' do
+        expect(@response_body['successful']['name']).to eql(@name)
+        expect(@response_body['data']['description']).to eql(@description)
+    end
+
+end
