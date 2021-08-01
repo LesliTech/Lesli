e3fb6399937c9b73ac9dd76b72401cf744dcaf0a
Luis Donis <ldonis.emc2@gmail.com>
2021-07-31 20:47:25 -0600

    feat: remove redirection after confirm account through url

---
+++ spec/controllers/devise/confirmations_controller_spec.rb
@@ -0,0 +1,85 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+require 'spec_controller_helper'
+
+RSpec.configure do |config|
+    config.include Devise::Test::IntegrationHelpers
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+end
+
+RSpec.describe Users::ConfirmationsController, type: :controller do
+
+    before :each do
+
+        request.env["HTTP_ACCEPT"] = "text/html"
+        request.env["devise.mapping"] = Devise.mappings[:user]
+
+        # creates a new user for every test
+        @password = "tardis2021$"
+        @user = Account.first.users.create!({
+            email: Faker::Internet.email,
+            password: @password,
+            password_confirmation: @password
+        })
+
+    end
+
+    it "expect to create a user with confirmation token" do 
+        expect(@user.confirmation_token).not_to be_nil
+        expect(@user.confirmation_token).to be_kind_of(String)
+    end
+
+    it "try to confirm account with wrong token param" do
+
+        get :show, params: { :wrong_confirmation_token_param => "" }
+
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("text/html; charset=utf-8")
+
+        expect(flash[:danger]).to be_present
+        expect(flash[:danger]).to eql("The token you provided is not valid")
+
+    end
+
+    it "try to confirm account with no token" do
+
+        get :show, params: { :confirmation_token => "" }
+
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("text/html; charset=utf-8")
+
+        expect(flash[:danger]).to be_present
+        expect(flash[:danger]).to eql("The token you provided is not valid")
+
+    end
+
+    it "confirm account" do
+
+        get :show, params: { :confirmation_token => @user.confirmation_token }
+
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("text/html; charset=utf-8")
+
+        expect(flash[:success]).to be_present
+        expect(flash[:success]).to eql("Your email address has been updated successfully")
+
+    end
+
+end
