f24e7bbceab593290b18afe2f7559fdbd6bf6561
Luis Donis <ldonis.emc2@gmail.com>
2021-08-10 11:35:41 -0600

    add request test for login view

--- spec/views/devise/sessions/new.html.erb_spec.rb
+++ spec/views/devise/sessions/new.html.erb_spec.rb
@@ -15,12 +15,29 @@ For more information read the license file including with this software.
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 // · 
 
+TODO: 
+    - exec login form test
+
 =end
 
 # include helpers, configuration & initializers for request tests
 require 'lesli_request_helper'
 
-RSpec.feature "Widget management", :type => :feature do
+RSpec.describe 'GET:/login', type: :request do
+    it 'is expected to respond with login view' do
+        get '/login'
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("text/html; charset=utf-8")
+        expect(response.body).to include("/password/new")
+        expect(response.body).to include("/register")
+        expect(response.body).to include("/pass/new")
+        expect(response.body).to include("/otp/new")
+    end
+end
+
+
+
+RSpec.feature 'GET:/login', :type => :feature do
     it 'is expected to respond with login view' do
         visit "/login"
         expect(page.status_code).to be(200)
