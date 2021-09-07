9fbb6482fcd70baf32d94aec7a6fe07274d5469d
Luis Donis <ldonis.emc2@gmail.com>
2021-09-06 18:44:49 -0600

    feat: add courier for audit

---
+++ lib/courier/audit/insights/users.rb
@@ -0,0 +1,33 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+module Courier
+    module Audit
+        module Insights
+            class Users
+                def self.index(current_user, query)
+                    return [] unless defined? CloudAudit
+                    cat = query.dig(:filters, :cat) || "by_role"
+                    return CloudAudit::Insights::User.by_role(current_user, query) if cat == "by_role"
+                    return CloudAudit::Insights::User.by_role_date(current_user, query) if cat == "by_role_date"
+                end
+            end
+        end
+    end
+end
