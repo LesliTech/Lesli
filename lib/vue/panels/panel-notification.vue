3c44a83c6b04bea5b87cb0047ac7baf41aa7a97a
Luis Donis <ldonis.emc2@gmail.com>
2021-09-16 12:28:56 -0600

    feat: connect wss notifications with header notification icon and notification panel

--- lib/vue/panels/panel-notification.vue
+++ lib/vue/panels/panel-notification.vue
@@ -34,6 +34,7 @@ export default {
 
     mounted() {
 
+        this.data.global.cloud_bell_notifications = this.lesli.notifications
         this.prepareDesktopNotification();
         this.startListeners();
         
@@ -70,6 +71,9 @@ export default {
         startListeners() {
             this.wss.onmessage = (message) => {
 
+                this.data.global.cloud_bell_notifications++
+
+                // data comes as binary with a json as string
                 let data = JSON.parse(message.data)
 
                 if (!data.subject) {
@@ -123,14 +127,12 @@ export default {
     },
     watch: {
 
-        // update notification count in the main header
+        // update notification count in the main header after mark a notification as read
         'notifications.pagination.count_total': function (val) {
             this.data.global.cloud_bell_notifications = val
         },
-        'data.global.cloud_bell_notification': function(notification) {
-            this.msg.info(notification.subject)
-            var notification = new Notification(notification.subject);
-        },
+
+        // get user notifications every time the user opens the notifications panel
         'data.global.show_panel_notifications': function(open) {
             if (open) {
                 this.getNotifications()
