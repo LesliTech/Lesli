2fe173b1db9df5dbb01e09f3506aa99f8d968540
The Lesli Development Team <hello@lesli.tech>
2021-09-16 13:10:45 -0600

    feat: add migration to rename kind and sender fields in bell notifications

---
+++ db/migrate/20210916185652_rename_fields_in_cloud_bell_notifications.rb
@@ -0,0 +1,6 @@
+class RenameFieldsInCloudBellNotifications < ActiveRecord::Migration[6.1]
+    def change
+        rename_column :cloud_bell_notifications, :kind, :category
+        rename_column :cloud_bell_notifications, :sender, :channel
+    end
+end
