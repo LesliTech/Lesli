912ddc483f09e679419550830da55f18237b1cdd
Luis Donis <ldonis.emc2@gmail.com>
2021-09-17 07:58:25 -0600

    feat: use bell notification service

--- lib/courier/bell/notification.rb
+++ lib/courier/bell/notification.rb
@@ -36,38 +36,16 @@ module Courier
                 CloudBell::Notification.read(current_user, id)
             end
 
-            def self.new(user, subject, role_names:nil, body:nil, url:nil, kind:nil, category:nil)
-                return if not defined? CloudBell
-
-                if not user or role_names
-                    notifications = []
-                    User.joins(:roles).where("roles.name in (?)", role_names).each do |user|
-                        notification = user.account.bell.notifications.create({
-                            category: category,
-                            subject: subject,
-                            status: 'created',
-                            body: body,
-                            user: user,
-                            url: url
-                        })
-
-                        notifications.append({ id: notification.id })
-                    end
-
-                    return notifications
-                else
-                    notification = user.account.bell.notifications.create({
-                        category: category,
-                        subject: subject,
-                        status: 'created',
-                        body: body,
-                        user: user,
-                        url: url
-                    })
-
-                    return { id: notification.id }
-                end
-
+            def self.new(user, subject, role_names:nil, body:nil, url:nil, category:nil)
+                return 0 if not defined? CloudBell
+                CloudBell::NotificationService.generate(
+                    user, 
+                    subject, 
+                    role_names:role_names, 
+                    category:category,
+                    body:body, 
+                    url:url
+                )
             end
 
         end
