1d14bbcc11f21229be84b44ea0b25387d23c5177
Luis Donis <ldonis.emc2@gmail.com>
2021-09-21 08:19:31 -0600

    fix: profile notifications test

--- spec/requests/profile/notifications/update_spec.rb
+++ spec/requests/profile/notifications/update_spec.rb
@@ -17,84 +17,85 @@ For more information read the license file including with this software.
 
 =end
 
-require 'rails_helper'
-require 'spec_helper'
-require 'byebug'
+
+# include helpers, configuration & initializers for request tests
+require 'lesli_request_helper'
 
 
+#
 RSpec.describe 'PUT:/administration/profile/notifications/:id.json', type: :request do
-    include_context 'user authentication'
 
-    before(:all) do
-
-        # register a notification to the user, so we have at least one active notification
-        @notification = Courier::Bell::Notification.new(@user, "notification from rspec")
-
-        # if CloudBell is not installed we'll work with zero as ID
-        @notification_id = @notification ? @notification[:id] : 0
-
-        # mark notification as read
-        put "/administration/profile/notifications/#{ @notification_id }.json"
-
-    end
-
-    include_examples 'successful standard json response'
+    include_context 'request user authentication'
 
     it 'is expected to respond with notification id marked as read' do
-        expect(@response_body["data"]).to eql(@notification_id)
+
+        # register a notification to the user, so we have at least one active notification
+        notification = Courier::Bell::Notification.new(@current_user, "notification from rspec")
+
+        # if CloudBell is not installed we'll work with zero as ID
+        notification_id = notification ? notification[:id] : 0
+
+        # mark notification as read
+        put "/administration/profile/notifications/#{ notification_id }.json"
+
+        expect_json_response_successful
+
+        response_body = response_json
+
+        expect(response_body['data']).to eql(notification_id)
+
     end
+
 end
 
 
 RSpec.describe 'PUT:/administration/profile/notifications/all.json', type: :request do
-    include_context 'user authentication'
 
-    before(:all) do
+    include_context 'request user authentication'
 
-        # register a notification to the user, so we have at least one active notification
+    it 'is expected to respond with total notifications marked as read' do
+
+        # register some notifications to the user, so we have at least one active notification
         (1..12).each do |i|
-            Courier::Bell::Notification.new(@user, "notification from rspec #{ i }")
+            Courier::Bell::Notification.new(@current_user, "notification from rspec #{ i }")
         end
 
         # get number of active notifications
-        @local_count = Courier::Bell::Notification.count(@user, true)
+        local_count = Courier::Bell::Notification.count(@current_user, true)
 
         # mark notification as read
         put "/administration/profile/notifications/all.json"
 
-    end
+        expect_json_response_successful
 
-    include_examples 'successful standard json response'
+        response_body = response_json
 
-    it 'is expected to respond with total notifications marked as read' do
-
-        expect(@response_body["data"]).to eql(@local_count)
+        expect(response_body['data']).to eql(local_count)
 
     end
 end
 
 
 RSpec.describe 'PUT:/administration/profile/notifications/:id.json', type: :request do
-    include_context 'user authentication'
 
-    before(:all) do
+    include_context 'request user authentication'
+
+    it 'is expected to respond with total notifications marked as read' do
 
         # register a notification to all the users of a rol
-        @notifications = Courier::Bell::Notification.new(nil, "notification from rspec", role_names: "owner")
+        notifications = Courier::Bell::Notification.new(nil, "notification from rspec", role_names: "owner")
 
         # get number of active notifications
-        @local_count = Courier::Bell::Notification.count(@user, true)
+        local_count = Courier::Bell::Notification.count(@current_user, true)
 
         # mark notification as read
         put "/administration/profile/notifications/all.json"
 
-    end
+        expect_json_response_successful
 
-    include_examples 'successful standard json response'
+        response_body = response_json
 
-    it 'is expected to respond with total notifications marked as read' do
-
-        expect(@response_body["data"]).to eql(@local_count)
+        expect(response_body['data']).to eql(local_count)
 
     end
 end
