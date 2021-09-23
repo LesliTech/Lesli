e565c5bdd110f933ee69241680c2501003473197
diego-alay <dalay@lomax.com.gt>
2021-09-23 09:02:37 -0600

    fix: avoid validation whene an assignment does not has a category

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,9 +101,9 @@ RSpec.describe "using method (full_name_initials)", type: :model do
     end
 end
 
-RSpec.describe "using method (has_privileges?) to validate if a user has privileges trough the role", type: :model do
+RSpec.describe "using method (has_privileges?) to validate if a user has privileges through the role", type: :model do
     account = User.find_by(email: Rails.application.config.lesli_settings["account"]["user"]["email"]).account # instance of account
-    
+
     # ============ create user =============
     first_name = Faker::Name.last_name
     last_name = Faker::Name.last_name
@@ -121,31 +121,31 @@ RSpec.describe "using method (has_privileges?) to validate if a user has privile
         }
     })
     # ======================================
-    
+
     # =========== create role ==============
     role = User.first.account.roles.create(
         active: true,
         name: Faker::Artist.name,
-        object_level_permission: account.roles.map(&:object_level_permission).min()   
+        object_level_permission: account.roles.map(&:object_level_permission).min()
     )
-    
+
     # ======================================
-     
+
     # ======= create a descriptor and assign privileges =======
     descriptor = account.role_descriptors.find_or_create_by(
         name: Faker::Artist.name
     ) # descriptor creation
-    
-    # add actions to the role descriptor 
-       
+
+    # add actions to the role descriptor
+
     system_actions = SystemController::Action.order(:id)
     limit_index = rand(50..(system_actions.count - 1))
-    
+
     system_actions = system_actions[(limit_index-50)..(limit_index - 1)] # fetch 50 privilege actions
-    
+
     categories = RoleDescriptor::PrivilegeAction.categories.map{|_k,v| v}
     category = categories[rand(categories.length)]
-    
+
     system_actions.each do |system_action|
         descriptor.privilege_actions.find_or_create_by(
             system_action: system_action,
@@ -153,28 +153,27 @@ RSpec.describe "using method (has_privileges?) to validate if a user has privile
             status: true
         )
     end
-    
+
     # ========================================================
-    
+
     # create the descriptor assignment using a random category
     role.descriptor_assignments.find_or_create_by(
         descriptor: descriptor,
         category: category
     )
-    
+
     user.user_roles.create({ role: role })
 
     system_actions = system_actions.concat(
         SystemController::Action.joins(:system_controller).order(:id)[0..19]
     ) # add 20 privilege actions
-    
+
     # validate if a user has enable the controller action on a specific category
     it "Validate privilege actions of the role assigned to the user" do
         system_actions.each do |action|
-            granted = user.has_privileges?([action.system_controller.name], [action.name]) 
-            
+            granted = user.has_privileges?([action.system_controller.name], [action.name])
+
             expect(granted).to eq(user.role_privilege_actions.find_by(system_action: action, category: category, status: true).present?)
         end
     end
 end
-    
