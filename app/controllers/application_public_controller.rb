401ed734609af4b8429cff4d5ab3ab93a968e678
diego-alay <dalay@lomax.com.gt>
2021-09-29 13:57:55 -0600

    feat: include Logger to use user_agent

--- app/controllers/application_public_controller.rb
+++ app/controllers/application_public_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationPublicController < ApplicationController
     include Application::Requester
     include Application::Responder
+    include Application::Logger
 
     before_action :set_locale
 end
