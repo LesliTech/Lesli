47bc05b14fd819fd8268d48decf15f60d9540e1f
Marcos <marcosbonifasi19@gmail.com>
2021-11-09 13:15:18 -0600

    test: tests for settings/options and method of creating integrations

---
+++ spec/requests/account/integrations/destroy_spec.rb
@@ -0,0 +1,35 @@
+=begin
+Copyright (c) 2021, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+
+require "rails_helper"
+require "spec_helper"
+require "byebug"
+
+RSpec.describe "GET:/administration/account/settings/options", type: :request do 
+
+    include_context "user authentication"
+
+    before(:all) do 
+        # get "/administration/account/settings/options.json"
+        puts @user.account.integrations.index
+        @user.account.integrations.destroy_all
+        puts @user.account.integrations.index
+    end
+
+
+end
