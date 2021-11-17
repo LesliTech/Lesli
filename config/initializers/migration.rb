3b64fca9f8428453131529919dd56cfcc64d9175
diego-alay <dalay@lomax.com.gt>
2021-11-17 18:56:51 +0100

    fix: add comments

--- config/initializers/migration.rb
+++ config/initializers/migration.rb
@@ -1,9 +1,25 @@
 module ActiveRecord
     class Migrator
-        def validate(migrations)
-            name, = migrations.group_by(&:name).find { |_, v| v.length > 1 }
+        # @return [void] @override
+        # based on https://github.com/rails/rails/blob/78e402077d4d69b08db7daf8944a32eef0cf6ac4/activerecord/lib/active_record/migration.rb#L1256
+        # @description Lesli platform has its own migration versioning so having the same class name is valid for us when a table is being changed
+        # this function is use to skip the default migration validation of duplicated names if the migration name starts with Alter.
+        # Important notes!!
+        # - This function is called each time that you run rake db:migrate
+        # - when a migration could has the same class name?
+        #   - Migrations names can be duplicated for any change on a table
+        # Example
+        # > v1.0
+        #   0010000310_create_users.rb -> class name: CreateUsers
+        # > v1.1
+        #   0010000311_alter_users.rb  -> class name: AlterUsers
+        # > v1.2
+        #   0010000312_alter_users.rb  -> class name: AlterUsers
 
-            if (name.downcase.include? "alter")
+        def validate(migrations)
+            name, = migrations.group_by(&:name).find { |_, v| v.length > 1 } # obtain migration name
+
+            if (name&.starts_with? "Alter") # validate if migration starts with Alter
 
                 # validate migration version
                 version, = migrations.group_by(&:version).find { |_, v| v.length > 1 }
