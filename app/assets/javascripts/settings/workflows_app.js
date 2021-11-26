e2132d7d254b681034761e88141bbe2f9b378276
Luis Donis <ldonis.emc2@gmail.com>
2021-11-26 10:48:21 -0600

    feat: standad notification colors

--- app/assets/javascripts/settings/workflows_app.js
+++ app/assets/javascripts/settings/workflows_app.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./app/vue/settings/workflows/app.js":
-/*!*******************************************!*\
-  !*** ./app/vue/settings/workflows/app.js ***!
-  \*******************************************/
-/***/ (() => {
-
-eval("\n\n//# sourceURL=webpack://lesli/./app/vue/settings/workflows/app.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./app/vue/settings/workflows/app.js"]();
 /******/ 	
 /******/ })()
 ;
