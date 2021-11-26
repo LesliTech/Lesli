bd9867d41b150441bd586f664972bb57caa51c73
The Lesli Development Team <hello@lesli.tech>
2021-11-26 17:20:02 +0100

    feat: add cron jobs of each engine

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,102 +16,21 @@ For more information read the license file including with this software.
 // ·
 
 =end
-set :output, 'log/cron.log'
 
-# ======================== DeutscheLeibrenten cron jobs
+# path is a variable of wenever class that returns the current path of the application
+RAILS_ROOT = path
 
-# Tasks
-every :day, at: '05:55 am' do
-    rake 'deutsche_leibrenten:cron:send_email_tasks_reminder_by_user' if defined? DeutscheLeibrenten
-end
+#output file
+set :output, "#{RAILS_ROOT}/log/cron.log"
 
-every :day, at: '10:10 pm' do
-    rake 'deutsche_leibrenten:cron:create_reminder_task' if defined? DeutscheLeibrenten
-end
+# require needed to include active-record models and initializers.
+require "#{RAILS_ROOT}/lesli.rb"
 
-every :day, at: '10:20 pm' do
-    rake 'deutsche_leibrenten:cron:send_email_deadline_reminder_tasks_by_user' if defined? DeutscheLeibrenten
-end
+# include all schedule.rb of each engine
+Lesli::engines().map do |engine|
+    schedule_path = "#{RAILS_ROOT}/engines/#{engine[:code]}/schedule.rb"
 
-every :day, at: '10:30 pm' do
-    rake 'deutsche_leibrenten:cron:send_email_users_with_delayed_tasks' if defined? DeutscheLeibrenten
-end
-
-
-# Leads
-every :hour do
-    rake 'deutsche_leibrenten:cron:send_alert_if_no_lead_registered' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '10:00 pm' do
-    rake 'deutsche_leibrenten:cron:send_daily_email_leads_coming' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '11:00 pm' do
-    rake 'deutsche_leibrenten:cron:create_lead_origin_reminder_task' if defined? DeutscheLeibrenten
-end
-
-every 1.month, at: '10:00 pm' do
-    rake 'deutsche_leibrenten:cron:send_monthly_email_leads_coming' if defined? DeutscheLeibrenten
-end
-
-
-# Companies
-every :monday, at: '4:00 am' do
-    rake 'deutsche_leibrenten:cron:send_report_companies_projects' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '11:00 pm' do
-    rake 'deutsche_leibrenten:cron:delete_kops_deadline_reached' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '11:10 pm' do
-    rake 'deutsche_leibrenten:cron:clear_kops_information' if defined? DeutscheLeibrenten
-end
-
-
-# Projects
-every :day, at: '06:00 am' do
-    rake 'deutsche_leibrenten:cron:notify_projects_created_by_callcenter' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '10:50 pm' do
-    rake 'deutsche_leibrenten:cron:delete_projects' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '11:30 am' do
-    rake 'deutsche_leibrenten:cron:create_project_follow_up_reminder_task' if defined? DeutscheLeibrenten
-end
-
-
-# System Maintenance
-every :day, at: '11:10 pm' do
-    rake 'deutsche_leibrenten:cron:upload_local_files_to_s3' if defined? DeutscheLeibrenten
-end
-
-every :day, at: '11:45 pm' do
-    rake 'deutsche_leibrenten:cron:delete_old_local_reports' if defined? DeutscheLeibrenten
-end
-
-
-# Reports
-every :day, at: '09:50 pm' do
-    rake 'deutsche_leibrenten:cron:generate_reports' if defined? DeutscheLeibrenten
-end
-
-
-# Integrations
-every :day, at: '11:15 pm' do
-    rake 'deutsche_leibrenten:cron:test_iframe_integration' if defined? DeutscheLeibrenten
-end
-
-
-# ======================== # Mitwerken Cron Jobs
-
-every :day, at: '06:00 am' do
-    rake 'mitwerken_cloud:cron:notify_outdated_projects' if defined? MitwerkenCloud
-end
-
-every :sunday, at: '10:00 pm' do
-    rake 'mitwerken_cloud:cron:reports:new_users_weekly' if defined? MitwerkenCloud
+    if(File.exist?(schedule_path)) # load file
+        instance_eval(File.read(schedule_path), schedule_path)
+    end
 end
