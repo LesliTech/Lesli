43ec7774b8448c7be8c86ef48b35c8b62caaffc6
The Lesli Development Team <hello@lesli.tech>
2021-11-30 13:47:23 -0600

    feat: first implementation of factorybot, tests of notifications, subscriptions and webpushes

--- spec/requests/profiles/notifications/index_spec.rb
+++ spec/requests/profiles/notifications/index_spec.rb
@@ -27,28 +27,37 @@ RSpec.describe 'GET:/administration/profile/notifications.json', type: :request 
     
     include_context 'request user authentication'
     
-    it 'is expected to respond with all the user\'s notifications' do
+    let(:local_count) { Courier::Bell::Notification.count(@current_user, true) }
+    let(:remote_count) do
+        remote_count = response_body['data']['pagination']['count_total'] if defined?(CloudBell)
+        remote_count = response_body['data'].size if not defined?(CloudBell)
+        remote_count
+    end
+    let(:response_body) { response_json }
 
+    before do 
         # register a notification to the user, so we have at least one active notification
         Courier::Bell::Notification.new(@current_user, "notification from rspec")
 
         # ask for the list of notifications fo the current user
         get '/administration/profile/notifications.json'
+    end
 
-        local_count = Courier::Bell::Notification.count(@current_user, true)
-
+    it 'is expected to respond with all the user\'s notifications' do
         expect_json_response_successful
-
-        response_body = response_json
-
-        remote_count = response_body['data']['pagination']['count_total'] if defined?(CloudBell)
-        remote_count = response_body['data'].size if not defined?(CloudBell)
-
         expect(local_count).to be >=(1) if  defined?(CloudBell)
         expect(remote_count).to be >=(1) if defined?(CloudBell)
 
         expect(remote_count).to eql(local_count)
-
     end
 
+end
+
+RSpec.describe "GET:/administration/profile/notifications.json", type: :request do
+    let(:login) { "/login?r=/administration/profile/notifications.json" }
+    before(:all) { get "/administration/profile/notifications.json" }
+
+    it "is expected to redirect to login when user is not authenticated" do
+        expect(response).to redirect_to(login)
+    end
 end
