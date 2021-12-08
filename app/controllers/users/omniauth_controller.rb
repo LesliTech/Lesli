e905960c1cc4a94410ec25e13dba4390b19a0736
FranciscoRosal <frosal@lomax.com.gt>
2021-12-08 12:52:34 -0600

    feat: creating omniauth callback controller

---
+++ app/controllers/users/omniauth_controller.rb
@@ -0,0 +1,39 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
+class Users::OmniauthController < Devise::OmniauthCallbacksController
+
+    def google_oauth2
+
+        auth_params = request.env['omniauth.auth']
+
+        user = User.omniauth_registration(auth_params)
+
+        if user.persisted?
+            respond_with_successful()
+        else
+            respond_with_error(user.errors.full_messages.to_sentence)
+        end
+
+    end
+
+    def failure
+        redirect_to root_path
+    end
+
+end
