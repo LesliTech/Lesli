c65ea7d0a2d35230ba469b89f01437d0b920b96e
volant360 <efrainh12@galileo.edu>
2021-12-14 16:56:13 +0100

    docs: updated inflections documentation

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,12 +10,19 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
+# Some methods you should use in different scenarios are
+# inflect.plural to specify a different pluralization rule for a noun
+# inflect.singulare to specify a different pluralization rule for a noun
+# inflect.irregular to speficy nouns that do not follow the language standard (like goose and geese)
+# inflect.uncountable to speficy nouns that are the same in singular and plural
+
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym "RESTful"
 # end
 ActiveSupport::Inflector.inflections do |inflect|
   
+    # Leave and Leaves is an irregular noun, so we should specify it so rails can pluralize it properly
     inflect.irregular "leave", "leaves"
   end
   
