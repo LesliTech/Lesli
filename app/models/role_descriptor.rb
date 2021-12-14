80275c33d2bd7370903232f07f251acf8b858435
diego-alay <dalay@lomax.com.gt>
2021-12-14 18:36:33 +0100

    fix: change validator

--- app/models/role_descriptor.rb
+++ app/models/role_descriptor.rb
@@ -27,7 +27,7 @@ class RoleDescriptor < ApplicationLesliRecord
 
     after_commit :clear_cache
 
-    validates_presence_of :name
+    validates :name, presence: :true
 
     def clear_cache
         Rails.cache.delete(user_cache_key('abilities_by_controller'))
