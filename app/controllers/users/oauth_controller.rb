e98592adc58f36a8a1dfeee9fd03598c7b2c87c2
The Lesli Development Team <hello@lesli.tech>
2021-12-20 09:45:01 -0600

    feat: add session validations

--- app/controllers/users/oauth_controller.rb
+++ app/controllers/users/oauth_controller.rb
@@ -26,40 +26,46 @@ class Users::OauthController < Devise::OmniauthCallbacksController
 
         user = User.omniauth_registration(auth_params)
 
-        if user.persisted?
-
-            sign_in(:user, user)
-
-            # register or sync the user with the user representation on Firebase
-            Courier::One::Firebase::User.sync_user(user) if defined? CloudOne
-
-            # register a new unique session
-            current_session = user.sessions.create({
-                :user_agent => get_user_agent,
-                :user_remote => request.remote_ip,
-                :session_source => "devise_standar_session",
-                :last_used_at => LC::Date.now
-            })
-
-            # make session id globally available
-            session[:user_session_id] = current_session[:id]
-
-            # register a successful sign-in log for the current user
-            user.logs.create({ user_sessions_id: session[:user_session_id], title: "session_creation_successful" })
-
-            default_path = user.roles.first&.default_path
-
-            if user.account&.onboarding? && user.has_roles?("owner")
-                default_path = "/onboarding"
-            end
-
-            return redirect_to(default_path) if default_path
-
-            redirect_to("/dashboard")
-
-        else
-            redirect_to(new_user_session_path)
+        unless user.persisted?
+            return redirect_to(new_user_session_path)
         end
+
+        # check if user meet requirements to login
+        user_validation = UserValidationService.new(user).valid?
+
+        # if user do not meet requirements to login
+        unless user_validation.success?
+            return redirect_to(new_user_session_path)
+        end
+
+        sign_in(:user, user)
+
+        # register or sync the user with the user representation on Firebase
+        Courier::One::Firebase::User.sync_user(user) if defined? CloudOne
+
+        # register a new unique session
+        current_session = user.sessions.create({
+            :user_agent => get_user_agent,
+            :user_remote => request.remote_ip,
+            :session_source => "devise_standar_session",
+            :last_used_at => LC::Date.now
+        })
+
+        # make session id globally available
+        session[:user_session_id] = current_session[:id]
+
+        # register a successful sign-in log for the current user
+        user.logs.create({ user_sessions_id: session[:user_session_id], title: "session_creation_successful" })
+
+        default_path = user.roles.first&.default_path
+
+        if user.account&.onboarding? && user.has_roles?("owner")
+            default_path = "/onboarding"
+        end
+
+        return redirect_to(default_path) if default_path
+
+        redirect_to("/dashboard")
 
     end
 
