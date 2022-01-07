79d74e54480ad8cd1a95dc4f5091bc529632a9b0
volant360 <efrainh12@galileo.edu>
2022-01-07 16:21:06 +0100

    feat: added render capability to show component

--- lib/vue/shared/dashboards/apps/show.vue
+++ lib/vue/shared/dashboards/apps/show.vue
@@ -1,174 +1,154 @@
 <script>
-/*
-
-Copyright (c) 2021, all rights reserved.
-
-All the information provided by this platform is protected by international laws related  to 
-industrial property, intellectual property, copyright and relative international laws. 
-All intellectual or industrial property rights of the code, texts, trade mark, design, 
-pictures and any other information belongs to the owner of this platform.
-
-Without the written permission of the owner, any replication, modification,
-transmission, publication is strictly forbidden.
-
-For more information read the license file including with this software.
-
-// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-// · 
-
-*/
+import componentForm from "../components/form.vue"
 
 
-// · Import components
-import componentBaseTable from "LesliWidgets/base/table.vue"
-
-import componentCoreHelloWorld from "LesliWidgets/core/hello-world.vue"
-import componentCoreWorldClock from "LesliWidgets/core/world-clock.vue"
-import componentCoreListSessions from "LesliWidgets/core/list-sessions.vue"
-import componentDriverShowToday from "LesliWidgets/cloud_driver/show-today.vue"
-import componentDriverListEvents from "LesliWidgets/cloud_driver/list-events.vue"
-import componentDriverShowCalendar from "LesliWidgets/cloud_driver/show-calendar.vue"
-import componentFocusChartTasks from "LesliWidgets/cloud_focus/chart-tasks.vue"
-
-
-
-// · 
 export default {
-    components: {
-        'component-base-table': componentBaseTable,
 
-        'component-core-hello-world': componentCoreHelloWorld,
-        'component-core-world-clock': componentCoreWorldClock,
-        'component-core-list-sessions': componentCoreListSessions,
-
-        'component-driver-show-today': componentDriverShowToday,
-        'component-driver-list-events': componentDriverListEvents,
-        'component-driver-show-calendar': componentDriverShowCalendar,
-
-        'component-focus-chart-tasks': componentFocusChartTasks,
-    },
     props: {
-        engine: String
+        cloudEngine: {
+            type: String,
+            required: true
+        },
+
+        engineNamespace: {
+            type: String,
+            required: true
+        },
+        renderComponents: {
+            required: true
+        },
+        appMountPath: {
+            type: String,
+            default: ''
+        },
+        newResourceAnchorPath: {
+            type: String,
+            default: null
+        },
+        newResourceAnchorText: {
+            type: String,
+            default: null
+        }
     },
-    data() {
+    
+    components: {
+        'component-form': componentForm
+    },
+
+    data(){
         return {
-            dashboard: {},
-            ooptions: {}
+            main_route: null,
+            dashboard: null,
+            translations: {
+                main: I18n.t(`${this.cloudEngine.toLowerCase()}.dashboards`),
+                dashboards: I18n.t('core.dashboards'),
+                core: I18n.t('core.shared')
+            },
+            render_default_dashboard: false,
+            configuration_ready: false,
+            dashboard_name: ''
         }
     },
-    mounted() {
 
-        this.shortcuts = shortcuts
-        //this.getDefaultDashboard()
-        this.dashboard = {
-            "id": 1,
-            "name": "Default Dashboard",
-            "default": true,
-            "main": false,
-            "user_main_id": null,
-            "users_id": null,
-            "roles_id": null,
-            "components": [{
-                layout: 5,
-                name: "Active sessions",
-                component_id: "core-list-sessions",
-                custom_configuration: {},
-                query_configuration: {}
-            }, {
-                name: "hello world",
-                component_id: "core-hello-world",
-                layout: 2,
-            }, {
-                name: "events",
-                component_id: "driver-show-calendar",
-                layout: 5,
-            }, {
-                name: "today",
-                component_id: "driver-show-today",
-                layout: 2,
-            }, {
-                name: "Upcoming events",
-                component_id: "driver-list-events",
-                layout: 5,
-            }, {
-                name: "My tasks",
-                component_id: "focus-chart-tasks",
-                layout: 5,
-            }, {
-                name: "Table widget",
-                component_id: 'base-table',
-                type: 'base-table',
-                layout: 5,
-                url: "/administration/users/8/sessions.json",
-                custom_configuration: {
-                    arrangement:{},
-                    url: "/administration/users/8/sessions.json",
-                    columns: ['user_agent', 'created_at_date']
-                },
-                query_configuration: {"filters":{},"pagination":{"per_page":6}}
-            }, {
-                name: "World clock",
-                component_id: 'core-world-clock',
-                layout: 4,
-                custom_configuration: {
-                    time_zones: ['UTC', 'America/Guatemala', 'America/New_York', 'Europe/Berlin']
-                }
-            }]
-        }
+    mounted(){
+        this.setMainRoute()
+        this.setComponents()
+        this.initialize()
     },
+
     methods: {
+        setMainRoute(){
+            this.main_route = `/${this.engineNamespace}/dashboards/default.json`
+        },
 
-        getDefaultDashboard() {
-            
-            this.http.get(this.url[this.engine]("dashboards/default")).then((result) => {
+        setComponents(){
+            // We configure the components based on the 'renderComponents' prop
+            for(let key in this.renderComponents){
+                this.$options.components[key] = this.renderComponents[key]
+            }
+        },
 
-                if (!result.successful) {
+        initialize(){
+            if(this.$route.params.id == this.engineNamespace){
+                this.getDefaultDashboard()
+            }
+            this.configuration_ready = true
+        },
 
+        setDashboardName(dashboard_name){
+            this.dashboard_name = dashboard_name
+        },
+
+        getDefaultDashboard(){
+            this.render_default_dashboard = true
+            this.http.get(this.main_route).then((result)=>{
+                if (result.successful) {
+                    this.dashboard = result.data
+                }else{
                     this.msg.error(result.error.message)
-                    
                 }
-
-                this.dashboard = result.data
-
             }).catch(error => {
-
                 console.log(error)
-
             })
-
         }
-
     }
 }
 </script>
 <template>
-    <section class="application-component lesli-dashboards">
+    <section class="application-component">
+        <div v-if="configuration_ready">
 
+            <div v-if="render_default_dashboard">
+                <component-header 
+                    :title="false"
+                >
+                    <div class="buttons" v-if="newResourceAnchorPath">
+                        <a class="button" :href="newResourceAnchorPath">
+                            <b-icon icon="plus" size="is-small"></b-icon>
+                            <span>{{ newResourceAnchorText }}</span>
+                        </a>
+                    </div>
+                </component-header>
 
-        {{ /* dashboard header */ }}
-        <!-- <component-header title="Dashboard"></component-header> -->
-
-
-        {{ /* show message if there is no dashboard */ }}
-        <div v-if="!dashboard" class="container">
-            <component-data-empty text="empty dashboard"></component-data-empty>
-        </div>
-
-
-        {{ /* render dashboard components */ }}
-        <section v-if="dashboard" class="columns is-multiline mt-1">
-            <div 
-                v-for="component in dashboard.components" 
-                :key="component.id" 
-                :class="['column', `is-${component.layout}`]">
-                <component
-                    class="widget-component"
-                    :is="`component-${ component.component_id }`"
-                    :component="component">
-                </component>
+                <div class="columns is-multiline" v-if="dashboard">
+                    <div v-for="component in dashboard.components" :key="component.id" :class="['column', `is-${component.layout}`]">
+                        <component
+                            :is="`component-${component.component_id.replace(/\_/g,'-')}`"
+                            :dashboard-id="dashboard.id"
+                            :component-id="component.id"
+                            :name="component.name"
+                        >
+                        </component>
+                    </div>
+                </div>
+                <component-data-loading v-else>
+                </component-data-loading>
             </div>
-        </section>
 
 
+            <div v-else>
+                <component-header 
+                    :title="dashboard_name"
+                >
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link class="button" :to="`${appMountPath}/`">
+                                <b-icon icon="list" size="is-small" />
+                                <span>{{ translations.core.view_btn_list }}</span>
+                            </router-link>
+                        </div>
+                    </div>
+                </component-header>
+                <component-form
+                    :set-dashboard-name="setDashboardName"
+                    :render-components="renderComponents"
+                    :cloud-engine="cloudEngine"
+                    :engineNamespace="engineNamespace"
+                    :app-mount-path="appMountPath"
+                >
+                </component-form>
+            </div>
+
+        </div>
     </section>
 </template>
