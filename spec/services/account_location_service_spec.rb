0d5c2b1f1d4594cd9fe4e81f5880ff518b11e250
volant360 <efrainh12@galileo.edu>
2022-03-22 20:49:19 +0100

    test: updated account location test so it only creates 1 account

--- spec/services/account_location_service_spec.rb
+++ spec/services/account_location_service_spec.rb
@@ -25,51 +25,53 @@ require "rails_helper"
 RSpec.describe AccountLocationService, type: :model do
     describe "Check if AccountLocationService create the locations for a new account" do
 
-        let(:new_account){ create(:account) }
+        before(:all) do
+            @new_account = create(:account)
+        end
 
         it "is expected account has empty default location" do
-            location = new_account.locations.find_by(:name => "Empty")
+            location = @new_account.locations.find_by(:name => "Empty")
 
             expect(location.blank?).to eq false
         end
 
         it "is expected account has continent Africa" do
-            location = new_account.locations.find_by(:name => "Africa")
+            location = @new_account.locations.find_by(:name => "Africa")
 
             expect(location.blank?).to eq false
             expect(location.level).to eq "continent"
         end
 
         it "is expected account has continent Asia" do
-            location = new_account.locations.find_by(:name => "Asia")
+            location = @new_account.locations.find_by(:name => "Asia")
 
             expect(location.blank?).to eq false
             expect(location.level).to eq "continent"
         end
 
         it "is expected account has continent Europe" do
-            location = new_account.locations.find_by(:name => "Europe")
+            location = @new_account.locations.find_by(:name => "Europe")
 
             expect(location.blank?).to eq false
             expect(location.level).to eq "continent"
         end
 
         it "is expected account has continent North America" do
-            location = new_account.locations.find_by(:name => "North America")
+            location = @new_account.locations.find_by(:name => "North America")
 
             expect(location.blank?).to eq false
             expect(location.level).to eq "continent"
         end
 
         it "is expected account has continent Oceania" do
-            location = new_account.locations.find_by(:name => "Oceania")
+            location = @new_account.locations.find_by(:name => "Oceania")
 
             expect(location.blank?).to eq false
             expect(location.level).to eq "continent"
         end
 
         it "is expected account has continent South America" do
-            location = new_account.locations.find_by(:name => "South America")
+            location = @new_account.locations.find_by(:name => "South America")
 
             expect(location.blank?).to eq false
             expect(location.level).to eq "continent"
