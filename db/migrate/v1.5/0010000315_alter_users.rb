d7d2f1072a6acb220e3c2b92982865224a4c94f8
Luis Donis <ldonis.emc2@gmail.com>
2022-03-30 13:11:43 -0600

    feat: add user name cache column

---
+++ db/migrate/v1.5/0010000315_alter_users.rb
@@ -0,0 +1,27 @@
+=begin
+
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+
+class AlterUsers < ActiveRecord::Migration[7.0]
+    def change
+        add_column :users, :name, :string
+        User.with_deleted.all.each do |user|
+            user.update(name: user.full_name)
+        end
+    end
+end
