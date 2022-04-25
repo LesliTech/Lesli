d8fadd59baef2756ec47bda25a8e59241994b0c9
Luis Donis <ldonis.emc2@gmail.com>
2022-04-24 22:11:10 -0600

    feat: vue3 as main js environment

---
+++ webpack.config.js
@@ -0,0 +1,135 @@
+"use strict"
+/*
+
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+*/
+
+
+// · Including plugins and dependencies
+var path = require("path")
+var webpack = require("webpack")
+var TerserPlugin = require("terser-webpack-plugin")
+var { VueLoaderPlugin } = require("vue-loader")
+
+
+// · 
+module.exports = env => {
+
+
+    // get specific modules to work with, example: npm run webpack -- babel bell
+    var requested_modules = process.argv.slice(5)
+
+
+    // set mode
+    env.mode = env.mode ? env.mode : "development"
+    env.watch = env.watch ? env.watch : false
+
+
+    // set production boolean
+    var production = env.mode == "production" ? true : false
+
+
+    // Base webpack configuration, this includes only core assets
+    var config = {
+
+        watch: env.watch == "true",
+        mode: production ? "production" : "development",
+
+
+        // Use a custom plugin to remove all the comments
+        // from the javascript files
+        optimization: {
+            minimize: production,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        format: {
+                            comments: false,
+                        },
+                    },
+                    extractComments: false,
+                    parallel: true
+                })
+            ]
+        },
+
+
+        // Defined the core apps as main entry apps
+        // engine javascript apps are defined dynamically
+        entry: {
+            "invites/application": "Lesli/vue3/invites/app.js",
+        },
+
+
+        // Set the name of the compiled javascript files
+        output: {
+            path: __dirname,
+            filename: "../../app/assets/javascripts/[name].js"
+        },
+
+
+        // resolvers & path alias
+        resolve: {
+
+            // include folder so we can import direct from node modules
+            modules: [path.resolve(__dirname, "node_modules")],
+            alias: {
+
+                vue: "vue/dist/vue.esm-bundler.js",
+
+                // Set aliases as shortcuts to import modules
+                Lesli: path.resolve(__dirname, "./app"),
+                LesliVue: path.resolve(__dirname, "./lib/vue3"), 
+
+            }
+        },
+        module: {
+            rules: [{
+                test: /\.vue$/,
+                use: "vue-loader"
+            }, {
+                test: /\.css$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader",   // translates CSS into CommonJS
+                ]
+            }, {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { "targets": "defaults" }]]
+                    }
+                }
+            }]
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+
+            // custom constants
+            new webpack.DefinePlugin({
+                lesli_app_mode_production: JSON.stringify(production),
+                lesli_app_mode_development: JSON.stringify(!production)
+            })
+        ]
+
+    }
+
+    return config
+
+}
