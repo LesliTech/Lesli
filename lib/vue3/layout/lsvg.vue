9692d8e9cbfd4ad33a971eb93a61f1b9443131bf
Luis Donis <ldonis.emc2@gmail.com>
2022-04-28 14:15:00 -0600

    feat: improve responsive for navigation

---
+++ lib/vue3/layout/lsvg.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+        id: {
+            required: true,
+            type: String
+        },
+        size: {
+            default: "24"
+        }
+    }
+}
+</script>
+<template>
+<svg :width="`${size}px`" :height="`${size}px`"><use :xlink:href="'#icon-' + id" /></svg>
+</template>
