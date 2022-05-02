62ae13d2c1338a8aaf672991ad4ef6c1a3e74ffa
Luis Donis <ldonis.emc2@gmail.com>
2022-05-02 11:16:47 -0600

    feat: add basic charts

--- lib/vue3/components/charts/general.vue
+++ lib/vue3/components/charts/general.vue
@@ -17,189 +17,116 @@ For more information read the license file including with this software.
 */
 
 
+// · 
+import { ref, reactive, onMounted } from "vue"
+
+
 // · Import frameworks, libraries and tools
 import ApexCharts from "apexcharts"
 
 
 // · 
 export default {
-    props: {
-        title: {
-            type: String,
-            required: false
-        },
-        type: {
-            type: String,
-            required: true
-        },
-        dataSources: {
-            type: Array,
-            required: true
-        },
-        dataLabels: {
-            type: Array,
-            required: true
-        },
-        optionsBase: {
-            type: Object,
-            required: false
-        },
-        options: {
-            type: Object,
-            required: false
-        }
-    },
-    data() {
-        return {
-            chart: null,
-            options_watcher_enabled: true,
-            chart_options: {
-                series: [],
-                labels: [],
-                grid: {
-                    show: false,
-                    padding: {
-                        top: 0,
-                        right: 0,
-                        bottom: 0,
-                        left: 0
-                    }
-                },
-                chart: {
-                    height: "auto",
-                    type: this.type,
-                    toolbar: {
-                        show: false
-                    }
-                },
-                title: {
-                    text: ''
-                },
-                legend: {
+
+    setup() {
+
+        var options = {
+            series: [],
+            labels: [],
+            grid: {
+                show: false,
+                padding: {
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0
+                }
+            },
+            chart: {
+                height: "auto",
+                type: "line", 
+                toolbar: {
+                    show: false
+                }
+            },
+            title: {
+                text: "hola grafica"
+            },
+            legend: {
+                show: true,
+                position: 'top',
+                horizontalAlign: 'center'
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            //colors: this.lesli.colors("charts"),
+            stroke: { },
+            plotOptions: { },
+            xaxis: {
+                categories: [],
+                tickPlacement: 'between',
+                labels: {
                     show: true,
-                    position: 'top',
-                    horizontalAlign: 'center'
-                },
-                dataLabels: {
-                    enabled: true,
-                },
-                colors: this.lesli.colors("charts"),
-                stroke: { },
-                plotOptions: { },
-                xaxis: {
-                    tickPlacement: 'between',
-                    labels: {
-                        show: true,
-                        rotate: -65,
-                        rotateAlways: false,
-                        trim: false,
-                        offsetY: 5,
-                        style: {
-                            fontSize: '15px'
-                        },
-                    }
-                },
-                yaxis: {
-                    show: false,
+                    rotate: -65,
+                    rotateAlways: false,
+                    trim: false,
+                    offsetY: 5,
+                    style: {
+                        fontSize: '15px'
+                    },
                 }
+            },
+            yaxis: {
+                show: true,
             }
         }
-    },
-    mounted() {
 
-        // merge general options with the specific options of wrapper chart component
-        this.chart_options = { 
-            ...this.chart_options,
-            ...this.optionsBase
+        var optionsBase = {
+            chart: {
+                height: 350, 
+                type: 'line'
+            },
+            stroke: {
+                show: true,
+                curve: 'smooth',
+                lineCap: 'round',
+            }
         }
 
-        this.initChart()
-
-    },
-    methods: {
-
-        initChart() {
-
-            // labels and data series are empty by default due child (wrapper) components 
-            // may set this arrays dynamically, so we have to always listen for changes on
-            // these arrays
-            this.chart_options.labels = []
-            this.chart_options.series = []
-
-            // start chart with unique html id
-            this.chart = new ApexCharts(document.querySelector("#chart-"+this._uid), this.chart_options)
-
-            this.chart.render()
-
-            // update chart with component custom options
-            if (this.options) {
-                this.chart.updateOptions(this.options)
-            }
-
-            // If the information is available from the start, we update the labels and sources
-            if (this.dataLabels) {
-                this.chart.updateOptions({ labels: this.dataLabels })
-            }
-
-            if (this.dataSources) {
-                this.chart.updateSeries(this.dataSources)
-            }
-
+        options = {
+            ...options,
+            ...optionsBase
         }
 
-    },
-    watch: {
 
-        // watch for changes on labels of data series
-        dataLabels() {
-            this.chart.updateOptions({
-                labels: this.dataLabels
-            })
-        },
+        onMounted(() => {
 
-        // watch for changes on data series
-        dataSources() {
-            this.chart.updateSeries(this.dataSources)
-        },
+            options.series = [{
+                name: "Desktops",
+                data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+            }]
 
-        // watch for changes on options, so we can change the behavior of the chart on real time
-        options: {
+            options.labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+            options.xaxis.categories = options.labels
 
-            // watch for every change on the options object
-            deep: true, 
-            handler: function(val) {
-                console.log(val)
+            console.log(options)
 
-                try {
+            var chart = new ApexCharts(document.querySelector("#chart"), options);
+            chart.render();
 
-                    // This condition prevents an infinite loop in the watcher
-                    if(this.options_watcher_enabled){
-
-                        this.options_watcher_enabled = false
-                        this.chart.updateOptions(val)
-
-                        this.$nextTick(()=>{
-                            this.options_watcher_enabled = true
-                        })
-
-                    }
-
-                } catch(error) {
-                    
-                    console.log("there is an error with general chart options watcher: ", error)
-
-                }
-
-            }
-
-        }
-
+        })
     }
 
 }
 </script>
 <template>
     <section>
-        <div :id="'component-chart-'+_uid"></div>
+        <div class="box">
+            <h3>Mi grafica</h3>
+            <p>Mi grafica que muestra datos bonitos</p>
+            <div id="chart"></div>
+        </div>
+        <!-- <div :id="'component-chart-'+_uid"></div> -->
     </section>
 </template>
