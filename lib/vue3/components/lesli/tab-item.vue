dc9a0a004b11a48e9fd7e1758154a630a3dc1557
Luis Donis <ldonis.emc2@gmail.com>
2022-05-16 21:16:37 -0600

    feat: add lesli-tab-item

---
+++ lib/vue3/components/lesli/tab-item.vue
@@ -0,0 +1,38 @@
+<script setup>
+/*
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+
+// · import vue tools
+import { ref, reactive, onMounted, watch, computed, useSlots } from "vue"
+
+// · defining props
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: false
+    }
+})
+
+</script>
+<template>
+    <slot></slot>
+</template>
