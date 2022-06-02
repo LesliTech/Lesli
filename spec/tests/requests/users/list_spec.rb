d08877227e6de7524014e5698862d2cd7f52903b
Luis Donis <ldonis.emc2@gmail.com>
2022-06-02 13:37:40 +0200

    fix: user list tests

--- spec/tests/requests/users/list_spec.rb
+++ spec/tests/requests/users/list_spec.rb
@@ -19,78 +19,83 @@ For more information read the license file including with this software.
 # include helpers, configuration & initializers for request tests
 require "lesli_request_helper"
 
-RSpec.describe "GET:/administration/users/list.json", type: :request do
-    include_context "request user authentication"
+RSpec.describe "Tests for Lesli 3", :unless => defined?(DeutscheLeibrenten) do
 
-    it "is expected to list users" do
-        get "/administration/users/list.json"
+    describe "GET:/administration/users/list.json", type: :request do
+        include_context "request user authentication"
 
-        # shared examples
-        expect_json_response_successful
+        it "is expected to list users" do
+            get "/administration/users/list.json"
 
-        # custom specs
-        expect(response_data).to be_an(Array)
-        expect(response_data.length).to be >= 1
+            # shared examples
+            expect_response_with_successful
 
-        expect(response_data.first).to have_key("id")
-        expect(response_data.first["id"]).to be_a(Numeric)
-        expect(response_data.first["id"]).to be >= 1
+            # custom specs
+            expect(response_body).to be_an(Array)
+            expect(response_body.length).to be >= 1
 
-        expect(response_data.first).to have_key("active")
-        expect(response_data.first["active"]).to be_in([true, false])
+            expect(response_body.first).to have_key("id")
+            expect(response_body.first["id"]).to be_a(Numeric)
+            expect(response_body.first["id"]).to be >= 1
 
-        expect(response_data.first).to have_key("email")
-        expect(response_data.first["email"]).to be_a(String)
+            expect(response_body.first).to have_key("active")
+            expect(response_body.first["active"]).to be_in([true, false])
 
-        expect(response_data.first).to have_key("editable")
-        expect(response_data.first["editable"]).to be_in([true, false])
+            expect(response_body.first).to have_key("email")
+            expect(response_body.first["email"]).to be_a(String)
 
-        expect(response_data.first).to have_key("name")
-        expect(response_data.first["name"]).to be_a(String)        
-    end
+            expect(response_body.first).to have_key("editable")
+            expect(response_body.first["editable"]).to be_in([true, false])
 
-    it "is expected that the user has a valid role" do
-        get "/administration/users/list.json"
+            expect(response_body.first).to have_key("name")
+            expect(response_body.first["name"]).to be_a(String)
 
-        # shared examples
-        expect_json_response_successful
+        end
 
-        # custom specs
-        expect(response_data.first).to have_key("roles")
-        expect(response_data.first["roles"]).to be_an(Array)
-        expect(response_data.first["roles"].length).to be >= 1
+        it "is expected that the user has a valid role" do
+            get "/administration/users/list.json"
 
-        expect(response_data.first["roles"].first).to be_a(Hash)
-        expect(response_data.first["roles"].first).to have_key("id")
-        expect(response_data.first["roles"].first["id"]).to be_a(Numeric)
-        
-        expect(response_data.first["roles"].first).to have_key("name")
-        expect(response_data.first["roles"].first["name"]).to be_a(String)
+            # shared examples
+            expect_response_with_successful
 
-        expect(response_data.first["roles"].first).to have_key("code")
-        expect(response_data.first["roles"].first["code"]).to be_a(String)
+            # custom specs
+            expect(response_body.first).to have_key("roles")
+            expect(response_body.first["roles"]).to be_an(Array)
+            expect(response_body.first["roles"].length).to be >= 1
 
-        expect(response_data.first["roles"].first).to have_key("active")
-        expect(response_data.first["roles"].first["active"]).to be_in([true, false])
+            expect(response_body.first["roles"].first).to be_a(Hash)
+            expect(response_body.first["roles"].first).to have_key("id")
+            expect(response_body.first["roles"].first["id"]).to be_a(Numeric)
+            
+            expect(response_body.first["roles"].first).to have_key("name")
+            expect(response_body.first["roles"].first["name"]).to be_a(String)
 
-        expect(response_data.first["roles"].first).to have_key("default_path") # this prop is nil in some cases and in others string
+            expect(response_body.first["roles"].first).to have_key("code")
+            expect(response_body.first["roles"].first["code"]).to be_a(String)
 
-        expect(response_data.first["roles"].first).to have_key("only_my_data")
-        expect(response_data.first["roles"].first["only_my_data"]).to be_in([true, false])
+            expect(response_body.first["roles"].first).to have_key("active")
+            expect(response_body.first["roles"].first["active"]).to be_in([true, false])
 
-        expect(response_data.first["roles"].first).to have_key("object_level_permission")
-        expect(response_data.first["roles"].first["object_level_permission"]).to be_a(Numeric)
+            expect(response_body.first["roles"].first).to have_key("default_path") # this prop is nil in some cases and in others string
 
-        expect(response_data.first["roles"].first).to have_key("deleted_at")
-        expect(response_data.first["roles"].first["deleted_at"]).to be_nil
+            expect(response_body.first["roles"].first).to have_key("only_my_data")
+            expect(response_body.first["roles"].first["only_my_data"]).to be_in([true, false])
 
-        expect(response_data.first["roles"].first).to have_key("created_at")
-        expect(response_data.first["roles"].first["created_at"]).to be_a(String)
+            expect(response_body.first["roles"].first).to have_key("object_level_permission")
+            expect(response_body.first["roles"].first["object_level_permission"]).to be_a(Numeric)
 
-        expect(response_data.first["roles"].first).to have_key("updated_at")
-        expect(response_data.first["roles"].first["updated_at"]).to be_a(String)
+            expect(response_body.first["roles"].first).to have_key("deleted_at")
+            expect(response_body.first["roles"].first["deleted_at"]).to be_nil
 
-        expect(response_data.first["roles"].first).to have_key("accounts_id")
-        expect(response_data.first["roles"].first["accounts_id"]).to be_a(Numeric)
+            expect(response_body.first["roles"].first).to have_key("created_at")
+            expect(response_body.first["roles"].first["created_at"]).to be_a(String)
+
+            expect(response_body.first["roles"].first).to have_key("updated_at")
+            expect(response_body.first["roles"].first["updated_at"]).to be_a(String)
+
+            expect(response_body.first["roles"].first).to have_key("accounts_id")
+            expect(response_body.first["roles"].first["accounts_id"]).to be_a(Numeric)
+        end
+
     end
 end
