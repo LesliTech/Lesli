a70ea4aefaf2260737a7984c4a76036b3e347311
Andy5am <38666859+Andy5am@users.noreply.github.com>
2022-06-27 13:34:45 -0600

    feat: information on task panel

--- lib/vue2/vue/panels/panel-task.vue
+++ lib/vue2/vue/panels/panel-task.vue
@@ -150,6 +150,16 @@ export default {
                         <a :href="`/${instanceEndpoint}/tasks/${notification.id}/edit`">
                             {{ notification.title }}
                         </a> 
+                        <p v-if="notification.model_global_identifier">
+                                <small class="has-text-grey-light"> 
+                                {{ `${notification.model_global_identifier}` }} 
+                            </small>
+                        </p>
+                        <p v-if="notification.customer_first_name">
+                                <small class="has-text-grey-light"> 
+                                {{ `${translations.focus.tasks.view_text_customer} : ${notification.customer_first_name} ${notification.customer_last_name}` }} 
+                            </small>
+                        </p>
                         <p>
                                 <small class="has-text-grey-light"> 
                                 {{ `${translations.focus.tasks.column_deadline} : ${notification.deadline}` }} 
