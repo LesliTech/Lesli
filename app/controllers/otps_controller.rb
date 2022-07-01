0325b750c501b209ce4282f9fcb4b9937fbfe376
Luis Donis <ldonis.emc2@gmail.com>
2022-07-01 14:30:04 +0200

    feat: implement OTP login with lesli3 design

--- app/controllers/otps_controller.rb
+++ app/controllers/otps_controller.rb
@@ -23,11 +23,8 @@ class OtpsController < ApplicationController
 
     before_action :set_user, only: [:create]
 
-    # GET /otp
-    def show
-
-        # we use "t" as alias for token
-        redirect_to("/otp/new") and return if params[:t].blank?
+    # PUT /otp
+    def update
 
         # alias for token error message
         error_msg = I18n.t("core.shared.messages_danger_not_valid_authorization_token_found")
@@ -36,7 +33,7 @@ class OtpsController < ApplicationController
         digest_token = Devise.token_generator.digest(User::AccessCode, :token, params[:t])
 
         # denied access if can't build the token
-        redirect_to(new_user_session_path, alert: error_msg) and return if digest_token.blank?
+        return respond_with_error(error_msg) if digest_token.blank?
 
         # search for the requested pass
         access_code = User::AccessCode.find_by(token: digest_token, token_type: "otp", last_used_at: nil)
@@ -48,17 +45,22 @@ class OtpsController < ApplicationController
                 token: (params[:t] || "")
             })
 
-            redirect_to(new_user_session_path, alert: error_msg) and return
+            return respond_with_error(error_msg)
 
         end
 
+        log = access_code.user.logs.create({ title: "session_creation_atempt", description: error_msg })
+
         # denied access if token do not meet validations
-        redirect_to(new_user_session_path, alert: error_msg) and return if !access_code.is_valid?
+        return respond_with_error(error_msg) if !access_code.is_valid?
+
+        # cache the user from the access code
+        resource = access_code.user
 
         # check if user meet requirements to create a new session
-        Auth::UserValidationService.new(access_code.user).valid? do |result|
+        Auth::UserValidationService.new(resource).valid? do |result|
             # if user do not meet requirements to create a new session
-            redirect_to(new_user_session_path, alert: error_msg) and return unless result.success?
+            return respond_with_error(error_msg) unless result.success?
         end
 
         # delete used token
@@ -68,41 +70,18 @@ class OtpsController < ApplicationController
         # do a user login
         sign_in(access_code.user)
 
-        # register a new unique session
-        current_session = access_code.user.sessions.create({
-            :user_agent => get_user_agent,
-            :user_remote => request.remote_ip,
-            :session_token => session[:session_id],
-            :session_source => "otp_session",
-            :last_used_at => Time.current
-        })
-
-        # make session id globally available
-        session[:user_session_id] = current_session[:id]
-
         # after session is created
-        Auth::UserSessionService.new(access_code.user).after_create(current_session) do |result|
+        Auth::UserSessionService.new(resource, log).create(get_user_agent, request.remote_ip, "otp_web_session") do |result|
 
-            default_path = result[:default_path]
-
-            # if first loggin for account owner send him to the onboarding page
-            if current_user.account.onboarding? && current_user.has_roles?("owner")
-                default_path = "/onboarding"
-            end
+            # make session id globally available
+            session[:user_session_id] = result[:user_sessions_id]
 
             # respond successful and send the path user should go
-            redirect_to(default_path) and return 
+            return respond_with_successful({ default_path: result[:default_path] })
 
         end
 
-        # redirect to the root path and return 
-        redirect_to("/") and return 
-
     end
-
-    def new
-    end
-
 
     # POST /otps
     def create
