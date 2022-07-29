94487602802db7811baa6a853501c5371b71134c
emersongcardona <60839491+emersongcardona@users.noreply.github.com>
2022-07-29 01:59:33 -0600

    test: profile#show test

--- spec/tests/requests/profiles/show_spec.rb
+++ spec/tests/requests/profiles/show_spec.rb
@@ -12,15 +12,46 @@
 =end
 
     require "lesli_request_helper"
-    RSpec.describe "GET:/administration/profile", type: :request, :unless => defined?(DeutscheLeibrenten) do
-        it "is expected to respond with data equal to nil, when email is not sent" do
-            post("/administration/profile")
-            expect_response_with_successful
-            expect(response_body).to eql({})
-        end
-        it "is expected to respond with otp" do
-            post("/otp.json", params: { email: User.first()[:email] })
-            expect_response_with_successful
-            expect(response_body).to be_a(String)
+    RSpec.describe "GET:/administration/profile.json", type: :request, :unless => defined?(DeutscheLeibrenten) do
+        describe "GET:/administration/profile.json", type: :request do
+            include_context "request user authentication"
+            it "is expected to respond a hash not empty whit diferent key value" do
+                get("/administration/profile.json")
+                expect_response_with_successful
+                expect(response_body).not_to be_nil 
+                expect(response_body).to be_an(Hash)
+                expect(response_body.length).to be >= 1
+                expect(response_body.keys.sort).to eq ["active", "alias", "created_at", "detail_attributes", "editable_security", "email", "full_name", "id", "mfa_enabled", "mfa_method", "roles", "updated_at"].sort
+            end
+
+            it "validate type data of hash" do 
+                get("/administration/profile.json")
+                expect_response_with_successful
+                expect(response_body["id"]).to be_a(Numeric)
+                expect(response_body["email"]).to be_a(String)
+                expect(response_body["alias"]).to be_a(String)
+                expect(response_body["active"]).to be_in([true, false])
+                expect(response_body["created_at"]).to be_a(String)
+                expect(response_body["updated_at"]).to be_a(String)
+                expect(response_body["mfa_enabled"]).to be_in([true, false])
+                expect(response_body["editable_security"]).to be_in([true, false])
+                expect(response_body["mfa_method"]).to be_nil
+            end
+
+            it "validate detail_attributes hash and attributes" do 
+                get("/administration/profile.json")
+                expect_response_with_successful
+                expect(response_body["detail_attributes"]).not_to be_nil 
+                expect(response_body["detail_attributes"]).to be_an(Hash)
+                expect(response_body["detail_attributes"].keys.sort).to eq ["address", "first_name", "last_name", "salutation", "telephone", "title", "work_address", "work_city", "work_region"].sort
+            end
+
+            it "validate roles hash and attributes" do 
+                get("/administration/profile.json")
+                expect_response_with_successful
+                expect(response_body["roles"].first).not_to be_nil 
+                expect(response_body["roles"].first).to be_an(Hash)
+                expect(response_body["roles"].first.keys.sort).to eq ["id", "name"].sort
+            end
         end
     end
