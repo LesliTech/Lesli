8b01f37a31a7de4b67896bbaedc45622f780b241
The Lesli Development Team <hello@lesli.tech>
2022-08-02 10:47:08 -0600

    fix: git commetns

--- spec/tests/requests/profiles/show_spec.rb
+++ spec/tests/requests/profiles/show_spec.rb
@@ -1,5 +1,5 @@
 =begin
-    Copyright (c) 2021, all rights reserved.
+    Copyright (c) 2022, all rights reserved.
     All the information provided by this platform is protected by international laws related  to
     industrial property, intellectual property, copyright and relative international laws.
     All intellectual or industrial property rights of the code, texts, trade mark, design,
@@ -12,7 +12,7 @@
 =end
 
     require "lesli_request_helper"
-    RSpec.describe "GET:/administration/profile.json", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
         describe "GET:/administration/profile.json", type: :request do
             include_context "request user authentication"
             
