a47106a1e236a951b2e2ac48d34d478faec5a4ab
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-08-12 12:54:28 -0400

    fix: move apple map component to lesli components

---
+++ lib/vue3/lesli/components/map/Map.vue
@@ -0,0 +1,124 @@
+<script setup>
+/*
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+// · import vue tools
+import { onMounted } from "vue"
+
+
+// · defining props
+const props = defineProps({
+    // · prop that indicates the distanceView of the map in meters
+    distanceView : {
+        type: Number,
+        required: false,
+        default: 240000,
+    },
+    // · prop that has all the annotation marks to set in the map
+    locations : {
+        type: Object,
+        required: true,
+        default: []
+    }
+})
+
+/**
+ * @description this function is used to initialize apple map
+ */
+function initializeMap(){
+
+    mapkit.init({
+        authorizationCallback: function(done) {
+            //ampkt is const used in javascript_apple_mapkit_js that contains the JWT token for apple maps
+            done(ampkt)
+        }
+    })
+
+    var landmarks = []
+
+    // Landmarks data
+    props.locations.forEach((location) => {
+        landmarks.push({
+            coordinate: new mapkit.Coordinate(Number(location.latitude), Number(location.longitude)),
+            title: location.title,
+            url: location.url ? location.url : "",
+            color: location.color ? location.color : "",
+            })
+    })
+
+    // Landmark annotation callout delegate
+    var CALLOUT_OFFSET = new DOMPoint(-148, -78)
+    var landmarkAnnotationCallout = {
+        calloutElementForAnnotation: function(annotation) {
+            return calloutForLandmarkAnnotation(annotation)
+        },
+    
+        calloutAnchorOffsetForAnnotation: function(annotation, element) {
+            return CALLOUT_OFFSET
+        },
+    
+        calloutAppearanceAnimationForAnnotation: function(annotation) {
+            return ".4s cubic-bezier(0.4, 0, 0, 1.5) 0s 1 normal scale-and-fadein"
+        }
+    }
+
+    // Landmarks annotations
+    var annotations = landmarks.map(function(landmark) {
+        var annotation = new mapkit.MarkerAnnotation(landmark.coordinate, {
+            callout: landmarkAnnotationCallout,
+            color: landmark.color ? landmark.color : "#f52929"
+        })
+        annotation.landmark = landmark
+        return annotation
+    })
+
+    var map = new mapkit.Map("summary-map", { cameraDistance: props.distanceView })
+    map.showItems(annotations)
+
+    // Landmark annotation custom callout
+    function calloutForLandmarkAnnotation(annotation) {
+        var div = document.createElement("div")
+        div.className = "landmark"
+    
+        var title = div.appendChild(document.createElement("h1"))
+        title.textContent = annotation.landmark.title
+    
+        var section = div.appendChild(document.createElement("section"))
+
+        if (annotation.landmark.url){
+            var link = section.appendChild(document.createElement("p"))
+            link.className = "homepage"
+
+            var a = link.appendChild(document.createElement("a"))
+            a.href = annotation.landmark.url
+            a.textContent = "website"
+        }
+
+        return div
+    }
+
+}
+
+// · initializing
+onMounted(() => {
+    initializeMap()
+})
+
+</script>
+
+<template>
+    <div id="summary-map" class="component-summary-map"></div>
+</template>
---
+++ lib/vue3/lesli/components/map/docs.md
@@ -0,0 +1,32 @@
+# Map
+
+The component lesli-map is used to display a map made with apple mapkit.
+
+## Props
+
+**:locations**
+prop that contains all the annotation that would be displayed in the map
+- **type**: Array
+- **required**: true
+- **default**: []
+
+**:distanceView**
+prop that indicates the distanceView of the map in meters
+- **type**: Number
+- **required**: false
+- **default**: 240000 (240km)
+
+## Example 
+```html
+
+<lesli-map
+    :locations="[
+        { latitude: "37.7951315", longitude: "-122.402986", title: "City Hall", color:"#c969e0"},
+        { latitude: "37.7954201", longitude: "-122.39352", title: "Pyramid Tower"},
+        { latitude: "37.8083396", longitude: "-122.415727", title: "Empire Building", url:"https://www.google.com/"}   
+    ]"
+    distanceView = 2500
+>
+</lesli-map>
+
+```
