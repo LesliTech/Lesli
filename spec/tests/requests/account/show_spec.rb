4efaaec95d960a8488a6a2c5436ec4cd00e601a0
The Lesli Development Team <hello@lesli.tech>
2022-08-16 09:46:28 -0600

    test: show administation account

---
+++ spec/tests/requests/account/show_spec.rb
@@ -0,0 +1,38 @@
+=begin
+
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+
+
+require "lesli_request_helper"
+
+RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    describe "GET:/administration.json", type: :request do
+        include_context "request user authentication"
+        
+        it "is expected pass share example response with successful" do
+            get("/administration.json")
+
+            #share example
+            expect_response_with_successful
+            puts "respuesta #{response_body}"
+            puts "cuenta #{@current_user}"
+            
+
+        end
+    end
+end
