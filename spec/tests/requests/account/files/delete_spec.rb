3ad16f9364a6d58b32888757aa13f14550a464b4
The Lesli Development Team <hello@lesli.tech>
2022-08-17 11:29:41 -0600

    test: delete and show debbugers

--- spec/tests/requests/account/files/delete_spec.rb
+++ spec/tests/requests/account/files/delete_spec.rb
@@ -21,7 +21,7 @@ For more information read the license file including with this software.
 require "lesli_request_helper"
 
 RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
-    describe "GET:/administration/account/files/:id.json", type: :request do
+    describe "DELETE:/administration/account/files/:id.json", type: :request do
         include_context "request user authentication"
         
         it "is expected to response with successful image" do
@@ -35,35 +35,10 @@ RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheL
                 attachment: fixture_file_upload("lesli-icon.png", "image/png"),
             })
             
-            get("/administration/account/files/#{file_subject.id}.json")
-            puts "/administration/account/files/#{file_subject.id}.json"
-            puts "respuesta #{response}"
-            #respond with an image
-            expect(response).to have_http_status(:success)
-            expect(response.content_type).to be_a(String)
-            expect(response.content_type).to eq("image/png")
-            expect(response.body).to be_a(String)
+            delete("/administration/account/files/#{file_subject.id}.json")
+            #share example
+            expect_response_with_successful
             
         end
-
-        it "is expected to response with fail image" do
-
-            @current_user.account.files.destroy_all
-        
-            file_subject = @current_user.account.files.create!({
-                name: "lesli-icon",
-                file_type: "app_logo",
-                user_creator: @current_user,
-                attachment: fixture_file_upload("lesli-icon.png", "image/png"),
-            })
-            
-
-            get("/administration/account/files/#{file_subject.id + 1 }.json")
-            
-            #respond with an error when id is not valid
-            expect(response_body).to be_a(Hash)
-            expect(response_body).to have_key("message")
-            expect(response_body["message"]).to be_a(String)
-        end
-    end
-end
+    end 
+end 
