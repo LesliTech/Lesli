0298c95e3f3511770fcfe2c745a4eabe14105097
The Lesli Development Team <hello@lesli.tech>
2022-08-22 14:10:15 -0600

    fix: documentation of select and dropdown

--- lib/vue3/lesli/components/dropdown/Dropdown.vue
+++ lib/vue3/lesli/components/dropdown/Dropdown.vue
@@ -19,6 +19,9 @@ For more information read the license file including with this software.
 // · import vue tools
 import { computed, ref } from "vue"
 
+// · import components
+import DropdownItem from "./DropdownItem.vue"
+
 // · defining translations
 const translations = {
     core: {
@@ -42,17 +45,23 @@ const props = defineProps({
 // · defining emits 
 const emit = defineEmits(['update:modelValue']);
 
+/**
+ * @param {*} string to generate unique id
+ * @description generate unique id for the dropdown
+ * @returns {string} unique id
+ */
+const generateId = (string) => string + '-' + Math.random().toString(36).slice(3, 9)
+
 // · variable used to show the dropdown menu
 const showDropdown = ref(false)
 
 // · unique id for the dropdown
-const dropdownMenuId = ref(Math.random().toString(36).slice(3, 9) + '-dropdown-menu')
-
+const dropdownMenuId = ref(generateId('dropdown-menu'))
+const dropdownClickeableClass = ref(generateId('dropdown-clickeable'))
 
 const isActiveDropdown = computed(() => {
     return showDropdown.value ? "is-active" : ""
 })
-
 
 /**
  * @description This function toggles the dropdown menu
@@ -72,27 +81,27 @@ const onSelectOption = (selectedOption) => {
 
 // when user click outside the dropdown, close the dropdown
 window.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("dropdown-clickeable")) showDropdown.value = false
+    if (!e.target.classList.contains(dropdownClickeableClass.value)) showDropdown.value = false
 })
 </script>
 
 <template>
-    <div @click="onDropdown"  class="dropdown" :class="isActiveDropdown">
+    <div @click="onDropdown" :class="['dropdown', isActiveDropdown ]">
         <div class="dropdown-trigger">
             <button
-                class="button is-primary dropdown-clickeable"
+                :class="['button', 'is-primary', dropdownClickeableClass]"
                 aria-haspopup="true"
                 :aria-controls="dropdownMenuId"
             >
-                <span class="dropdown-clickeable">{{ props.label }}</span>
+                <span :class="dropdownClickeableClass">{{ props.label }}</span>
                 <span class="icon is-small">
-                    <span class="material-icons dropdown-clickeable">arrow_drop_down</span>
+                    <span :class="['material-icons', dropdownClickeableClass]">arrow_drop_down</span>
                 </span>
             </button>
         </div>
         <div class="dropdown-menu" :id="dropdownMenuId" role="menu">
             <div class="dropdown-content">
-                <a v-for="option in props.options" @click="onSelectOption(option)" :key="option.value" class="dropdown-item">{{ option.label }}</a>
+                <dropdown-item v-for="(option, i) in props.options" :key="i" :option="option" @click="onSelectOption"></dropdown-item>
             </div>
         </div>
     </div>
