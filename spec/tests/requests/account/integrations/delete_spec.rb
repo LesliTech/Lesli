66c9e50e247779d7b19048f53f36866b980c7caa
The Lesli Development Team <hello@lesli.tech>
2022-08-22 10:55:56 -0600

    test: delete test

---
+++ spec/tests/requests/account/integrations/delete_spec.rb
@@ -0,0 +1,44 @@
+=begin
+
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+require 'lesli_request_helper'
+
+
+RSpec.describe "DELETE:/administration/account/integrations/:id.json", type: :request do
+    describe "DELETE:/administration/account/integrations/:id.json", type: :request do
+    include_context 'request user authentication'
+
+        it 'is expected to respond succesful' do
+
+            puts "buscar id integrations #{@current_user.account.integrations.to_json}"
+            delete("/administration/account/integrations/new.json")
+
+            #share examples
+            expect_response_with_successful
+            puts "respuesta #{response_body}"
+        end
+    end
+end
+
+
+
+
+
+
+
+/administration/account/integrations/:id
