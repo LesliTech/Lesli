8ec76668c4544e4169dd17f976faccf542016800
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-08-24 13:23:40 -0400

    feat: migration from vue2 to vue3 for account settings

---
+++ app/vue3/administration/apps/account_settings/components/form-security.vue
@@ -0,0 +1,171 @@
+<script setup>
+/*
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+// · import vue tools
+import { onMounted, computed, ref } from "vue"
+
+// · import lesli stores
+import { useAccountSettings } from "Lesli/vue3/administration/stores/accountSettings"
+
+// · implement stores
+const storeAccountSettings = useAccountSettings()
+
+// . declare variables 
+const enforcementPassword = ref({ label:"", value:"" })
+
+function updateEnforcement() {
+    console.log(enforcementPassword)
+    settings.value.password_enforce_complexity = enforcementPassword.value.value
+}
+
+// · translations
+const translations = {
+    core: {
+        accounts: I18n.t("core.accounts"),
+        shared: I18n.t('core.shared'),
+        account: {
+            settings: I18n.t("core.account/settings"),
+        }
+    },
+}
+
+onMounted(() => {
+    storeAccountSettings.getSettings()
+});
+
+// . get reactive info from onboarding store
+const settings = computed(() => storeAccountSettings.settings)
+
+</script>
+<template>
+    <form @submit.prevent="storeAccountSettings.postSettings">
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_complex_enable }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <lesli-select
+                    :options="[
+                        {
+                            label: translations.core.account.settings.view_text_password_enforcement_enabled,
+                            value: '1',
+                        },
+                        {
+                            label: translations.core.account.settings.view_text_password_enforcement_disabled,
+                            value: '0',
+                        },
+                    ]"
+                    v-model="enforcementPassword"
+                    @change="updateEnforcement"
+                >
+                </lesli-select>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_lowercase_count }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <div class="control">
+                    <input class="input" type="number" v-model="settings.password_lowercase_count">
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_uppercase_count }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <div class="control">
+                    <input class="input" type="number" v-model="settings.password_uppercase_count">
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_numeric_count }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <div class="control">
+                    <input class="input" type="number" v-model="settings.password_digit_count">
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_special_char_count }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <div class="control">
+                    <input class="input" type="number" v-model="settings.password_special_char_count">
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_minimum_length }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <div class="control">
+                    <input class="input" type="number" v-model="settings.password_minimum_length">
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.account.settings.column_password_expiration_time_days }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <div class="control">
+                    <input class="input" type="number" v-model="settings.password_expiration_time_days">
+                </div>
+            </div>
+        </div>
+
+        <div class="control">
+            <lesli-button class="button is-primary">
+                <span v-if="storeAccountSettings.submitting_form">
+                    {{translations.core.shared.view_btn_saving}}
+                </span>
+                <span v-else>
+                    {{translations.core.shared.view_btn_save}}
+                </span>
+            </lesli-button>
+        </div>
+    </form>
+</template>
---
+++ app/vue3/administration/apps/account_settings/components/form-time.vue
@@ -0,0 +1,247 @@
+<script setup>
+/*
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+// · import vue tools
+import { onMounted, computed, ref } from "vue"
+
+// · import lesli stores
+import { useAccountSettings } from "Lesli/vue3/administration/stores/accountSettings"
+
+// · implement stores
+const storeAccountSettings = useAccountSettings()
+
+// · translations
+const translations = {
+    core: {
+        onboardings: I18n.t("core.onboardings"),
+        accounts: I18n.t("core.accounts"),
+        account_settings: I18n.t("core.account/settings"),
+        shared: I18n.t('core.shared'),
+    },
+};
+
+onMounted(() => {
+    storeAccountSettings.getOptions()
+    storeAccountSettings.getSettings()
+})
+
+// . get reactive info from onboarding store
+const companyInfo = computed(() => storeAccountSettings.companyInfo)
+const settings = computed(() => storeAccountSettings.settings)
+
+// . declare variables
+const timezoneValue = ref([])
+const timeFormat = ref({ label:"", value:"" })
+const dateFormat = ref({ label:"", value:"" })
+const dateWords = ref({ label:"", value:"" })
+const datetimeWords = ref(dateWords.value.label + timeFormat.value.label )
+const combinedDate = ref(dateFormat.value.label + timeFormat.value.label )
+
+// . Functions for updating date format values in store 
+
+function updateTimezone() {
+    settings.value.datetime_time_zone = timezoneValue.value.value
+}
+function updateDatetime() {
+    combinedDate.value = dateFormat.value.label +" "+ timeFormat.value.label
+    settings.value.datetime_format_date_time = dateFormat.value.value +" "+timeFormat.value.value
+}
+
+function updateDate() {
+    settings.value.datetime_format_date = dateFormat.value.value
+    updateDatetime()
+}
+
+function updateTime() {
+    settings.value.datetime_format_time = timeFormat.value.value
+    updateDatetime()
+    updateDateWords()
+}
+
+function updateDateWords(){
+    datetimeWords.value = dateWords.value.label +" "+ timeFormat.value.label
+    settings.value.datetime_format_date_time_words = dateWords.value.value +" "+timeFormat.value.value
+}
+
+</script>
+<template>
+    <form @submit.prevent="storeAccountSettings.postSettings">
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{translations.core.account_settings.column_time_zone}}
+                </label>
+            </div>
+            <div class="column is-6">
+                <lesli-select
+                    :options="storeAccountSettings.options.time_zones"
+                    v-model="timezoneValue"
+                    @change="updateTimezone"
+                >
+                </lesli-select>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{translations.core.onboardings.view_text_date_format}}
+                </label>
+            </div>
+
+            <div class="column is-6">
+                <lesli-select
+                    :options="[
+                        {
+                            label: '24.03.2018',
+                            value: '%d.%m.%Y',
+                        },
+                        {
+                            label: '24-03-2018',
+                            value: '%d-%m-%Y',
+                        },
+                        {
+                            label: '24/03/2018',
+                            value: '%d/%m/%Y',
+                        },
+                    ]"
+                    v-model="dateFormat"
+                    @change="updateDate"
+                >
+                </lesli-select>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{translations.core.onboardings.view_text_time_format}}
+                </label>
+            </div>
+            <div class="column is-6">
+                <lesli-select
+                    :options="[
+                        {
+                            label: '10:30',
+                            value: '%I:%M',
+                        },
+                        {
+                            label: '10:30:20',
+                            value: '%I:%M:%S',
+                        },
+                        {
+                            label: '10:30 PM',
+                            value: '%I:%M %p',
+                        },
+                        {
+                            label: '22:30',
+                            value: '%H:%M',
+                        },
+                        {
+                            label: '22:30:20',
+                            value: '%H:%M:%S',
+                        },
+                    ]"
+                    v-model="timeFormat"
+                    @change="updateTime"
+                >
+                </lesli-select>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{translations.core.onboardings.view_text_format_combined}}
+                </label>
+            </div>
+            <div class="column is-6">
+                <input
+                    class="input"
+                    type="text"
+                    readonly
+                    v-model="combinedDate"
+                />
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.onboardings.view_text_date_text}}
+                </label>
+            </div>
+            <div class="column is-6">
+                <lesli-select
+                    :options="[
+                        {
+                            label: 'Saturday, March 24, 2018',
+                            value: '%A, %B %d, %Y',
+                        },
+                        {
+                            label: 'Sat, Mar 24, 2018',
+                            value: '%a, %b %d, %Y',
+                        },
+                        {
+                            label: 'Mar 24, 2018',
+                            value: '%b %d, %Y',
+                        },
+                        {
+                            label: 'March 24, 2018',
+                            value: '%B %d, %Y',
+                        }
+                    ]"
+                    v-model="dateWords"
+                    @change="updateDateWords"
+                >
+                </lesli-select>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-4">
+                <label class="label">
+                    {{ translations.core.onboardings.view_text_time_text }}
+                </label>
+            </div>
+            <div class="column is-6">
+                <input
+                    class="input"
+                    type="text"
+                    readonly
+                    v-model="datetimeWords"
+                />
+            </div>
+        </div>
+
+        <div class="control">
+            <button class="button is-primary">
+                <span v-if="storeAccountSettings.submitting_form">
+                    <b-icon icon="circle-notch" custom-class="fa-spin" size="is-small" />
+                    &nbsp;
+                    {{translations.core.shared.view_btn_saving}}
+                </span>
+                <span v-else>
+                    <b-icon icon="save" size="is-small" />
+                    &nbsp;
+                    {{translations.core.shared.view_btn_save}}
+                </span>
+            </button>
+        </div>
+    </form>
+</template>
