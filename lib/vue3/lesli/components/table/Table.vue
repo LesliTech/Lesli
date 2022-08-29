6690fd899335e72c01025f5fa9f09f7db238324f
The Lesli Development Team <hello@lesli.tech>
2022-08-29 07:34:32 -0400

    fix: ident

--- lib/vue3/lesli/components/table/Table.vue
+++ lib/vue3/lesli/components/table/Table.vue
@@ -191,7 +191,7 @@
                             every row of the table (i)
                         -->
                         <td v-if="slots.options" class="options">
-                            <div :class="['dropdown', 'is-right is-hoverable', { 'is-active': dropdownActive[i] }, {'is-up': i==(Object.keys(props.records).length-1) }]">
+                            <div :class="['dropdown', 'is-right is-hoverable', { 'is-active': dropdownActive[i] }]">
                                 <div class="dropdown-trigger">
                                     <button class="button has-text-info" 
                                         @blur="dropdownActive[i] = false"
