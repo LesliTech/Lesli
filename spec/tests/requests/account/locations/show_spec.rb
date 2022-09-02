de40157890ada995ffa772303d2fbee069b71f32
emerson cardona <emersongcardona@gmail.com>
2022-09-01 22:50:18 -0600

    test: location show test

--- spec/tests/requests/account/locations/show_spec.rb
+++ spec/tests/requests/account/locations/show_spec.rb
@@ -16,101 +16,98 @@ For more information read the license file including with this software.
 
 =end
 
-require "rails_helper"
-require "spec_helper"
-require "byebug"
+require "lesli_request_helper"
 
-RSpec.describe "GET:/administration/account/locations/:id", type: :request do
+RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    describe "GET:/administration/account/locations/:id", type: :request do
+        include_context "request user authentication"
 
-    include_context "user authentication"
+        it "is expect to respond with a @location found and shown" do
+            @new_location_data = {
+                :name => Faker::Nation.capital_city,
+                :short_name => nil,
+                :postal_code => nil,
+                :latitude => nil,
+                :longitude => nil,
+                :code => nil,
+                :level => "region",
+                :native_level => nil,
+                :parent_id => nil,
+            }
 
-    before(:all) do
+            @location = @current_user.account.locations.new(@new_location_data)
+            @location.save
 
-        @new_location_data = {
-            :name => Faker::Nation.capital_city,
-            :short_name => nil,
-            :postal_code => nil,
-            :latitude => nil,
-            :longitude => nil,
-            :code => nil,
-            :calling_code => nil,
-            :level => "region",
-            :native_level => nil,
-            :parent_id => nil,
-            :accounts_id => @user.account.id
+            get "/administration/account/locations/#{@location.id}.json"
 
-        }
+            #share example
+            expect_response_with_successful
 
-        @location = @user.account.locations.new(@new_location_data)
-        @location.save
+            #validate data
+            expect(response_body).to be_a(Hash)
 
-        get "/administration/account/locations/#{@location.id}.json"
-    end
+            expect(response_body).to have_key("id")
+            expect(response_body["id"]).to be_a(Numeric)
 
-    include_examples "successful standard json response"
+            expect(response_body).to have_key("name")
+            expect(response_body["name"]).to be_a(String)
+            expect(response_body["name"]).to eql(@location[:name])
 
-    it "is expect to respond with a location found and shown" do
+            expect(response_body).to have_key("short_name")
+            expect(response_body["short_name"]).to be_nil
+            expect(response_body["short_name"]).to eql(@location[:short_name])
 
-        expect(@response_body_data).to be_a(Hash)
+            expect(response_body).to have_key("postal_code")
+            expect(response_body["postal_code"]).to be_nil
+            expect(response_body["postal_code"]).to eql(@location[:postal_code])
 
-        expect(@response_body_data).to have_key("id")
-        expect(@response_body_data["id"]).to be_a(Numeric)
-        expect(@response_body_data["id"]).to eql(@location.id)
+            expect(response_body).to have_key("latitude")
+            expect(response_body["latitude"]).to be_nil
+            expect(response_body["latitude"]).to eql(@location[:latitude])
 
-        expect(@response_body_data).to have_key("name")
-        expect(@response_body_data["name"]).to be_a(String)
-        expect(@response_body_data["name"]).to eql(@location[:name])
+            expect(response_body).to have_key("longitude")
+            expect(response_body["longitude"]).to be_nil
+            expect(response_body["longitude"]).to eql(@location[:longitude])
 
-        expect(@response_body_data).to have_key("short_name")
-        expect(@response_body_data["short_name"]).to be_nil
-        expect(@response_body_data["short_name"]).to eql(@location[:short_name])
+            expect(response_body).to have_key("code")
+            expect(response_body["code"]).to be_nil
+            expect(response_body["code"]).to eql(@location[:code])
 
+            expect(response_body).to have_key("level")
+            expect(response_body["level"]).to be_a(String)
+            expect(response_body["level"]).to eql(@location[:level])
 
-        expect(@response_body_data).to have_key("postal_code")
-        expect(@response_body_data["postal_code"]).to be_nil
-        expect(@response_body_data["postal_code"]).to eql(@location[:postal_code])
+            expect(response_body).to have_key("native_level")
+            expect(response_body["native_level"]).to be_nil
+            expect(response_body["native_level"]).to eql(@location[:native_level])
 
+            expect(response_body).to have_key("parent_id")
+            expect(response_body["parent_id"]).to be_nil
+            expect(response_body["parent_id"]).to eql(@location[:parent_id])
 
-        expect(@response_body_data).to have_key("latitude")
-        expect(@response_body_data["latitude"]).to be_nil
-        expect(@response_body_data["latitude"]).to eql(@location[:latitude])
+            expect(response_body).to have_key("deleted_at")
+            expect(response_body["deleted_at"]).to be_nil
 
-        expect(@response_body_data).to have_key("longitude")
-        expect(@response_body_data["longitude"]).to be_nil
-        expect(@response_body_data["longitude"]).to eql(@location[:longitude])
+            expect(response_body).to have_key("created_at")
+            expect(response_body["created_at"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("code")
-        expect(@response_body_data["code"]).to be_nil
-        expect(@response_body_data["code"]).to eql(@location[:code])
+            expect(response_body).to have_key("updated_at")
+            expect(response_body["updated_at"]).to be_a(String)
+        end
 
-        expect(@response_body_data).to have_key("calling_code")
-        expect(@response_body_data["calling_code"]).to be_nil
-        expect(@response_body_data["calling_code"]).to eql(@location[:calling_code])
+        it "is expect to respond with not found when search invalid id" do
 
-        expect(@response_body_data).to have_key("level")
-        expect(@response_body_data["level"]).to be_a(String)
-        expect(@response_body_data["level"]).to eql(@location[:level])
+            @location_id = @current_user.account.locations.last.id
 
-        expect(@response_body_data).to have_key("native_level")
-        expect(@response_body_data["native_level"]).to be_nil
-        expect(@response_body_data["native_level"]).to eql(@location[:native_level])
+            get "/administration/account/locations/#{@location_id + 1}.json"
 
-        expect(@response_body_data).to have_key("parent_id")
-        expect(@response_body_data["parent_id"]).to be_nil
-        expect(@response_body_data["parent_id"]).to eql(@location[:parent_id])
+            #share example
+            expect_response_with_not_found
 
-        expect(@response_body_data).to have_key("deleted_at")
-        expect(@response_body_data["deleted_at"]).to be_nil
+            #validate response
+            expect(response_body).to have_key("message")
+            expect(response_body["message"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("created_at")
-        expect(@response_body_data["created_at"]).to be_a(String)
-
-        expect(@response_body_data).to have_key("updated_at")
-        expect(@response_body_data["updated_at"]).to be_a(String)
-
-        expect(@response_body_data).to have_key("accounts_id")
-        expect(@response_body_data["accounts_id"]).to be_a(Numeric)
-        expect(@response_body_data["accounts_id"]).to eql(@location[:accounts_id])
-
+        end
     end
 end
