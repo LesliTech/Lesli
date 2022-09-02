82e83a5f65b445a1b36e1bf74e9f1e5ca928d5ea
The Lesli Development Team <hello@lesli.tech>
2022-09-02 12:42:32 -0600

    test: index test succesful

--- spec/tests/requests/account/cronos/show_spec.rb
+++ spec/tests/requests/account/cronos/show_spec.rb
@@ -16,111 +16,107 @@ For more information read the license file including with this software.
 
 =end
 
-require "rails_helper"
-require "spec_helper"
-require "byebug"
+require "lesli_request_helper"
 
-RSpec.describe "GET:/administration/account/cronos/:id", type: :request do
-    include_context "user authentication"
+RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    describe "GET:/administration/account/cronos/:id.json", type: :request do
+        include_context "request user authentication"
 
-    before(:all) do
+        it "is expected to respond with  created cronos information" do
+            @current_user.account.cronos.destroy_all
 
-        @account_crono = @user.account.cronos.create!({
-            :name => Faker::Lorem.word,
-            :description => Faker::Lorem.paragraph,
-            :minute => Time.now.min,
-            :hour => Time.now.hour,
-            :day_of_month => Time.now.day,
-            :month => Time.now.month,
-            :day_of_week => Time.now.wday,
-            :users_id => @user.id
-        })
+            crono = @current_user.account.cronos.new({
+                :name => Faker::Lorem.word,
+                :description => Faker::Lorem.paragraph,
+                :minute => Time.now.min,
+                :hour => Time.now.hour,
+                :day_of_month => Time.now.day,
+                :month => Time.now.month,
+                :day_of_week => Time.now.wday,
+                :task_name =>  Faker::Lorem.word, 
+                :engine_code => Faker::Lorem.word
+            })
+            crono.save
+            
+            get("/administration/account/cronos/#{crono.id}.json")
 
-        get "/administration/account/cronos/#{@account_crono.id}.json"
-    end
+            #share examples
+            expect_response_with_successful
+    
+            expect(response_body).to be_a(Hash)
+            expect(response_body).to have_key("id")
+            expect(response_body["id"]).to be_a(Numeric)
 
-    include_examples "successful standard json response"
+            expect(response_body).to have_key("name")
+            expect(response_body["name"]).to be_a(String)
+            expect(response_body["name"]).to eql(crono[:name])
 
-    it "is expected to respond with a crono created successfully" do
-        # PENDING, CONTROLLER IS NOT WORKING CORRECTLY
-        expect(@response_body_data).to be_a(Hash)
+            expect(response_body).to have_key("description")
+            expect(response_body["description"]).to be_a(String)
+            expect(response_body["description"]).to eql(crono[:description])
 
-        expect(@response_body_data).to have_key("id")
-        expect(@response_body_data["id"]).to be_a(Numeric)
+            expect(response_body).to have_key("status")
+            expect(response_body["status"]).to be_nil
 
-        expect(@response_body_data).to have_key("name")
-        expect(@response_body_data["name"]).to be_a(String)
-        expect(@response_body_data["name"]).to eql(@account_crono[:name])
+            expect(response_body).to have_key("engine_code")
+            expect(response_body["engine_code"]).to be_a(String)
+            expect(response_body["engine_code"]).to eql(crono[:engine_code])
 
-        expect(@response_body_data).to have_key("description")
-        expect(@response_body_data["description"]).to be_a(String)
-        expect(@response_body_data["description"]).to eql(@account_crono[:description])
+            expect(response_body).to have_key("task_name")
+            expect(response_body["task_name"]).to be_a(String)
+            expect(response_body["task_name"]).to eql(crono[:task_name])
 
-        expect(@response_body_data).to have_key("status")
-        expect(@response_body_data["status"]).to be_nil
+            expect(response_body).to have_key("rake")
+            expect(response_body["rake"]).to be_nil
 
-        expect(@response_body_data).to have_key("engine")
-        expect(@response_body_data["engine"]).to be_nil
+            expect(response_body).to have_key("minute")
+            expect(response_body["minute"]).to be_a(Numeric)
+            expect(response_body["minute"]).to eql(crono[:minute])
 
-        expect(@response_body_data).to have_key("rake")
-        expect(@response_body_data["rake"]).to be_nil
+            expect(response_body).to have_key("hour")
+            expect(response_body["hour"]).to be_a(Numeric)
+            expect(response_body["hour"]).to eql(crono[:hour])
 
-        expect(@response_body_data).to have_key("minute")
-        expect(@response_body_data["minute"]).to be_a(Numeric)
-        expect(@response_body_data["minute"]).to eql(@account_crono[:minute])
+            expect(response_body).to have_key("day_of_month")
+            expect(response_body["day_of_month"]).to be_a(Numeric)
+            expect(response_body["day_of_month"]).to eql(crono[:day_of_month])
 
-        expect(@response_body_data).to have_key("hour")
-        expect(@response_body_data["hour"]).to be_a(Numeric)
-        expect(@response_body_data["hour"]).to eql(@account_crono[:hour])
+            expect(response_body).to have_key("month")
+            expect(response_body["month"]).to be_a(Numeric)
+            expect(response_body["month"]).to eql(crono[:month])
 
-        expect(@response_body_data).to have_key("day_of_month")
-        expect(@response_body_data["day_of_month"]).to be_a(Numeric)
-        expect(@response_body_data["day_of_month"]).to eql(@account_crono[:day_of_month])
+            expect(response_body).to have_key("day_of_week")
+            expect(response_body["day_of_week"]).to be_a(Numeric)
+            expect(response_body["day_of_week"]).to eql(crono[:day_of_week])
 
-        expect(@response_body_data).to have_key("month")
-        expect(@response_body_data["month"]).to be_a(Numeric)
-        expect(@response_body_data["month"]).to eql(@account_crono[:month])
+            expect(response_body).to have_key("deleted_at")
+            expect(response_body["deleted_at"]).to be_nil
 
-        expect(@response_body_data).to have_key("day_of_week")
-        expect(@response_body_data["day_of_week"]).to be_a(Numeric)
-        expect(@response_body_data["day_of_week"]).to eql(@account_crono[:day_of_week])
+            expect(response_body).to have_key("created_at")
+            expect(response_body["created_at"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("deleted_at")
-        expect(@response_body_data["deleted_at"]).to be_nil
 
-        expect(@response_body_data).to have_key("created_at")
-        expect(@response_body_data["created_at"]).to be_a(String)
+            expect(response_body).to have_key("accounts_id")
+            expect(response_body["accounts_id"]).to be_a(Numeric)
+            expect(response_body["accounts_id"]).to eql(@current_user.account.id)
 
-        expect(@response_body_data).to have_key("accounts_id")
-        expect(@response_body_data["accounts_id"]).to be_a(Numeric)
-        expect(@response_body_data["accounts_id"]).to eql(@user.account.id)
+        end
 
-        expect(@response_body_data).to have_key("users_id")
-        expect(@response_body_data["users_id"]).to be_a(Numeric)
-        expect(@response_body_data["users_id"]).to eql(@user.id)
-    end
-end
+        it "is expected to return with not found" do
 
-RSpec.describe "GET:/administration/account/cronos/:id", type: :request do
-    include_context "user authentication"
+            cronos = @current_user.account.cronos.all.with_deleted.order(:id)
+            invalid_crono_id = cronos.empty? ? 1 : cronos.last["id"] + 1
 
-    before(:all) do
-        # Look for an ID that does not exist
-        @cronos = @user.account.cronos.all.with_deleted.order(:id)
-        @invalid_crono_id = @cronos.empty? ? 1 : @cronos.last["id"] + 1
+            get ("/administration/account/cronos/#{invalid_crono_id}.json")
 
-        get "/administration/account/cronos/#{@invalid_crono_id}.json"
-    end
+            #share examples
+            expect_response_with_not_found
 
-    include_examples "not found standard json response"
+            expect(response_body).to be_a(Hash)
+            expect(response_body.keys).to contain_exactly("message")
+            expect(response_body["message"]).to be_a(String)
 
-    it "is expected to return with not found" do
-        expect(@response_body).to be_a(Hash)
+        end
 
-        expect(@response_body).to have_key("error")
-        expect(@response_body["error"].keys).to contain_exactly("message", "details")
-
-        expect(@response_body["error"]["message"]).to be_a(String)
-        expect(@response_body["error"]["details"]).to be_an(Array)
     end
 end
