1197fe9a68fa9287db9388f4718f9b847d39b9ad
emerson cardona <emersongcardona@gmail.com>
2022-09-02 12:42:32 -0600

    test: index test succesful

--- spec/tests/requests/account/cronos/index_spec.rb
+++ spec/tests/requests/account/cronos/index_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheL
         it "is expected to respond with a list of all cronos saved in the DB" do
             @current_user.account.cronos.destroy_all
 
-            crono_params = {
+            crono = @current_user.account.cronos.new({
                 :name => Faker::Lorem.word,
                 :description => Faker::Lorem.paragraph,
                 :minute => Time.now.min,
@@ -35,11 +35,8 @@ RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheL
                 :day_of_week => Time.now.wday,
                 :task_name =>  Faker::Lorem.word, 
                 :engine_code => Faker::Lorem.word
-            }
-
-            crono = @current_user.account.cronos.new(crono_params)
+            })
             crono.save
-
             
             get("/administration/account/cronos.json")
 
@@ -52,45 +49,45 @@ RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheL
 
             expect(response_body.first).to have_key("name")
             expect(response_body.first["name"]).to be_a(String)
-            expect(response_body.first["name"]).to eql(crono_params[:name])
+            expect(response_body.first["name"]).to eql(crono[:name])
 
             expect(response_body.first).to have_key("description")
             expect(response_body.first["description"]).to be_a(String)
-            expect(response_body.first["description"]).to eql(crono_params[:description])
+            expect(response_body.first["description"]).to eql(crono[:description])
 
             expect(response_body.first).to have_key("status")
             expect(response_body.first["status"]).to be_nil
 
             expect(response_body.first).to have_key("engine_code")
             expect(response_body.first["engine_code"]).to be_a(String)
-            expect(response_body.first["engine_code"]).to eql(crono_params[:engine_code])
+            expect(response_body.first["engine_code"]).to eql(crono[:engine_code])
 
             expect(response_body.first).to have_key("task_name")
             expect(response_body.first["task_name"]).to be_a(String)
-            expect(response_body.first["task_name"]).to eql(crono_params[:task_name])
+            expect(response_body.first["task_name"]).to eql(crono[:task_name])
 
             expect(response_body.first).to have_key("rake")
             expect(response_body.first["rake"]).to be_nil
 
             expect(response_body.first).to have_key("minute")
             expect(response_body.first["minute"]).to be_a(Numeric)
-            expect(response_body.first["minute"]).to eql(crono_params[:minute])
+            expect(response_body.first["minute"]).to eql(crono[:minute])
 
             expect(response_body.first).to have_key("hour")
             expect(response_body.first["hour"]).to be_a(Numeric)
-            expect(response_body.first["hour"]).to eql(crono_params[:hour])
+            expect(response_body.first["hour"]).to eql(crono[:hour])
 
             expect(response_body.first).to have_key("day_of_month")
             expect(response_body.first["day_of_month"]).to be_a(Numeric)
-            expect(response_body.first["day_of_month"]).to eql(crono_params[:day_of_month])
+            expect(response_body.first["day_of_month"]).to eql(crono[:day_of_month])
 
             expect(response_body.first).to have_key("month")
             expect(response_body.first["month"]).to be_a(Numeric)
-            expect(response_body.first["month"]).to eql(crono_params[:month])
+            expect(response_body.first["month"]).to eql(crono[:month])
 
             expect(response_body.first).to have_key("day_of_week")
             expect(response_body.first["day_of_week"]).to be_a(Numeric)
-            expect(response_body.first["day_of_week"]).to eql(crono_params[:day_of_week])
+            expect(response_body.first["day_of_week"]).to eql(crono[:day_of_week])
 
             expect(response_body.first).to have_key("deleted_at")
             expect(response_body.first["deleted_at"]).to be_nil
