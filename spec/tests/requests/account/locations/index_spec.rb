82a3b49e9ed36f489fa5d772c915fdbc7805a056
The Lesli Development Team <hello@lesli.tech>
2022-09-01 22:33:50 -0600

    test: index test

--- spec/tests/requests/account/locations/index_spec.rb
+++ spec/tests/requests/account/locations/index_spec.rb
@@ -16,94 +16,87 @@ For more information read the license file including with this software.
 
 =end
 
-require "rails_helper"
-require "spec_helper"
-require "byebug"
+require "lesli_request_helper"
 
-RSpec.describe "GET:/administration/account/locations", type: :request do
+RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    describe "GET:/administration/account/locations", type: :request do
+        include_context "request user authentication"
 
-    include_context "user authentication"
+        it "is expected to return with a list of locations created" do
 
-    before(:all) do
+            @data = {
+                :name => Faker::Nation.capital_city,
+                :short_name => nil,
+                :postal_code => nil,
+                :latitude => nil,
+                :longitude => nil,
+                :code => nil,
+                :level => "region",
+                :native_level => nil,
+                :parent_id => nil,
+            }
 
-        @data = {
-            :name => Faker::Nation.capital_city,
-            :short_name => nil,
-            :postal_code => nil,
-            :latitude => nil,
-            :longitude => nil,
-            :code => nil,
-            :calling_code => nil,
-            :level => "region",
-            :native_level => nil,
-            :parent_id => nil,
-            :accounts_id => @user.account.id
+            # Destroy all records in the DB
+            @location = @current_user.account.locations.new(@data)
+            @location.save!
 
-        }
+            get("/administration/account/locations.json")
 
-        # Destroy all records in the DB
-        @location = @user.account.locations.new(@data)
-        @location.save!
+            #share example
+            expect_response_with_successful
 
-        get "/administration/account/locations.json?filters[type]=region"
+            #validate data
+            expect(response_body.first).to be_a(Hash)
 
+            expect(response_body.first).to have_key("id")
+            expect(response_body.first["id"]).to be_a(Numeric)
+
+            expect(response_body.first).to have_key("name")
+            expect(response_body.first["name"]).to be_a(String)
+            expect(response_body.first["name"]).to eql(@data[:name])
+
+            expect(response_body.first).to have_key("short_name")
+            expect(response_body.first["short_name"]).to be_nil
+            expect(response_body.first["short_name"]).to eql(@data[:short_name])
+
+            expect(response_body.first).to have_key("postal_code")
+            expect(response_body.first["postal_code"]).to be_nil
+            expect(response_body.first["postal_code"]).to eql(@data[:postal_code])
+
+            expect(response_body.first).to have_key("latitude")
+            expect(response_body.first["latitude"]).to be_nil
+            expect(response_body.first["latitude"]).to eql(@data[:latitude])
+
+            expect(response_body.first).to have_key("longitude")
+            expect(response_body.first["longitude"]).to be_nil
+            expect(response_body.first["longitude"]).to eql(@data[:longitude])
+
+            expect(response_body.first).to have_key("code")
+            expect(response_body.first["code"]).to be_nil
+            expect(response_body.first["code"]).to eql(@data[:code])
+
+            expect(response_body.first).to have_key("level")
+            expect(response_body.first["level"]).to be_a(String)
+            expect(response_body.first["level"]).to eql(@data[:level])
+
+            expect(response_body.first).to have_key("native_level")
+            expect(response_body.first["native_level"]).to be_nil
+            expect(response_body.first["native_level"]).to eql(@data[:native_level])
+
+            expect(response_body.first).to have_key("parent_id")
+            expect(response_body.first["parent_id"]).to be_nil
+            expect(response_body.first["parent_id"]).to eql(@data[:parent_id])
+
+            expect(response_body.first).to have_key("deleted_at")
+            expect(response_body.first["deleted_at"]).to be_nil
+
+            expect(response_body.first).to have_key("created_at")
+            expect(response_body.first["created_at"]).to be_a(String)
+
+            expect(response_body.first).to have_key("updated_at")
+            expect(response_body.first["updated_at"]).to be_a(String)
+
+        end
     end
-
-    include_examples "successful standard json response"
-
-    it "is expected to return with a list of locations created" do
-
-        expect(@response_body_data).to be_an(Array)
-        expect(@response_body_data.length).to be > 0
-
-        expect(@response_body_data.last).to be_a(Hash)
-
-        expect(@response_body_data.last).to have_key("id")
-        expect(@response_body_data.last["id"]).to be_a(Numeric)
-
-        expect(@response_body_data.last).to have_key("name")
-        expect(@response_body_data.last["name"]).to be_a(String)
-
-        expect(@response_body_data.last).to have_key("short_name")
-        expect(@response_body_data.last["short_name"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("postal_code")
-        expect(@response_body_data.last["postal_code"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("latitude")
-        expect(@response_body_data.last["latitude"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("longitude")
-        expect(@response_body_data.last["longitude"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("code")
-        expect(@response_body_data.last["code"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("calling_code")
-        expect(@response_body_data.last["calling_code"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("level")
-        expect(@response_body_data.last["level"]).to be_a(String)
-
-        expect(@response_body_data.last).to have_key("native_level")
-        expect(@response_body_data.last["native_level"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("parent_id")
-        expect(@response_body_data.last["parent_id"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("deleted_at")
-        expect(@response_body_data.last["deleted_at"]).to be_nil
-
-        expect(@response_body_data.last).to have_key("created_at")
-        expect(@response_body_data.last["created_at"]).to be_a(String)
-
-        expect(@response_body_data.last).to have_key("updated_at")
-        expect(@response_body_data.last["updated_at"]).to be_a(String)
-
-        expect(@response_body_data.last).to have_key("accounts_id")
-        expect(@response_body_data.last["accounts_id"]).to be_a(Numeric)
-        expect(@response_body_data.last["accounts_id"]).to eql(@user.account.id)
-
-    end
-
 end
+
