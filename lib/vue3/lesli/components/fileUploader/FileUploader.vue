e3d4ed1e585dd461d43aed35360f641d6156215c
The Lesli Development Team <hello@lesli.tech>
2022-09-07 15:30:57 -0600

    refactor: lesli-file-uploader prop is-dropzone-active

--- lib/vue3/lesli/components/fileUploader/FileUploader.vue
+++ lib/vue3/lesli/components/fileUploader/FileUploader.vue
@@ -41,6 +41,11 @@ const props = defineProps({
         required: false,
         default: "",
     },
+    isDropzoneActive: {
+        type: Boolean,
+        required: false,
+        default: true,
+    },
 })
 
 // · translations
@@ -55,10 +60,10 @@ const isDragActive = ref(false)
 
 // · this variable have the files that user have dropped or selected
 const files = ref([])
+const imagesToRender = ref([])
 
 // · this is a reactive prop variable that indicates if the component have to clear the files
 const clearCurrentFiles = computed(() => props.clearFiles)
-const fileType = computed(() => props.fileType)
 
 /**
  * @description this function change the value of the dragActive variable
@@ -69,26 +74,27 @@ const toggleIsDragActive = () => {
     }
 }
 
-/**
- * @param {Event} event is the event that triggered the function
- * @description this function is called when the user drop a files
- */
-const onDropFiles = (event) => {
-    // · get the files that user have dropped
-    files.value = Array.from(event.dataTransfer.files)
-    // · emit the files that user have dropped
-    emit("files", files.value)
-}
 
 /**
  * @param {Event} event is the event that triggered the function
  * @description this function is called when the user select a files
  */
-const onSelectFiles = (event) => {
+const onSelectOrDropFiles = (event, isDropzone = true) => {
+    files.value = []
+    imagesToRender.value = []
     // · get the files that user have selected
-    files.value = Array.from(event.target.files)
+    files.value = !isDropzone ? Array.from(event.dataTransfer.files) : Array.from(event.target.files)
     // · if user have selected files, the drag and drop is enabled
-    isDragActive.value = true
+    isDragActive.value = isDropzone
+
+    for (let i = 0; i < files.value.length; i++) {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+            imagesToRender.value.push(e.target.result)
+        }
+        reader.readAsDataURL(files.value[i]);
+    }
+
     // · emit the files that user have selected
     emit("files", files.value)
 }
@@ -97,9 +103,14 @@ const onSelectFiles = (event) => {
  * @param {File} fileToRemove is the file that user want to remove
  * @description this function is called when the user click on the remove button of a file
  */
-const onRemoveFile = (fileToRemove) => {
+const onRemoveFile = (fileToRemove, position = null) => {
+    if (files.value.length === 0) return
+    
     // · update the files without the file that user want to remove
     files.value = files.value.filter((file) => file.name !== fileToRemove.name)
+
+    if (position) imagesToRender.value.splice(position, 1)
+
     // · emit the files without the file that user want to remove
     emit("files", files.value)
 }
@@ -118,52 +129,79 @@ watch(clearCurrentFiles, (value) => {
 
 <template>
     <div class="lesli-file-uploader mt-2">
-        <div v-if="files.length !== 0" class="my-4">
-            <div v-for="(file, i) in files" :key="i" class="item mt-4 ml-0 is-flex is-justify-content-space-between is-align-items-center is-fullwidth box">
-                <div>
-                    <p class="has-text-weight-semibold">{{ file.name }}</p>
-                    <p>{{ props.fileType }}</p>
-                </div>
-                <button
-                    class="button is-white delete-button ml-2 is-round"
-                    @click="onRemoveFile(file)"
-                >
-                    <span class="delete-icon material-icons">delete</span>
-                </button>
-        </div>
-        </div>
         
-        <div
-            class="dropzone is-flex is-flex-direction-column is-justify-content-center is-align-items-center p-4 pointer"
-            @dragenter.prevent="toggleIsDragActive()"
-            @dragleave.prevent="toggleIsDragActive()"
-            @dragover.prevent
-            @drop.prevent="onDropFiles"
-        >
-            <div class="is-flex is-flex-direction-column is-align-items-center">
-                <span class="upload-icon material-icons">file_upload</span>
-                <h5 class="mt-4">{{ translations.core.shared.view_text_drag_files }}</h5>
-                <p class="mt-1">{{ translations.core.shared.view_text_prefer_question }}</p>
-                <label
-                    for="file"
-                    class="mt-2 is-flex is-flex-direction-column is-align-items-center"
-                >
-                    <span class="button is-primary is-small">
-                        {{ translations.core.shared.view_text_select_files }}
+        <template v-if="!isDropzoneActive">
+            <label for="file">
+                <span class="button is-primary">
+                    <span>Add new file</span>
+                    <span class="icon is-small">
+                        <span class="material-icons">add</span>
                     </span>
-                    <input
-                        type="file"
-                        @change="onSelectFiles"
-                        name="file"
-                        id="file"
-                        class="is-hidden"
-                        multiple
-                        accept="image/png, image/jpeg, image/jpg"
-                    />
-                </label>
+                </span>
+                <input
+                    type="file"
+                    @change="onSelectOrDropFiles"
+                    name="file"
+                    id="file"
+                    class="is-hidden"
+                    multiple
+                    accept="image/png, image/jpeg, image/jpg"
+                />
+            </label>
+            <div v-if="files.length !== 0" class="preview mt-5 mb-4">
+                <div v-for="(file, i) in files" :key="i" class="preview-item">
+                    <img :src="imagesToRender[i]" alt="">
+                    <span class="material-icons" @click="onRemoveFile(file, i)">cancel</span>
+                </div>
             </div>
-        </div>
+        </template>
 
-
+        <template v-else>
+            <div v-if="files.length !== 0" class="my-4">
+                <div v-for="(file, i) in files" :key="i" class="item mt-4 ml-0 is-flex is-justify-content-space-between is-align-items-center is-fullwidth box">
+                    <div>
+                        <p class="has-text-weight-semibold">{{ file.name }}</p>
+                        <p>{{ props.fileType }}</p>
+                    </div>
+                    <button
+                        class="button is-white delete-button ml-2 is-round"
+                        @click="onRemoveFile(file)"
+                    >
+                        <span class="delete-icon material-icons">delete</span>
+                    </button>
+                </div>
+            </div>
+            <div
+                v-if="isDropzoneActive"
+                class="dropzone is-flex is-flex-direction-column is-justify-content-center is-align-items-center p-4 pointer"
+                @dragenter.prevent="toggleIsDragActive()"
+                @dragleave.prevent="toggleIsDragActive()"
+                @dragover.prevent
+                @drop.prevent="onSelectOrDropFiles($event, false)"
+            >
+                <div class="is-flex is-flex-direction-column is-align-items-center">
+                    <span class="upload-icon material-icons">file_upload</span>
+                    <h5 class="mt-4">{{ translations.core.shared.view_text_drag_files }}</h5>
+                    <p class="mt-1">{{ translations.core.shared.view_text_prefer_question }}</p>
+                    <label
+                        for="file"
+                        class="mt-2 is-flex is-flex-direction-column is-align-items-center"
+                    >
+                        <span class="button is-primary is-small">
+                            {{ translations.core.shared.view_text_select_files }}
+                        </span>
+                        <input
+                            type="file"
+                            @change="onSelectOrDropFiles"
+                            name="file"
+                            id="file"
+                            class="is-hidden"
+                            multiple
+                            accept="image/png, image/jpeg, image/jpg"
+                        />
+                    </label>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
