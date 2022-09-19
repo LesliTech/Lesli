279ba21630f038167056eac2250c636590e63322
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-09-19 06:57:37 -0400

    assets: update assets

--- app/assets/javascripts/websites/application.js
+++ app/assets/javascripts/websites/application.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "../../app/vue2/websites/app.js":
-/*!**************************************!*\
-  !*** ../../app/vue2/websites/app.js ***!
-  \**************************************/
-/***/ (() => {
-
-eval("console.log('javascript works for websites/landing');\n\n//# sourceURL=webpack://lesliv2/../../app/vue2/websites/app.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["../../app/vue2/websites/app.js"]();
 /******/ 	
 /******/ })()
 ;
