9b536e5f5c510dd503ef43f3d686e2a0f2c50de5
The Lesli Development Team <hello@lesli.tech>
2022-09-18 21:34:17 -0400

    fix: change announcement key

--- lib/vue3/layout/application-announcements.vue
+++ lib/vue3/layout/application-announcements.vue
@@ -35,7 +35,7 @@ onMounted(() => {
 </script>
 <template>
     <section class="application-announcements" v-if="storeAnnouncements.announcements.length > 0">
-        <article :class="['message', `is-${announcement.category}`]" v-for="announcement in storeAnnouncements.announcements" :key="announcement">
+        <article :class="['message', `is-${announcement.category}`]" v-for="announcement in storeAnnouncements.announcements" :key="announcement.id">
             <div class="message-header">
                 <p>{{announcement.name}}</p>
                 <button class="delete" aria-label="delete" v-if="announcement.can_be_closed" @click="storeAnnouncements.closeAnnouncement(announcement.id)"></button>
