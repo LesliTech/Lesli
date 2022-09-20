e8bfc463a9d591fa1bbd6586ee14efd00166fd25
The Lesli Development Team <hello@lesli.tech>
2022-09-19 22:41:26 -0600

    test: show and index exchange successful test

--- spec/tests/requests/account/currencies/exchange_rates/destroy_spec.rb
+++ spec/tests/requests/account/currencies/exchange_rates/destroy_spec.rb
@@ -16,68 +16,57 @@ For more information read the license file including with this software.
 
 =end
 
-require "rails_helper"
-require "spec_helper"
-require "byebug"
+require "lesli_request_helper"
 
-RSpec.describe "DELETE:/administration/account/currencies/:currency_id/exchange_rates/:id", type: :request do 
-    include_context "user authentication"
+RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    describe "DELETE:/administration/account/currencies/:currency_id/exchange_rates/:id", type: :request do
+        include_context "request user authentication"
 
-    before(:all) do 
-        # create a valid currency
-        @new_currency = @user.account.currencies.create!({
-            name: Faker::Currency.name,
-            symbol: Faker::Currency.symbol,
-            country_alpha_3: Faker::Currency.code,
-            user_main_id: @user.id,
-            users_id: @user.id
-        })
+        it "is expected to respond with a exchange rate deleted successfully" do 
+            # create a valid currency
+            @new_currency = @current_user.account.currencies.create!({
+                name: Faker::Currency.name,
+                symbol: Faker::Currency.symbol,
+                country_alpha_3: Faker::Currency.code,
+                user_main_id: @current_user.id,
+                users_id: @current_user.id
+            })
 
-        @exchange_rates_params = {
-            valid_from: Time.now,
-            valid_to: Time.now,
-            exchange_rate: Faker::Number.decimal(l_digits: 1, r_digits: 3)
-        }
+            @exchange_rates_params = {
+                valid_from: Time.now,
+                valid_to: Time.now,
+                exchange_rate: Faker::Number.decimal(l_digits: 1, r_digits: 3)
+            }
 
-        @new_currency_exchange_rate = @user.account.currencies.find_by(id: @new_currency.id).exchange_rates.create!(@exchange_rates_params)
+            @new_currency_exchange_rate = @current_user.account.currencies.find_by(id: @new_currency.id).exchange_rates.create!(@exchange_rates_params)
 
-        delete "/administration/account/currencies/#{@new_currency.id}/exchange_rates/#{@new_currency_exchange_rate.id}.json"
-    end
+            delete "/administration/account/currencies/#{@new_currency.id}/exchange_rates/#{@new_currency_exchange_rate.id}.json"
 
-    include_examples "successful standard json response"
+            expect_response_with_successful
 
-    it "is expected to respond with a exchange rate deleted successfully" do 
-        expect(@response_body_data).to be_nil
-        expect(@user.account.currencies.find_by(id: @new_currency.id).exchange_rates.find_by(id: @new_currency_exchange_rate.id)).to be_nil
-    end
-end
+            expect(@current_user.account.currencies.find_by(id: @new_currency.id).exchange_rates.find_by(id: @new_currency_exchange_rate.id)).to be_nil
+        end
 
-RSpec.describe "DELETE:/administration/account/currencies/:currency_id/exchange_rates/:id", type: :request do 
-    include_context "user authentication"
 
-    before(:all) do 
-        # this is a new currency, therefore does not have any exchange rate yet, no records
-        @new_currency = @user.account.currencies.create!({
-            name: Faker::Currency.name,
-            symbol: Faker::Currency.symbol,
-            country_alpha_3: Faker::Currency.code,
-            user_main_id: @user.id,
-            users_id: @user.id
-        })
+        it "is expected to respond with not found when an ID that does not exist is given" do 
+            # this is a new currency, therefore does not have any exchange rate yet, no records
+            @new_currency = @current_user.account.currencies.create!({
+                name: Faker::Currency.name,
+                symbol: Faker::Currency.symbol,
+                country_alpha_3: Faker::Currency.code,
+                user_main_id: @current_user.id,
+                users_id: @current_user.id
+            })
 
-        #there is no currency with ID = 1, therefore should return with not found
-        delete "/administration/account/currencies/#{@new_currency.id}/exchange_rates/1.json"
-    end
+            #there is no currency with ID = 1, therefore should return with not found
+            delete "/administration/account/currencies/#{@new_currency.id}/exchange_rates/1.json"
 
-    include_examples "not found standard json response"
+            expect_response_with_not_found
 
-    it "is expected to respond with not found when an ID that does not exist is given" do 
-        expect(@response_body).to be_a(Hash)
+            expect(response_body).to be_a(Hash)
 
-        expect(@response_body).to have_key("error")
-        expect(@response_body["error"].keys).to contain_exactly("message", "details")
-        
-        expect(@response_body["error"]["message"]).to be_a(String)
-        expect(@response_body["error"]["details"]).to be_an(Array)
+            expect(response_body.keys).to contain_exactly("message")
+            expect(response_body["message"]).to be_a(String)
+        end
     end
 end
--- spec/tests/requests/account/currencies/exchange_rates/show_spec.rb
+++ spec/tests/requests/account/currencies/exchange_rates/show_spec.rb
@@ -16,101 +16,90 @@ For more information read the license file including with this software.
 
 =end
 
-require "rails_helper"
-require "spec_helper"
-require "byebug"
+require "lesli_request_helper"
 
-RSpec.describe "GET:/administration/account/currencies/:currency_id/exchange_rates/:id", type: :request do 
-    include_context "user authentication"
+RSpec.describe "Tests for Lesli3", type: :request, :unless => defined?(DeutscheLeibrenten) do
+    describe "GET:/administration/account/currencies/:currency_id/exchange_rates/:id", type: :request do
+        include_context "request user authentication"
 
-    before(:all) do 
-        @valid_from = Time.now
-        @valid_to = Time.now 
+        it "is expected to respond with a exchange rate" do 
+            @valid_from = Time.now
+            @valid_to = Time.now 
 
-        # create a valid currency
-        @new_currency = @user.account.currencies.create!({
-            name: Faker::Currency.name,
-            symbol: Faker::Currency.symbol,
-            country_alpha_3: Faker::Currency.code,
-            user_main_id: @user.id,
-            users_id: @user.id
-        })
+            # create a valid currency
+            @new_currency = @current_user.account.currencies.create!({
+                name: Faker::Currency.name,
+                symbol: Faker::Currency.symbol,
+                country_alpha_3: Faker::Currency.code,
+                user_main_id: @current_user.id,
+                users_id: @current_user.id
+            })
 
-        @exchange_rates_params = {
-            valid_from: @valid_from,
-            valid_to: @valid_to,
-            exchange_rate: Faker::Number.decimal(l_digits: 1, r_digits: 3)
-        }
+            @exchange_rates_params = {
+                valid_from: @valid_from,
+                valid_to: @valid_to,
+                exchange_rate: Faker::Number.decimal(l_digits: 1, r_digits: 3)
+            }
 
-        @new_currency_exchange_rate = @user.account.currencies.find_by(id: @new_currency.id).exchange_rates.create!(@exchange_rates_params)
+            @new_currency_exchange_rate = @current_user.account.currencies.find_by(id: @new_currency.id).exchange_rates.create!(@exchange_rates_params)
 
-        get "/administration/account/currencies/#{@new_currency.id}/exchange_rates/#{@new_currency_exchange_rate.id}.json"
-    end
+            get "/administration/account/currencies/#{@new_currency.id}/exchange_rates/#{@new_currency_exchange_rate.id}.json"
 
-    include_examples "successful standard json response"
+            expect_response_with_successful
+            
+            expect(response_body).to be_a(Hash)
 
-    it "is expected to respond with a exchange rate" do 
-        expect(@response_body_data).to be_a(Hash)
+            expect(response_body).to have_key("id")
+            expect(response_body["id"]).to be_a(Numeric)
 
-        expect(@response_body_data).to have_key("id")
-        expect(@response_body_data["id"]).to be_a(Numeric)
+            expect(response_body).to have_key("exchange_rate")
+            expect(response_body["exchange_rate"]).to be_a(String)
+            expect(response_body["exchange_rate"].to_f).to eql(@exchange_rates_params[:exchange_rate])
 
-        expect(@response_body_data).to have_key("exchange_rate")
-        expect(@response_body_data["exchange_rate"]).to be_a(String)
-        expect(@response_body_data["exchange_rate"].to_f).to eql(@exchange_rates_params[:exchange_rate])
+            expect(response_body).to have_key("valid_from")
+            expect(response_body["valid_from"]).to be_a(String)
+            
+            expect(response_body).to have_key("valid_to")
+            expect(response_body["valid_to"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("valid_from")
-        expect(@response_body_data["valid_from"]).to be_a(String)
-        expect(LC::Date2.new(Time.parse(@response_body_data["valid_from"])).date_time.to_s).to eql(LC::Date2.new(@valid_from).date_time.to_s)
-        
-        expect(@response_body_data).to have_key("valid_to")
-        expect(@response_body_data["valid_to"]).to be_a(String)
-        expect(LC::Date2.new(Time.parse(@response_body_data["valid_to"])).date_time.to_s).to eql(LC::Date2.new(@valid_to).date_time.to_s)
+            expect(response_body).to have_key("created_at")
+            expect(response_body["created_at"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("created_at")
-        expect(@response_body_data["created_at"]).to be_a(String)
+            expect(response_body).to have_key("account_currencies_id")
+            expect(response_body["account_currencies_id"]).to be_a(Numeric)
+            expect(response_body["account_currencies_id"]).to eql(@new_currency.id)
 
-        expect(@response_body_data).to have_key("account_currencies_id")
-        expect(@response_body_data["account_currencies_id"]).to be_a(Numeric)
-        expect(@response_body_data["account_currencies_id"]).to eql(@new_currency.id)
+            expect(response_body).to have_key("valid_from_text")
+            expect(response_body["valid_from_text"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("valid_from_text")
-        expect(@response_body_data["valid_from_text"]).to be_a(String)
+            expect(response_body).to have_key("valid_to_text")
+            expect(response_body["valid_to_text"]).to be_a(String)
 
-        expect(@response_body_data).to have_key("valid_to_text")
-        expect(@response_body_data["valid_to_text"]).to be_a(String)
+            expect(response_body).to have_key("created_at_text")
+            expect(response_body["created_at_text"]).to be_a(String)
+        end
 
-        expect(@response_body_data).to have_key("created_at_text")
-        expect(@response_body_data["created_at_text"]).to be_a(String)
-    end
-end
+        it "is expected to return with a exchange rate not found" do 
+            # this is a new currency, therefore does not have any exchange rate yet, no records
+            @new_currency = @current_user.account.currencies.create!({
+                name: Faker::Currency.name,
+                symbol: Faker::Currency.symbol,
+                country_alpha_3: Faker::Currency.code,
+                user_main_id: @current_user.id,
+                users_id: @current_user.id
+            })
 
-RSpec.describe "GET:/administration/account/currencies/:currency_id/exchange_rates/:id", type: :request do 
-    include_context "user authentication"
+            #there is no currency with ID = 1, therefore should return with not found
+            get "/administration/account/currencies/#{@new_currency.id}/exchange_rates/1.json"
 
-    before(:all) do 
-        # this is a new currency, therefore does not have any exchange rate yet, no records
-        @new_currency = @user.account.currencies.create!({
-            name: Faker::Currency.name,
-            symbol: Faker::Currency.symbol,
-            country_alpha_3: Faker::Currency.code,
-            user_main_id: @user.id,
-            users_id: @user.id
-        })
+    
+            expect_response_with_not_found
 
-        #there is no currency with ID = 1, therefore should return with not found
-        get "/administration/account/currencies/#{@new_currency.id}/exchange_rates/1.json"
-    end
+            expect(response_body).to be_a(Hash)
 
-    include_examples "not found standard json response"
-
-    it "is expected to return with a exchange rate not found" do 
-        expect(@response_body).to be_a(Hash)
-
-        expect(@response_body).to have_key("error")
-        expect(@response_body["error"].keys).to contain_exactly("message", "details")
-        
-        expect(@response_body["error"]["message"]).to be_a(String)
-        expect(@response_body["error"]["details"]).to be_an(Array)
+            expect(response_body.keys).to contain_exactly("message")
+            expect(response_body["message"]).to be_a(String)
+        end
     end
 end
+
