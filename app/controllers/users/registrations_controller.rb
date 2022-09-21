3ca225ab3aa1c2ae07053e9d602f7c1c2f38d1e5
The Lesli Development Team <hello@lesli.tech>
2022-09-21 09:58:33 -0600

    fix: registration with specific fields

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -68,7 +68,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     #    };
     #    this.http.post('127.0.0.1/register', data);
     def create
-        
+
         # Check if instance allow multi-account
         if !Rails.application.config.lesli[:security][:allow_registration]
             respond_with_error(I18n.t("core.users/registrations.messages_error_registration_not_allowed"))
@@ -91,6 +91,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
         
         # persist new user
         if user.save
+
+            if params[:user][:detail_attributes]
+                detail_attributes = { 
+                    first_name: params[:user][:detail_attributes][:first_name],
+                    last_name: params[:user][:detail_attributes][:last_name],
+                    telephone: params[:user][:detail_attributes][:telephone],
+                    user: user
+                } 
+                detail_attributes = ::User::Detail.new(detail_attributes)
+                detail_attributes.save
+            end
 
             # save a default locale for user
             user.settings.create(:name => 'locale', :value => I18n.locale)
@@ -137,6 +148,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
         end
             
     end 
+
+    def options
+        respond_with_successful({
+            countries: ::Account.first.locations.where(level: 'country').select(:name, :calling_code)
+        })
+    end
 
 
     protected
