7f13d18d4cef50529334cb291fc74f8dd58dd26d
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-10-23 22:07:36 -0400

    fix: change form buttons

--- lib/vue3/panels/panel-announcements.vue
+++ lib/vue3/panels/panel-announcements.vue
@@ -93,112 +93,115 @@ watch(() => storeLayout.showAnnouncements, () => {
         </template>
         <template #default>
             <div class="form">
-                <form @submit.prevent="storeAnnouncementsPanel.formSubmit()" class="card py-4">
-                    <div class="columns is-marginless has-border-bottom">
-                        <div class="column is-4">
-                            <label class="label">
-                                {{ translations.bell.announcements.column_name }}
-                                <sup class="has-text-danger">*</sup>
-                            </label>
-                        </div>
-                        <div class="column">
-                            <div class="control is-clearfix">
-                                <input
-                                    type="text"
-                                    autocomplete="on"
-                                    name="subject"
-                                    required
-                                    class="input"
-                                    v-model="storeAnnouncementsPanel.announcement.name"
-                                />
-                            </div>
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-4">
+                        <label class="label">
+                            {{ translations.bell.announcements.column_name }}
+                            <sup class="has-text-danger">*</sup>
+                        </label>
+                    </div>
+                    <div class="column">
+                        <div class="control is-clearfix">
+                            <input
+                                type="text"
+                                autocomplete="on"
+                                name="subject"
+                                required
+                                class="input"
+                                v-model="storeAnnouncementsPanel.announcement.name"
+                            />
                         </div>
                     </div>
+                </div>
 
-                    <div class="columns is-marginless has-border-bottom">
-                        <div class="column is-4">
-                            <label class="label">
-                                {{translations.bell.announcements.column_message}}
-                                <sup class="has-text-danger">*</sup>
-                            </label>
-                        </div>
-                        <div class="column">
-                            <div class="control is-clearfix">
-                                <editorRichText mode="small" v-model="storeAnnouncementsPanel.announcement.message" ></editorRichText>
-                            </div>
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-4">
+                        <label class="label">
+                            {{translations.bell.announcements.column_message}}
+                            <sup class="has-text-danger">*</sup>
+                        </label>
+                    </div>
+                    <div class="column">
+                        <div class="control is-clearfix">
+                            <editorRichText mode="small" v-model="storeAnnouncementsPanel.announcement.message"></editorRichText>
                         </div>
                     </div>
+                </div>
 
-                    <div class="columns is-marginless has-border-bottom">
-                        <div class="column is-4">
-                            <label class="label">{{translations.bell.announcements.column_start_at}}</label>
-                        </div>
-                        <div class="column">
-                            <input class="input is-info" type="date" v-model="storeAnnouncementsPanel.announcement.start_at">
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-4">
+                        <label class="label">{{translations.bell.announcements.column_start_at}}</label>
+                    </div>
+                    <div class="column">
+                        <input class="input is-info" type="date" v-model="storeAnnouncementsPanel.announcement.start_at">
+                    </div>
+                </div>
+
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-4">
+                        <label class="label">{{translations.bell.announcements.column_end_at}}</label>
+                    </div>
+                    <div class="column">
+                        <input class="input is-info" type="date" v-model="storeAnnouncementsPanel.announcement.end_at">
+                    </div>
+                </div>
+
+
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-4">
+                        <label class="label">
+                            {{translations.bell.announcements.column_kind}}
+                        </label>
+                    </div>
+                    <div class="column">
+                        <div class="control is-clearfix">
+                            <lesli-select
+                                v-model = "storeAnnouncementsPanel.announcement.category"
+                                :options="selectOptions">
+                            </lesli-select>
                         </div>
                     </div>
+                </div>
 
-                    <div class="columns is-marginless has-border-bottom">
-                        <div class="column is-4">
-                            <label class="label">{{translations.bell.announcements.column_end_at}}</label>
-                        </div>
-                        <div class="column">
-                            <input class="input is-info" type="date" v-model="storeAnnouncementsPanel.announcement.end_at">
-                        </div>
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-4">
+                        <label class="label">
+                            {{translations.bell.announcements.column_can_be_closed}}
+                            <sup class="has-text-danger">*</sup>
+                        </label>
                     </div>
-
-
-                    <div class="columns is-marginless has-border-bottom">
-                        <div class="column is-4">
-                            <label class="label">
-                                {{translations.bell.announcements.column_kind}}
-                            </label>
-                        </div>
-                        <div class="column">
-                            <div class="control is-clearfix">
-                                <lesli-select
-                                    v-model = "storeAnnouncementsPanel.announcement.category"
-                                    :options="selectOptions">
-                                </lesli-select>
-                            </div>
-                        </div>
+                    <div class="column">
+                        <label :for="option.label" class="radio" v-for="option in closeOptions" :key="option">
+                            <input name="user_salutation" type="radio" :id="option.label" :value="option.value" v-model="storeAnnouncementsPanel.announcement.can_be_closed" required/>
+                            {{option.label}}
+                        </label>  
                     </div>
+                </div>
 
-                    <div class="columns is-marginless has-border-bottom">
-                        <div class="column is-4">
-                            <label class="label">
-                                {{translations.bell.announcements.column_can_be_closed}}
-                                <sup class="has-text-danger">*</sup>
-                            </label>
-                        </div>
-                        <div class="column">
-                            <label :for="option.label" class="radio" v-for="option in closeOptions" :key="option">
-                                <input name="user_salutation" type="radio" :id="option.label" :value="option.value" v-model="storeAnnouncementsPanel.announcement.can_be_closed" required/>
-                                {{option.label}}
-                            </label>  
-                        </div>
-                    </div>
-
-                    <div class="px-3 ql-bg-blue">
-                        <button
-                            type="submit"
-                            class="button is-fullwidth has-text-centered submit-button is-primary"
+                <div class="columns is-marginless has-border-bottom">
+                    <div class="column is-7">
+                        <lesli-button
+                            icon="save"
+                            primary
+                            @click="storeAnnouncementsPanel.formSubmit()"
                         >
                             {{ translations.core.shared.view_btn_save }}
-                        </button>
+                        </lesli-button>
                     </div>
-
-                    <div class="px-3 ql-bg-blue">
-                        <button
-                            type="button"
-                            class="button is-fullwidth has-text-centered submit-button"
-                            @click.stop="storeAnnouncementsPanel.clearForm()"
+                    <div class="column">
+                        <lesli-button
+                            icon="backspace"
+                            @click="storeAnnouncementsPanel.clearForm()"
                         >
-                        {{ translations.core.shared.view_btn_clear }}
-                        </button>
+                            {{ translations.core.shared.view_btn_clear }}
+                        </lesli-button>
+   
                     </div>
-                    
-                </form>
+                </div>
+
+
+
+
             </div>
             <div class="announcements" v-if="storeAnnouncementsPanel.announcements.length > 0">
                 <lesli-table
