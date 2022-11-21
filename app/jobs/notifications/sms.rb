c4453c64f9709b2d25255d57c0df6da2f699ec00
The Lesli Development Team <hello@lesli.tech>
2022-11-21 08:16:12 -0600

    feat: add validation to phone numbers with zero at the beginning

--- app/jobs/notifications/sms.rb
+++ app/jobs/notifications/sms.rb
@@ -4,16 +4,35 @@ module Notifications
         queue_as :default
 
         def perform(telephone, message)
-
-            if not Rails.application.config.lesli[:configuration][:notifications][:sms]
-                LC::Debug.msg "SMS are deactivated.", telephone, message
-                return
-            end
-
-            LC::Debug.msg "New SMS.", telephone, message if Rails.env == "development"
-
-            sms = LC::Config::Providers::Aws::Sns.new()
             begin
+                # raise error if telephone is nil or empty
+                raise ArgumentError.new("telephone number is required, got #{telephone}") if telephone.nil? || telephone.empty?
+                raise ArgumentError.new("message is required, got #{message}") if message.nil? || message.empty?
+            
+
+                if not Rails.application.config.lesli[:configuration][:notifications][:sms]
+                    LC::Debug.msg "SMS are deactivated.", telephone, message
+                    return
+                end
+
+                sms = LC::Config::Providers::Aws::Sns.new()
+
+                # verify telephone
+                # verify if telephone has spaces
+                if telephone.include? " "
+                    telephone_elements = telephone.split(" ")
+                    
+                    telephone_number = telephone_elements[1]
+                    telephone_code = telephone_elements[0]
+
+                    # cast to number telephone_number to remove zero at the beginning
+                    telephone_number = telephone_number.to_i
+
+                    telephone = "#{telephone_code} #{telephone_number}"
+                end
+
+                LC::Debug.msg "New SMS.", telephone, message if Rails.env == "development"
+
                 if sms.instance_variable_get(:@client).present?
                     sms.publish(phone_number: telephone, message: message)
                     log_account_activity("Lesli", "app/jobs/notifications/sms", "sms_send_true", { telephone: telephone })
