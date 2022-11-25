9a87512c2d6ce5598bb13048889a84e8a52d6a49
Iris2030 <ibazhinova@gmail.com>
2022-11-25 10:30:35 +0200

    fix: fixed the invite tests

--- spec/tests/models/invite/invite_spec.rb
+++ spec/tests/models/invite/invite_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
     
-    it 'ensures email presence' do
+    it 'should not save successfully without an email' do
         
         invite = Invite.new(full_name: Faker::Name.name, telephone: Faker::PhoneNumber.cell_phone, note: Faker::Lorem.sentence).save
         expect(invite).to eq(false) 
@@ -13,12 +13,9 @@ RSpec.describe Invite, type: :model do
         
         invite = FactoryBot.build(:invite)
         
-        puts invite.created_at.class
-        
         expect(invite.full_name).to be_a_kind_of(String)
         expect(invite.note).to be_a_kind_of(String)
         expect(invite.status).to eql(nil)
-        expect(invite.accounts_id).to eql(nil)
         expect(invite.deleted_at).to eql(nil)
         expect(invite.sent_at).to eql(nil)
         expect(invite.telephone).to be_a_kind_of(String)
@@ -29,28 +26,13 @@ RSpec.describe Invite, type: :model do
         
         invite = Invite.new(
             full_name:  Faker::Name.name,
-            telephone: '232-67-987',
+            telephone: Faker::PhoneNumber.subscriber_number(length: 10),
             note: Faker::Lorem.sentence
         )
         
         expect(invite.telephone.length).to be_eql(10)
         
     end
-    
-    it "is expects to return correct information" do
-        
-        invite = FactoryBot.build(:invite)
-        full_name = invite.full_name
-        telephone = invite.telephone
-        email = invite.email
-        note =  invite.note
-        
-        expect(invite.full_name).to eql(full_name)
-        expect(invite.telephone).to eql(telephone)
-        expect(invite.email).to eql(email)
-        expect(invite.note).to eql(note)
-        
-    end  
     
     it  'should save successfully' do
         invite = Invite.new(
