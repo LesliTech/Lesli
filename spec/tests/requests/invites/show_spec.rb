4f006d005859121cc99b17dd89ffea32ab6e11b6
The Lesli Development Team <hello@lesli.tech>
2022-11-29 10:54:23 +0200

    fix: invite tests fixed the structure

--- spec/tests/requests/invites/show_spec.rb
+++ spec/tests/requests/invites/show_spec.rb
@@ -1,32 +1,48 @@
+=begin
+
+Copyright (c) 2022, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// ·
+
+=end
+
+
 require "lesli_request_helper"
 
-RSpec.describe "InvitesController " do
+RSpec.describe "Get:/invite", type: :request do
     
-    describe "Get:/invite", type: :request do
-        
-        include_context "request user authentication"
-        
-        it "returns a successfull response" do
-            get("/invite")
-            expect(response).to have_http_status(:ok)
-            expect(response.status).to eq(200)
-        end
-        
-        it "returns a content type to be text/html" do
-            get("/invite")
-            expect(response.content_type).to eq "text/html; charset=utf-8"
-        end
-        
-        it "renders a right template" do
-            get("/invite")
-            expect(response).to render_template(:show)
-        end
-        
-        it "does not render a different template" do
-            get("/invite")
-            expect(response).to_not render_template(:post)
-        end  
-        
+    include_context "request user authentication"
+    
+    it "returns a successfull response" do
+        get("/invite")
+        expect(response).to have_http_status(:ok)
+        expect(response.status).to eq(200)
+    end
+    
+    it "returns a content type to be text/html" do
+        get("/invite")
+        expect(response.content_type).to eq "text/html; charset=utf-8"
+    end
+    
+    it "renders a right template" do
+        get("/invite")
+        expect(response).to render_template(:show)
+    end
+    
+    it "does not render a different template" do
+        get("/invite")
+        expect(response).to_not render_template(:post)
     end  
     
 end
