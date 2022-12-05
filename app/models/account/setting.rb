5aa31c6ecee6ab44de6ad51cf4dd9ae0a9395f19
FranciscoRosal <frosal@lomax.com.gt>
2022-12-05 12:36:21 -0600

    fix: reset account settings on each test

--- app/models/account/setting.rb
+++ app/models/account/setting.rb
@@ -37,22 +37,23 @@ class Account::Setting < ApplicationRecord
 
     def self.reset_all(account)
         # Set default password complexity configuration
-        account.settings.upsert({ name: 'password_enforce_complexity', value: 1 })
-        account.settings.upsert({ name: 'password_expiration_time_days', value: 365 })
-        account.settings.upsert({ name: 'password_special_char_count', value: 0 })
-        account.settings.upsert({ name: 'password_lowercase_count', value: 0 })
-        account.settings.upsert({ name: 'password_uppercase_count', value: 0 })
-        account.settings.upsert({ name: 'password_minimum_length', value: 6 })
-        account.settings.upsert({ name: 'password_digit_count', value: 0 })
+        # Here is used update instead of create_with because the purpose is to force the set of the value
+        account.settings.find_or_create_by(name: 'password_enforce_complexity').update(value: 1)
+        account.settings.find_or_create_by(name: 'password_expiration_time_days').update(value: 365)
+        account.settings.find_or_create_by(name: 'password_special_char_count').update(value: 0)
+        account.settings.find_or_create_by(name: 'password_lowercase_count').update(value: 0)
+        account.settings.find_or_create_by(name: 'password_uppercase_count').update(value: 0)
+        account.settings.find_or_create_by(name: 'password_minimum_length').update(value: 6)
+        account.settings.find_or_create_by(name: 'password_digit_count').update(value: 0)
 
         datetime = Rails.application.config.lesli[:configuration][:datetime]
-        account.settings.upsert({ name: 'datetime_time_zone', value: datetime[:time_zone] })
-        account.settings.upsert({ name: 'datetime_start_week_on', value: datetime[:start_week_on] })
-        account.settings.upsert({ name: 'datetime_format_date', value: datetime[:formats][:date] })
-        account.settings.upsert({ name: 'datetime_format_time', value: datetime[:formats][:time] })
-        account.settings.upsert({ name: 'datetime_format_date_time', value: datetime[:formats][:date_time] })
-        account.settings.upsert({ name: 'datetime_format_date_words', value: datetime[:formats][:date_words] })
-        account.settings.upsert({ name: 'datetime_format_date_time_words', value: datetime[:formats][:date_time_words] })
+        account.settings.find_or_create_by(name: 'datetime_time_zone').update(value: datetime[:time_zone])
+        account.settings.find_or_create_by(name: 'datetime_start_week_on').update(value: datetime[:start_week_on])
+        account.settings.find_or_create_by(name: 'datetime_format_date').update(value: datetime[:formats][:date])
+        account.settings.find_or_create_by(name: 'datetime_format_time').update(value: datetime[:formats][:time])
+        account.settings.find_or_create_by(name: 'datetime_format_date_time').update(value: datetime[:formats][:date_time])
+        account.settings.find_or_create_by(name: 'datetime_format_date_words').update(value: datetime[:formats][:date_words])
+        account.settings.find_or_create_by(name: 'datetime_format_date_time_words').update(value: datetime[:formats][:date_time_words])
     end
 
     def self.theme_settings_keys
