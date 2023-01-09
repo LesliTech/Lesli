78d357452c9c60933d182b7e5b6d9886318faf5c
Marcos Bonifasi <marcosbonifasi19@gmail.com>
2023-01-08 20:00:26 -0600

    feat: make migration ready to work for new fronted

--- db/migrate/v3.3/0010008533_alter_descriptors.rb
+++ db/migrate/v3.3/0010008533_alter_descriptors.rb
@@ -19,47 +19,5 @@ class AlterDescriptors < ActiveRecord::Migration[7.0]
         add_column  :descriptors,   :description,       :string
         add_column  :descriptors,   :descriptors_id,    :bigint
         add_index   :descriptors,   :descriptors_id
-
-        # We need to migrate all the descriptors names of the table role_descriptors to descriptors
-
-        # It has to be done with empty roles tables, so, would be like a migration from scratch
-        # therefore, we're going to truncate the tables
-        [
-            "descriptor_privileges",
-            "role_describers",
-            "descriptor_activities",
-            "user_describers",
-            "descriptors",
-            "role_privileges"
-        ].each do |table|
-            L2.warning "Truncating table '#{table}' ..."
-            ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table} RESTART IDENTITY CASCADE;")
-        end
-
-
-        # Alright, we need to query to get all the info a role descriptor has access to
-        query = Account.first.roles.joins(
-            descriptor_assignments: [
-                descriptor: [privilege_actions: {system_action: :system_controller}]
-            ]
-        )
-
-        # Getting the fields needed to copy the roles info
-        query = query.select(
-            :id, 
-            "role_descriptors.name",
-            "role_descriptors.description",
-            ""
-        )
-
-        # Now we start the migration as such
-        RoleDescriptor.all.each do |role_descriptor|
-            Descriptor.create!(
-                name: role_descriptor.name, 
-                description: role_descriptor.description,
-                descriptors_id: role_descriptor.role_descriptors_id,
-                account: Account.first,
-            )
-        end
     end
 end
