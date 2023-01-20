71e70fc0cd967b2188b7b55d20eda7406cecbc2a
The Lesli Development Team <hello@lesli.tech>
2023-01-20 10:41:41 -0600

    chore: update license

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 =begin
 
-Copyright (c) 2020, all rights reserved.
+Copyright (c) 2023, all rights reserved.
 
-All the information provided by this platform is protected by international laws related  to 
-industrial property, intellectual property, copyright and relative international laws. 
-All intellectual or industrial property rights of the code, texts, trade mark, design, 
+All the information provided by this platform is protected by international laws related  to
+industrial property, intellectual property, copyright and relative international laws.
+All intellectual or industrial property rights of the code, texts, trade mark, design,
 pictures and any other information belongs to the owner of this platform.
 
 Without the written permission of the owner, any replication, modification,
@@ -13,7 +13,7 @@ transmission, publication is strictly forbidden.
 For more information read the license file including with this software.
 
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-// · 
+// ·
 
 =end
 class Users::RegistrationsController < Devise::RegistrationsController
@@ -29,7 +29,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
 
 
-    #@return [User] The user handler by the current instance of this controller. 
+    #@return [User] The user handler by the current instance of this controller.
     #@description Returns the user associated to this controller. If there is no
     #    user, a new instance is created and returned
     #@example
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     #@controller_action_param :email [String] The registered user email
     #@controller_action_param :password [String] The new password
     #@controller_action_param :password_confirmation [String] the new password confirmation
-    #@return [Json] Json that contains wheter the account was created or not. 
+    #@return [Json] Json that contains wheter the account was created or not.
     #    If it is not successful, it returs an error message
     #@description Creates a new inactive account and sends a confirmation email so the
     #    new user can activate it
@@ -72,30 +72,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
         # Check if instance allow multi-account
         if !Rails.application.config.lesli[:security][:allow_registration]
             respond_with_error(I18n.t("core.users/registrations.messages_error_registration_not_allowed"))
-            return 
+            return
         end
 
         # Validate user is unique
         if ::User.with_deleted.find_by(email: sign_up_params["email"])
             return respond_with_error(I18n.t("core.users/registrations.messages_info_user_already_exists"))
         end
-        
+
         # build new user
         user = build_resource(sign_up_params)
-        
+
         # run password complexity validations
         password_complexity = UserValidationService.new(user).password_complexity(sign_up_params[:password])
 
         # return if there are errors with the complexity validations
-        return respond_with_error("password_complexity_error", password_complexity.error) if not password_complexity.successful?
-        
+        return respond_with_error("password_complexity_error", password_complexity.error) unless password_complexity.successful?
+
         # persist new user
         if user.save
 
             detail_attributes = params.dig(:user, :detail_attributes)
 
             unless detail_attributes.nil? || detail_attributes.empty?
-                user.detail.update({ 
+                user.detail.update({
                     first_name: params.dig(:user, :detail_attributes, :first_name) || "",
                     last_name: params.dig(:user, :detail_attributes, :last_name) || "",
                     telephone: params.dig(:user, :detail_attributes, :telephone) || ""
@@ -110,11 +110,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
         else
             respond_with_error(user.errors.full_messages.to_sentence)
         end
-        
+
     end
 
     # Self password update through the the profile/administration area
-    def update 
+    def update
 
         log = @user.logs.create({ title: "password_update_atempt" })
 
@@ -122,14 +122,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
         password_complexity = UserValidationService.new(@user).password_complexity(sign_up_params[:password])
 
         # return if there are errors with the complexity validations
-        return respond_with_error(password_complexity.error[0], password_complexity.error) if not password_complexity.successful?
+        return respond_with_error(password_complexity.error[0], password_complexity.error) unless password_complexity.successful?
 
         if @user.update(sign_up_params)
 
             # reset password expiration due the user just updated his password
             if @user.has_expired_password?
                 @user.update(password_expiration_at: nil)
-            end 
+            end
 
             log.update(title: "password_update_successful")
 
@@ -145,8 +145,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
             respond_with_error(@user.errors.full_messages)
 
         end
-            
-    end 
+
+    end
 
     def options
         respond_with_successful({
@@ -182,5 +182,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def configure_sign_up_params
         devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
     end
-    
+
 end
