69adac83a4db5c43a0e1003704d8c2db277e046d
The Lesli Development Team <hello@lesli.tech>
2023-02-16 08:16:29 -0600

    fix: add singular prop for dropdown transition options

--- lib/vue3/shared/workflows/components/workflow-status-dropdown.vue
+++ lib/vue3/shared/workflows/components/workflow-status-dropdown.vue
@@ -51,6 +51,11 @@ const props = defineProps({
         type: Number,
         required: true,
     },
+    // · prop that indicates the resource that you need to interact with.
+    cloudObjectSingular: {
+        type: Object,
+        required: false,
+    },
 })
 
 // · defining emits
@@ -60,6 +65,7 @@ const emit = defineEmits(['onUpdatedStatus']);
 storeWorkflow.cloudModule = props.cloudModule
 storeWorkflow.cloudObject = props.cloudObject
 storeWorkflow.cloudObjectId = props.cloudObjectId
+storeWorkflow.cloudObjectSingular = props.cloudObjectSingular
 
 // · dropdown's current value
 const dropdownValue = ref(null)
