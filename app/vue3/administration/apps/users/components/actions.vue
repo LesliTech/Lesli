72cdd249f55365550ee7c376321553f3735f9d09
Luis Donis <ldonis.emc2@gmail.com>
2023-02-19 14:38:42 -0600

    feat: add password reset

--- app/vue3/administration/apps/users/components/actions.vue
+++ app/vue3/administration/apps/users/components/actions.vue
@@ -28,50 +28,60 @@ const storeUser = useUser()
 
 </script>
 <template>
-    <div class="level">
-        <div class="level-left">
-            <div class="level-item">
-                <button class="button is-link is-light is-outlined" @click="storeUser.doRequestPasswordChange">
-                    <span class="icon">
-                        <span class="material-icons">
-                            enhanced_encryption
-                        </span>
-                    </span>
-                    <span>Request password change</span>
-                </button>
-            </div>
-            <div class="level-item">
-                <button class="button is-link is-light is-outlined" @click="storeUser.doLogout">
-                    <span class="icon">
+    <div>
+        <div class="mb-4">
+            <button class="button is-link is-light is-outlined" @click="storeUser.doRequestPasswordChange">
+                <span class="icon">
                     <span class="material-icons">
-                        logout
+                        lock_reset
                     </span>
-                    </span>
-                    <span>Forced logout</span>
-                </button>
-            </div>
-            <div class="level-item">
-                <button class="button is-link is-light is-outlined" @click="storeUser.doRevokeAccess">
-                    <span class="icon">
-                        <span class="material-icons">
-                            lock_outline
-                        </span>
-                    </span>
-                    <span>Revoke access</span>
-                </button>
-            </div>
+                </span>
+                <span>Request password</span>
+            </button>
         </div>
-        <div class="level-right">
-            <div class="level-item">
-                <button class="button is-danger is-light is-outlined" @click="storeUser.markUserAsDeleted">
-                    <span class="icon">
-                        <span class="material-icons">
-                            delete_forever
-                        </span>
+
+        <div class="mb-4">
+            <button class="button is-link is-light is-outlined" @click="storeUser.doPasswordReset">
+                <span class="icon">
+                    <span class="material-icons">
+                        key
                     </span>
-                    <span>Mark as deleted</span>
-                </button>
-            </div>
+                </span>
+                <span>Reset password</span>
+            </button>
+        </div>
+
+        <div class="mb-4">
+            <button class="button is-link is-light is-outlined" @click="storeUser.doRevokeAccess">
+                <span class="icon">
+                    <span class="material-icons">
+                        lock_outline
+                    </span>
+                </span>
+                <span>Revoke access</span>
+            </button>
+        </div>
+
+        <div class="mb-4">
+            <button class="button is-link is-light is-outlined" @click="storeUser.doLogout">
+                <span class="icon">
+                <span class="material-icons">
+                    logout
+                </span>
+                </span>
+                <span>Forced logout</span>
+            </button>
+        </div>
+
+        <div class="mb-4">
+            <button class="button is-danger is-light is-outlined" @click="storeUser.markUserAsDeleted">
+                <span class="icon">
+                    <span class="material-icons">
+                        delete_forever
+                    </span>
+                </span>
+                <span>Mark as deleted</span>
+            </button>
         </div>
     </div>
 </template>
