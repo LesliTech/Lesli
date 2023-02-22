52915846162e472e8d484ec322aaa3dad36432db
The Lesli Development Team <hello@lesli.tech>
2023-02-22 08:05:28 -0600

    refactor: rename key for custom default role

--- app/services/user/registration_service.rb
+++ app/services/user/registration_service.rb
@@ -53,12 +53,12 @@ class User::RegistrationService
 
         # add profile role to user only if multi-account is allowed
         if allow_multiaccount == false
-            # Custom instances can define a custom default role for new users on registration when multi-account is not allowed
+            # Custom instances can define a default role for new users on registration when multi-account is not allowed
             # Otherwise, the default role is "limited"
-            custom_default_role_name = Rails.application.config.lesli.dig(:security, :custom_default_role)
+            default_role_name = Rails.application.config.lesli.dig(:security, :default_role)
 
-            if custom_default_role_name.present?
-                @resource.user_roles.create({ role: account.roles.find_by(name: custom_default_role_name) })
+            if default_role_name.present?
+                @resource.user_roles.create({ role: account.roles.find_by(name: default_role_name) })
             else
                 @resource.user_roles.create({ role: account.roles.find_by(name: "limited") })
             end
