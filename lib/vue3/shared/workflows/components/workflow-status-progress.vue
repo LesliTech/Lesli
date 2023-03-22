d531b8b6aa9df866c2734e1bef58ba7866e6cc1e
The Lesli Development Team <hello@lesli.tech>
2023-03-22 12:15:54 -0600

    fix: update status

--- lib/vue3/shared/workflows/components/workflow-status-progress.vue
+++ lib/vue3/shared/workflows/components/workflow-status-progress.vue
@@ -17,7 +17,7 @@ For more information read the license file including with this software.
 */
 
 // · import vue tools
-import { ref, computed, onMounted, watch, onUnmounted } from "vue"
+import { ref, computed, onMounted } from "vue"
 
 // · import stores
 import { useWorkflow } from "LesliVue/stores/shared/workflow2"
@@ -48,6 +48,10 @@ const props = defineProps({
         type: String,
         required: true,
     },
+    cloudObjectId: {
+        type: String,
+        required: true,
+    },
     // · prop that indicates the object id of the resource that you need to interact with.
     workflowId: {
         type: Number,
@@ -61,14 +65,32 @@ const props = defineProps({
 })
 
 // · defining emits
-const emit = defineEmits(['onUpdatedStatus']);
+const emit = defineEmits(['onUpdatedStatus'])
+
+
+// returns a string like: "mitweken_cloud_workflow_statuses_id"
+const workflowStatusKey = computed(() => {
+    return Object.keys(storeWorkflow.workflow_statuses[0]).find(key => key.endsWith("workflows_id")).replace("workflows_id", "workflow_statuses_id")
+})
+
+const dropdownActive = ref(null)
 
 // set props to store
 storeWorkflow.cloudModule = props.cloudModule
 storeWorkflow.cloudObject = props.cloudObject
+storeWorkflow.cloudObjectId = props.cloudObjectId
 storeWorkflow.workflowId = props.workflowId
 storeWorkflow.currentStatus = props.currentStatus
 
+
+/**
+ * @description This function is called when the user updates the status of the project.
+ */
+ const onUpdatedStatus = (status) => {
+    storeWorkflow.currentStatus = status
+    storeWorkflow.updateStatus({[workflowStatusKey.value]: status.id})
+    emit('onUpdatedStatus')
+}
 
 // · fetch transition options
 onMounted(() => {
@@ -83,12 +105,42 @@ onMounted(() => {
     <stepper
         :steps="storeWorkflow.workflow_statuses"
         v-model="storeWorkflow.currentStatus"
-
         :stepsOptions="storeWorkflow.workflow_statuses"
     >
-
     </stepper>
 
+    <!-- Change the current status of the object -->
 
-
+    <div :class="['dropdown', 'is-right is-hoverable']">
+        <div class="dropdown-trigger">
+            <button class="button"
+                @blur="dropdownActive = false"
+                @click="dropdownActive = !dropdownActive">
+                <span class="icon">
+                    <span v-if="!dropdownActive" class="material-icons">
+                        edit
+                    </span>
+                    <span v-if="dropdownActive" class="material-icons">
+                        close
+                    </span>
+                </span>
+            </button>
+        </div>
+        <Transition>
+            <div v-if="dropdownActive" class="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                    <div class="dropdown-item" v-for="option in storeWorkflow.workflow_statuses">
+                        <div :class="{ active: option.number === currentStatus.number }">
+                            <a  @click="onUpdatedStatus(option)">
+                                {{ option.name }}
+                            </a>
+                            <span v-if="option.number === currentStatus.number" class="material-icons">
+                                    check_circle
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Transition>
+    </div>
 </template>
