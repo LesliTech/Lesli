fb4de1d944ff65a1efacbe5dfaceb1d3bbbeae43
Luis Donis <ldonis.emc2@gmail.com>
2023-05-16 18:45:37 -0600

    fix: engine selector

--- webpack.config.js
+++ webpack.config.js
@@ -32,185 +32,48 @@ Building a better future, one line of code at a time.
 
 
 // · Including plugins and dependencies
-var fs = require("fs")
 var path = require("path")
-var dayjs = require("dayjs")
-var webpack = require("webpack")
 var debug = require("lesli-js/debug/nodejs")
-var TerserPlugin = require("terser-webpack-plugin")
-var { VueLoaderPlugin } = require("vue-loader")
 
 
-
-
-const pathEngines = path.resolve("engines")
-
-
-
-var webpackConfig = []
-
+// · Including webpack configuration
+const webpackBase = require("./webpack/base")
 const applicationCore = require("./webpack/core")
-
 const applicationEngines = require("./webpack/engines")
+
+
+// get specific modules to work with, example: npm run webpack -- babel bell
+const requestedEngines = process.argv.slice(5)
+
 
 // · 
 module.exports = env => {
     
-
+    
     // set mode
     env.mode = env.mode ? env.mode : "development"
     env.watch = env.watch ? env.watch : false
-
-
-    // set production boolean
-    var production = env.mode == "production" ? true : false
-
-
-    // Base webpack configuration, this includes only core assets
-    var configuration = {
-
-        watch: env.watch == "true",
-        mode: production ? "production" : "development",
-
-        // Show only necessary stats when compiling
-        stats: {
-            all: false,
-            assets: true,
-            assetsSort: '!size',
-            env: true,
-            errors: true,
-            errorDetails: true,
-            errorsCount: true,
-            warnings: true,
-        },
-
-        // Set max size limit for JS bundles to 0.98 MB
-        performance: {
-            maxAssetSize: 1100000,
-            maxEntrypointSize: 1100000,
-            assetFilter: function(assetFilename) {
-                return !(['cloud_development/application.js'].includes(assetFilename));
-            }
-        },
-
-        // Use a custom plugin to remove all the comments
-        // from the javascript files
-        optimization: {
-            minimize: production,
-            minimizer: [
-                new TerserPlugin({
-                    terserOptions: {
-                        format: {
-                            comments: false,
-                        },
-                    },
-                    extractComments: false,
-                    parallel: true
-                })
-            ]
-        },
-
-
-        // Defined the core apps as main entry apps
-        // engine javascript apps are defined dynamically
-        entry: { },
-
-
-        // Set the name of the compiled javascript files
-        output: {
-            path: "",
-            filename: "[name].js"
-        },
-
-
-        // resolvers & path alias
-        resolve: {
-
-            // include folder so we can import direct from node modules
-            modules: [path.resolve("node_modules")],
-            alias: {
-
-                vue: "vue/dist/vue.esm-bundler.js",
-
-                // Set aliases as shortcuts to import modules
-                Lesli: path.resolve("lib", "vue"), 
-                LesliApp: path.resolve("app", "vue"),
-                LesliBuilder: path.resolve("engines", "lesli_builder", "app", "vue"),
-
-                CloudAdmin: path.resolve("app", "vue", "administration"),
-                CloudBell: path.resolve("engines", "cloud_bell", "app", "vue"),
-                CloudTalk: path.resolve("engines", "cloud_talk", "app", "vue"),
-                CloudTeam: path.resolve("engines", "cloud_team", "app", "vue"),
-                CloudTime: path.resolve("engines", "cloud_time", "app", "vue"),
-                CloudHelp: path.resolve("engines", "cloud_help", "app", "vue"),
-                CloudWork: path.resolve("engines", "cloud_work", "app", "vue"),
-                CloudAudit: path.resolve("engines", "cloud_audit", "app", "vue"),
-                CloudBabel: path.resolve("engines", "cloud_babel", "app", "vue"),
-                CloudFocus: path.resolve("engines", "cloud_focus", "app", "vue"),
-                CloudMailer: path.resolve("engines", "cloud_mailer", "app", "vue"),
-                CloudDriver: path.resolve("engines", "cloud_driver", "app", "vue"),
-                CloudSocial: path.resolve("engines", "cloud_social", "app", "vue"),
-                CloudShared: path.resolve("engines", "cloud_shared", "app", "vue"),
-                CloudScraper: path.resolve("engines", "cloud_scraper", "app", "vue"),
-                CloudAuthority: path.resolve("engines", "cloud_authority", "app", "vue"),
-                CloudDevelopment: path.resolve("engines", "cloud_development", "app", "vue")
-            }
-        },
-        module: {
-            rules: [{
-                test: /\.vue$/,
-                use: {
-                    loader: "vue-loader",
-                    options: {
-                        compilerOptions: {
-                            // ignore custom html components & elements
-                            isCustomElement: tag => tag === 'trix-editor'
-                        }
-                    }
-                }
-            }, {
-                test: /\.css$/,
-                use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader",   // translates CSS into CommonJS
-                ]
-            }, {
-                test: /\.m?js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [["@babel/preset-env", { "targets": "defaults" }]]
-                    }
-                }
-            }]
-        },
-        plugins: [
-            new VueLoaderPlugin(),
-
-            // custom constants
-            new webpack.DefinePlugin({
-                lesli_application_production: JSON.stringify(production),
-                lesli_application_development: JSON.stringify(!production),
-                lesli_application_version: JSON.stringify(dayjs().format("YYMMDD.HHmm")),
-                __VUE_OPTIONS_API__: true,
-                __VUE_PROD_DEVTOOLS__: true
-            })
-        ]
-
-    }
-
-    // we always compile the core
-    webpackConfig.push(Object.assign({}, configuration, applicationCore))
-
-    applicationEngines.forEach(engine => {
-        webpackConfig.push(Object.assign({}, configuration, engine.config))
-    })
+    env.production = ( env.mode == "production" ? true : false)
     
-    // show a nice debug message :) 
+
+    // webpack "threads"
+    var webpackConfig = []
+
+
+    // core vue applications
+    webpackConfig.push(Object.assign({}, webpackBase(env), applicationCore(env, requestedEngines)))
+
+
+    // engine vue applications
+    applicationEngines(env, requestedEngines).forEach(engine => {
+        webpackConfig.push(Object.assign({}, webpackBase(env), engine.config))
+    })
+
+
+    // show a nice debug message for every installed engine :) 
     webpackConfig.forEach(config => {
-        for (let [name, path] of Object.entries(config.entry)) {
-            debug.info(path.replace(pathEngines, ""))
+        for (let [name, path2] of Object.entries(config.entry)) {
+            debug.info(path2.replace(path.resolve("engines"), ""))
         }
     })
 
