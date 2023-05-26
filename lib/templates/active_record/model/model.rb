3e4428f1d759c02d538545137b1fa32313b1f999
Luis Donis <ldonis.emc2@gmail.com>
2023-05-26 00:53:21 -0600

    chore: update generator templates

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -1,7 +1,7 @@
 =begin
-<%= File.read(Pathname.new(LESLI_ROOT).join("license")).to_s.force_encoding("ASCII-8BIT") %>
-=end
+
+<%= File.read(Pathname.new(LESLI_ROOT).join("lesli.txt")).to_s.force_encoding("ASCII-8BIT") %>=end
 <% module_namespacing do -%>
-class <%= class_name %> < ApplicationRecord
+class <%= class_name %> < ApplicationLesliRecord
 end
 <% end -%>
