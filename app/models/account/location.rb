d249b33f37ab758ab2cf9a7dfd030708facc402e
Marcos Bonifasi <marcosbonifasi19@gmail.com>
2023-06-07 00:50:49 -0600

    fix: association

--- app/models/account/location.rb
+++ app/models/account/location.rb
@@ -1,5 +1,5 @@
 class Account::Location < ApplicationRecord
-    belongs_to  :account,         class_name: "Account",  foreign_key: "accounts_id"
+    belongs_to  :account,         class_name: "Account",  foreign_key: "account_id"
     belongs_to  :parent_location, class_name: "Location", foreign_key: "parent_id", optional: true
 
     has_many    :sub_locations,   class_name: "Location", foreign_key: "parent_id"
