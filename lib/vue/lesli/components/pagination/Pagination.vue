8917a252161a64e749445cb0bbacf242e0807ad4
Luis Donis <ldonis.emc2@gmail.com>
2023-06-26 20:37:34 -0600

    chore: improve pagination UI

--- lib/vue/lesli/components/pagination/Pagination.vue
+++ lib/vue/lesli/components/pagination/Pagination.vue
@@ -1,31 +1,49 @@
 <script setup>
 /*
-Copyright (c) 2022, all rights reserved.
+Lesli
 
-All the information provided by this platform is protected by international laws related  to 
-industrial property, intellectual property, copyright and relative international laws. 
-All intellectual or industrial property rights of the code, texts, trade mark, design, 
-pictures and any other information belongs to the owner of this platform.
+Copyright (c) 2023, Lesli Technologies, S. A.
 
-Without the written permission of the owner, any replication, modification,
-transmission, publication is strictly forbidden.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-For more information read the license file including with this software.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
-// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS development platform.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 // · 
 */
 
 
 // · import vue tools
-import { ref, reactive, onMounted, watch, computed } from "vue"
+import { computed } from "vue"
+
 
 // · defining emits
 const emit = defineEmits(['paginate']);
 
 
 // · defining props
+// · Pagination example
+// { "page": 1, "pages": 15, "total": 225, "results": 225 } 
 const props = defineProps({
+    
     pagination: {
         type: Object,
         required: true
@@ -39,88 +57,63 @@ const props = defineProps({
 })
 
 
-// · pagination details
-const previousPage = ref(0)
-const currentPage = ref(0)
-const nextPage = ref(0)
-const lastPage = ref(0)
-
-
-// · look for changes in pagination 
-watch(() => props.pagination, (val) => {
-
-    currentPage.value = props.pagination.page
-    lastPage.value = props.pagination.pages
-
-    previousPage.value = currentPage.value - 1
-    nextPage.value = currentPage.value + 1
-    return
-
-    if (currentPage.value > 1) {
-        previousPage.value = currentPage.value - 1
-    }
-
-    if (currentPage.value < lastPage.value) {
-        nextPage.value = currentPage.value + 1
-    }
-
-    // if first page we enable to navigate to the last page
-    if (currentPage.value >= lastPage.value) {
-        nextPage.value = lastPage.value
-    }
-
-    // if last page we enable to navigate to the first page
-    if (currentPage.value <= 1) {
-        previousPage.value = lastPage.value
-    }
-
-})
-
-
-// · emit the page selected
-function paginate(page) {
-    emit('paginate', page)
+// · 
+function paginateNext() {
+    props.pagination.page++
+    emit('paginate', props.pagination.page)
 }
 
 
 // · 
-const disableNext = computed(() => nextPage.value > lastPage.value)
-const disablePrevious = computed(() => previousPage.value < 1)
+function paginatePrev() {
+    props.pagination.page--
+    emit('paginate', props.pagination.page)
+}
+
+
+// · 
+function paginateLast() {
+    props.pagination.page = props.pagination.pages
+    emit('paginate', props.pagination.page)
+}
+
+
+// · 
+function paginateTo(page) {
+    props.pagination.page = page
+    emit('paginate', props.pagination.page)
+}
+
+
+// · 
+const disableNext = computed(() => props.pagination.page >= props.pagination.pages)
+const disablePrev = computed(() => props.pagination.page <= 1)
 
 </script>
 <template>
     <nav class="pagination mb-5" role="navigation" aria-label="pagination">
         <ul class="pagination-list" v-if="props.mode==='complete'">
             <template v-if="props.pagination.pages > 1">
-                <li>
-                    <button class="button pagination-link" @click.stop="paginate(1)" :disabled="currentPage == 1">
-                        1 
-                    </button>
-                </li>
-                <li>
-                    <span class="pagination-ellipsis">&hellip;</span>
-                </li>
-                <li>
-                    <button class="button pagination-link" @click.stop="paginate(previousPage)" :disabled="disablePrevious">
-                        {{( disablePrevious ? '' : previousPage )}}
-                    </button>
-                </li>
-                <li>
-                    <button class="button pagination-link is-current" aria-label="Page 46">
-                        {{ currentPage }}
-                    </button>
-                </li>
-                <li>
-                    <button class="button pagination-link" @click.stop="paginate(nextPage)" :disabled="disableNext">
-                        {{( disableNext ? '' : nextPage )}}
-                    </button>
-                </li>
+                <template v-for="page in 5"> 
+                    <li v-if="page < props.pagination.pages">
+                        <button 
+                            class="button pagination-link" 
+                            :class="{'is-current': props.pagination.page == page}"
+                            @click.stop="paginateTo(page)">
+                            {{ page }}
+                        </button>
+                    </li>
+                </template>
                 <li>
                     <span class="pagination-ellipsis">&hellip;</span>
                 </li>
                 <li>
-                    <button class="button pagination-link" @click.stop="paginate(lastPage)" :disabled="currentPage == lastPage">
-                        {{ lastPage }}
+                    <button 
+                        class="button pagination-link" 
+                        :class="{'is-current': props.pagination.page == props.pagination.pages }"
+                        :disabled="disableNext"
+                        @click.stop="paginateLast()">
+                        {{ props.pagination.pages }}
                     </button>
                 </li>
             </template>
@@ -128,13 +121,13 @@ const disablePrevious = computed(() => previousPage.value < 1)
         
         <button 
             class="button is-primary is-outlined pagination-previous" 
-            @click.stop="paginate(previousPage)"
-            :disabled="disablePrevious">
+            @click.stop="paginatePrev()"
+            :disabled="disablePrev">
             Previous 
         </button>
         <button 
             class="button is-primary is-outlined pagination-previous" 
-            @click.stop="paginate(nextPage)"
+            @click.stop="paginateNext()"
             :disabled="disableNext">
             Next page 
         </button>
