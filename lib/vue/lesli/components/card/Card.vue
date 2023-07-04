4e24ede6cedf28848171769537b6509fa077cc13
The Lesli Development Team <hello@lesli.tech>
2023-07-03 23:44:28 -0600

    refactor: update license

--- lib/vue/lesli/components/card/Card.vue
+++ lib/vue/lesli/components/card/Card.vue
@@ -1,5 +1,6 @@
 <script setup>
 /*
+
 Lesli
 
 Copyright (c) 2023, Lesli Technologies, S. A.
@@ -17,18 +18,20 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see http://www.gnu.org/licenses/.
 
-Lesli · Ruby on Rails SaaS development platform.
+Lesli · Your Smart Business Assistant. 
 
 Made with ♥ by https://www.lesli.tech
 Building a better future, one line of code at a time.
 
 @contact  hello@lesli.tech
-@website  https://www.lesli.tech
+@website  https://lesli.tech
 @license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
 
-// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 // · 
+
 */
+
 
 
 // · import vue tools
