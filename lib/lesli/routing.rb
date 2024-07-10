317d9b643686b62fede4f3008c86ef4cfca49176
Luis Donis <ldonis.emc2@gmail.com>
2024-07-09 22:53:53 -0600

    feat: force to download gems for spec

---
+++ lib/lesli/routing.rb
@@ -0,0 +1,57 @@
+=begin
+
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by LesliTech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+=end
+
+# ·
+module Lesli 
+    module Routing 
+        def self.mount_dashboard_for lesli_engine
+
+            lesli_engine::Engine.routes.draw do
+
+                # Dashboard alias
+                root to: "dashboards#show"
+
+                # Dashboard management
+                resource :dashboard, only: [:show]
+                resources :dashboards do
+                    collection do
+                        post "list" => :index
+                        get :options
+                    end
+                    scope module: :dashboard do
+                        resources :components
+                    end
+                end
+            end
+        end
+    end 
+end
