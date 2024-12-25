1f3a4edfc3e872371a43a3e345344c9af3d34bd2
Luis Donis <ldonis.emc2@gmail.com>
2024-12-24 21:33:52 -0600

    migrate actions to own repo

--- lib/lesli/courier.rb
+++ lib/lesli/courier.rb
@@ -39,55 +39,56 @@ Building a better future, one line of code at a time.
 # · LesliCourier.new(:lesli_test, []).from(:ticket_service).call(:index_with_deadline)
 # · LesliCourier.new(:lesli_test, []).from(:ticket_service).with(curent_user, query).call(:index)
 # · 
-class LesliCourier
+module Lesli
+    class Courier
 
-    # Store the module_name (e.g., :lesli_support as LesliSupport)
-    def initialize(module_name, on_error=nil)
-        @module_name = module_name.to_s.camelize
-        @on_error=on_error
-    end
+        # Store the module_name (e.g., :lesli_support as LesliSupport)
+        def initialize(module_name, on_error=nil)
+            @module_name = module_name.to_s.camelize
+            @on_error=on_error
+        end
 
-    # Dynamically determine the class to call (e.g., :ticket_service as TicketService)
-    def from(service_name)
-        @service_name = service_name.to_s.camelize
-        self
-    end
+        # Dynamically determine the class to call (e.g., :ticket_service as TicketService)
+        def from(service_name)
+            @service_name = service_name.to_s.camelize
+            self
+        end
 
-    # Call the dynamically constructed service method with optional arguments (via `with`)
-    def with(*args)
-        # Store the args for later use in the final call
-        @args = args
-        self
-    end
+        # Call the dynamically constructed service method with optional arguments (via `with`)
+        def with(*args)
+            # Store the args for later use in the final call
+            @args = args
+            self
+        end
 
-    # Store the method name to call on the service (e.g., :index_with_deadline)
-    def call(method_name)
-        @method_name = method_name
-        self.execute
-    end
+        # Store the method name to call on the service (e.g., :index_with_deadline)
+        def call(method_name)
+            @method_name = method_name
+            self.execute
+        end
 
-    # Execute the final call to the service, either with or without params
-    def execute
+        # Execute the final call to the service, either with or without params
+        def execute
 
-        module_name = "#{@module_name}"
+            module_name = "#{@module_name}"
 
-        # Look up the module and class dynamically
-        return @on_error unless Object.const_defined?(module_name)
+            # Look up the module and class dynamically
+            return @on_error unless Object.const_defined?(module_name)
 
-        module_service_name = "#{@module_name}::#{@service_name}"
+            module_service_name = "#{@module_name}::#{@service_name}"
 
-        return @on_error unless Object.const_defined?(module_service_name)
+            return @on_error unless Object.const_defined?(module_service_name)
 
-        module_service_class = module_service_name.constantize
-        
-        # Instantiate the service class with or without arguments
-        module_service_instance = module_service_class.new(*@args.to_a) 
+            module_service_class = module_service_name.constantize
+            
+            # Instantiate the service class with or without arguments
+            module_service_instance = module_service_class.new(*@args.to_a) 
 
-        # Check if the method is defined on the service instance
-        return @on_error unless module_service_instance.respond_to?(@method_name)
+            # Check if the method is defined on the service instance
+            return @on_error unless module_service_instance.respond_to?(@method_name)
 
-        # Dynamically call the method on the service instance
-        module_service_instance.send(@method_name)
+            # Dynamically call the method on the service instance
+            module_service_instance.send(@method_name)
+        end
     end
 end
-
