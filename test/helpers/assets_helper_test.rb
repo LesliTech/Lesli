e45dc31641da0cbd6db63c4fd328ce3080e39a0d
Luis Donis <ldonis.emc2@gmail.com>
2025-08-11 21:01:30 -0600

    add helper tests

---
+++ test/helpers/assets_helper_test.rb
@@ -0,0 +1,29 @@
+# test/helpers/lesli/assets_helper_test.rb
+require "test_helper"
+
+class Lesli::AssetsHelperTest < ActionView::TestCase
+    include Lesli::AssetsHelper
+
+    test "returns application stylesheet by default" do
+        def lesli_engine(key)
+            "root"
+        end
+        assert_equal("application", lesli_asset_path())
+    end
+
+    test "returns engine/stylesheet when engine is provided" do
+        assert_equal("lesli/application", lesli_asset_path(:lesli))
+    end
+
+    test "returns engine/application when engine and stylesheet are provided" do
+        assert_equal("lesli/application", lesli_asset_path(:lesli, "application"))
+    end
+
+    test "returns gem/application when gem and stylesheet are provided" do
+        assert_equal("lesli_assets/application", lesli_asset_path(:lesli_assets, "application"))
+    end
+
+    test "returns gem/custom when gem and stylesheet are provided" do
+        assert_equal("lesli_assets/custom", lesli_asset_path(:lesli_assets, "custom"))
+    end
+end
