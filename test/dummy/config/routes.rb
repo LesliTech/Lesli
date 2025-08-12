218513887c5f0d0897f81edb49293fb59474704d
Luis Donis <ldonis.emc2@gmail.com>
2025-08-11 20:34:59 -0600

    add minitest base

---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  mount Lesli::Engine => "/lesli"
+end
---
+++ test/lesli_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LesliTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert Lesli::VERSION
+  end
+end
