6816ba14373574a7d7ad7e88fb125a5cdf538fcf
Luis Donis <ldonis.emc2@gmail.com>
2025-08-11 21:50:59 -0600

    add base minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,10 @@ if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+
+module ResponseRequestHelper
+    def expect_response_with_successful response
+        assert_equal "application/json; charset=utf-8", response.content_type
+    end
+end
