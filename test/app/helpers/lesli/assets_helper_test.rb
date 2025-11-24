f273a2ca94e5f8ab642f967b3b86bfb3d8cb1de1
Luis Donis <ldonis.emc2@gmail.com>
2025-11-23 19:38:31 -0600

    update tests

--- test/app/helpers/lesli/assets_helper_test.rb
+++ test/app/helpers/lesli/assets_helper_test.rb
@@ -8,30 +8,30 @@ module Lesli
             def lesli_engine(key)
                 "root"
             end
-            assert_equal("application", lesli_asset_path())
+            assert_equal("application", lesli_stylesheet_path())
         end
 
         test "returns application stylesheet by default for engine" do
             def lesli_engine(key)
                 "lesli"
             end
-            assert_equal("lesli/custom", lesli_asset_path(nil, "custom"))
+            assert_equal("lesli/custom", lesli_stylesheet_path(nil, "custom"))
         end
 
         test "returns engine/stylesheet when engine is provided" do
-            assert_equal("lesli/application", lesli_asset_path(:lesli))
+            assert_equal("lesli/application", lesli_stylesheet_path(:lesli))
         end
 
         test "returns engine/application when engine and stylesheet are provided" do
-            assert_equal("lesli/application", lesli_asset_path(:lesli, "application"))
+            assert_equal("lesli/application", lesli_stylesheet_path(:lesli, "application"))
         end
 
         test "returns gem/application when gem and stylesheet are provided" do
-            assert_equal("lesli_assets/application", lesli_asset_path(:lesli_assets, "application"))
+            assert_equal("lesli_assets/application", lesli_stylesheet_path(:lesli_assets, "application"))
         end
 
         test "returns gem/custom when gem and stylesheet are provided" do
-            assert_equal("lesli_assets/custom", lesli_asset_path(:lesli_assets, "custom"))
+            assert_equal("lesli_assets/custom", lesli_stylesheet_path(:lesli_assets, "custom"))
         end
 
         test "returns favicon reference" do
